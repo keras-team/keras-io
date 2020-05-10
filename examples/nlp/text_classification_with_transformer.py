44009d482efe52bd48e02f902eb945ff7686d915
Apoorv Nandan <apoorv.nandan@gmail.com>
2020-05-11 01:45:35 +0530

    Updating code to best practices

--- examples/nlp/text_classification_with_transformer.py
+++ examples/nlp/text_classification_with_transformer.py
@@ -3,7 +3,7 @@ Title: Text classification with Transformer
 Author: [Apoorv Nandan](https://twitter.com/NandanApoorv)
 Date created: 2020/05/10
 Last modified: 2020/05/10
-Description: Implement transformer block as a Keras layer and use it for text classification.
+Description: Implement a Transformer block as a Keras layer and use it for text classification.
 """
 """
 ## Setup
@@ -11,54 +11,56 @@ Description: Implement transformer block as a Keras layer and use it for text cl
 
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 
 """
 ## Implement multi head self attention as a Keras layer
 """
 
 
-class MultiHeadSelfAttention(keras.layers.Layer):
+class MultiHeadSelfAttention(layers.Layer):
     def __init__(self, embed_dim, num_heads=8):
         super(MultiHeadSelfAttention, self).__init__()
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        assert (
-            embed_dim % num_heads == 0
-        ), "embedding dimension not divisible by num heads"
+        if embed_dim % num_heads != 0:
+            raise ValueError(
+                f"embedding dimension = {embed_dim} should be divisible by number of heads = {num_heads}"
+            )
         self.projection_dim = embed_dim // num_heads
-        self.wq = keras.layers.Dense(embed_dim)
-        self.wk = keras.layers.Dense(embed_dim)
-        self.wv = keras.layers.Dense(embed_dim)
-        self.combine_heads = keras.layers.Dense(embed_dim)
+        self.query_dense = layers.Dense(embed_dim)
+        self.key_dense = layers.Dense(embed_dim)
+        self.value_dense = layers.Dense(embed_dim)
+        self.combine_heads = layers.Dense(embed_dim)
 
-    def attention(self, q, k, v):
-        score = tf.matmul(q, k, transpose_b=True)
-        dk = tf.cast(tf.shape(k)[-1], tf.float32)
-        scaled_score = score / tf.math.sqrt(dk)
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
         weights = tf.nn.softmax(scaled_score, axis=-1)
-        output = tf.matmul(weights, v)
+        output = tf.matmul(weights, value)
         return output, weights
 
     def separate_heads(self, x, batch_size):
         x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
         return tf.transpose(x, perm=[0, 2, 1, 3])
 
-    def call(self, x):
+    def call(self, inputs):
         # x.shape = [batch_size, seq_len, embedding_dim]
-        batch_size = tf.shape(x)[0]
-        q = self.wq(x)  # (batch_size, seq_len, embed_dim)
-        k = self.wk(x)  # (batch_size, seq_len, embed_dim)
-        v = self.wv(x)  # (batch_size, seq_len, embed_dim)
-        q = self.separate_heads(
-            q, batch_size
+        batch_size = tf.shape(inputs)[0]
+        query = self.query_dense(inputs)  # (batch_size, seq_len, embed_dim)
+        key = self.key_dense(inputs)  # (batch_size, seq_len, embed_dim)
+        value = self.value_dense(inputs)  # (batch_size, seq_len, embed_dim)
+        query = self.separate_heads(
+            query, batch_size
         )  # (batch_size, num_heads, seq_len, projection_dim)
-        k = self.separate_heads(
-            k, batch_size
+        key = self.separate_heads(
+            key, batch_size
         )  # (batch_size, num_heads, seq_len, projection_dim)
-        v = self.separate_heads(
-            v, batch_size
+        value = self.separate_heads(
+            value, batch_size
         )  # (batch_size, num_heads, seq_len, projection_dim)
-        attention, weights = self.attention(q, k, v)
+        attention, weights = self.attention(query, key, value)
         attention = tf.transpose(
             attention, perm=[0, 2, 1, 3]
         )  # (batch_size, seq_len, num_heads, projection_dim)
@@ -72,38 +74,29 @@ class MultiHeadSelfAttention(keras.layers.Layer):
 
 
 """
-## Implement transformer block as a layer
+## Implement a Transformer block as a layer
 """
 
 
-class TransformerLayer(tf.keras.layers.Layer):
+class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
-        super(TransformerLayer, self).__init__()
-
+        super(TransformerBlock, self).__init__()
         self.att = MultiHeadSelfAttention(embed_dim, num_heads)
         self.ffn = keras.Sequential(
-            [
-                keras.layers.Dense(ff_dim, activation="relu"),
-                keras.layers.Dense(embed_dim),
-            ]
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
         )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
 
-        self.layernorm1 = keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = keras.layers.LayerNormalization(epsilon=1e-6)
-
-        self.dropout1 = tf.keras.layers.Dropout(rate)
-        self.dropout2 = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, training):
-        attn_output = self.att(x)
+    def call(self, inputs, training):
+        attn_output = self.att(inputs)
         attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(x + attn_output)
-
+        out1 = self.layernorm1(inputs + attn_output)
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.layernorm2(out1 + ffn_output)
-
-        return out2
+        return self.layernorm2(out1 + ffn_output)
 
 
 """
@@ -113,13 +106,11 @@ Two seperate embedding layers, one for tokens, one for token index (positions).
 """
 
 
-class EmbeddingLayer(keras.layers.Layer):
+class TokenAndPositionEmbedding(layers.Layer):
     def __init__(self, maxlen, vocab_size, emded_dim):
-        super(EmbeddingLayer, self).__init__()
-        self.token_emb = keras.layers.Embedding(
-            input_dim=vocab_size, output_dim=emded_dim
-        )
-        self.pos_emb = keras.layers.Embedding(input_dim=maxlen, output_dim=emded_dim)
+        super(TokenAndPositionEmbedding, self).__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=emded_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=emded_dim)
 
     def call(self, x):
         maxlen = tf.shape(x)[-1]
@@ -130,54 +121,48 @@ class EmbeddingLayer(keras.layers.Layer):
 
 
 """
-## Create classifier model using transformer layer
-
-Transformer layer outputs one vector for each time step of your input sequence. Here, we take the mean across all time steps and build a two layered feed forward network on top of it.
-"""
-
-
-class TransformerClassifier(tf.keras.Model):
-    def __init__(self, maxlen, vocab_size, embed_dim, ff_dim, num_heads):
-        super(TransformerClassifier, self).__init__()
-        self.emb = EmbeddingLayer(maxlen, vocab_size, embed_dim)
-        self.transformer = TransformerLayer(embed_dim, num_heads, ff_dim)
-        self.prehead = keras.layers.Dense(20, activation="relu")
-        self.dropout1 = keras.layers.Dropout(0.05)
-        self.dropout2 = keras.layers.Dropout(0.05)
-        self.head = keras.layers.Dense(2, activation="softmax")
-
-    def call(self, x, training):
-        x = self.emb(x)
-        x = self.transformer(x, training)
-        x = tf.math.reduce_mean(x, axis=1)
-        x = self.dropout1(x, training=training)
-        x = self.prehead(x)
-        x = self.dropout2(x, training=training)
-        x = self.head(x)
-        return x
-
-
-"""
 ## Download and prepare dataset
 """
 
-max_features = 6000  # Only consider the top 20k words
+vocab_size = 20000  # Only consider the top 20k words
 maxlen = 200  # Only consider the first 200 words of each movie review
-(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(
-    num_words=max_features
-)
+(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=vocab_size)
 print(len(x_train), "Training sequences")
 print(len(x_val), "Validation sequences")
 x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
 x_val = keras.preprocessing.sequence.pad_sequences(x_val, maxlen=maxlen)
 
 """
+## Create classifier model using transformer layer
+
+Transformer layer outputs one vector for each time step of our input sequence.
+Here, we take the mean across all time steps and 
+use a feed forward network on top of it to classify text.
+"""
+
+
+embed_dim = 32  # Embedding size for each token
+num_heads = 2  # Number of attention heads
+ff_dim = 32  # Hidden layer size in feed forward network inside transformer
+
+inputs = layers.Input(shape=(maxlen,))
+embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+x = embedding_layer(inputs)
+transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+x = transformer_block(x)
+x = layers.GlobalAveragePooling1D()(x)
+x = layers.Dropout(0.1)(x)
+x = layers.Dense(20, activation="relu")(x)
+x = layers.Dropout(0.1)(x)
+outputs = layers.Dense(2, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+
+"""
 ## Train and Evaluate
 """
 
-model = TransformerClassifier(
-    maxlen=maxlen, vocab_size=max_features, embed_dim=32, ff_dim=32, num_heads=1
-)
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
 history = model.fit(
     x_train, y_train, batch_size=32, epochs=2, validation_data=(x_val, y_val)
