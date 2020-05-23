9126acb2e9a8c5bc681d84f9ceaa1397a9078533
François Chollet <francois.chollet@gmail.com>
2020-05-23 12:58:23 -0700

    Style nits

--- examples/generative/pixelcnn.py
+++ examples/generative/pixelcnn.py
@@ -12,7 +12,7 @@ from tensorflow import keras, nn
 from tensorflow.keras import layers
 
 """
-##Getting the Data
+## Getting the Data
 """
 
 # Model / data parameters
@@ -30,11 +30,11 @@ data = np.where(data < (0.33 * 256), 0, 1)
 data = data.astype(np.float32)
 
 """
-##Create two classes for the requisite Layers for the model
+## Create two Layer classes required by the model
 """
 
-# the first layer to create will be the PixelCNN layer, this layer simply
-# builds on the 2D convolutional layer but with the requisite masking included
+# The first layer is the PixelCNN layer. This layer simply
+# builds on the 2D convolutional layer, but includes masking.
 class PixelConvLayer(layers.Layer):
     def __init__(self, mask_type, **kwargs):
         super(PixelConvLayer, self).__init__()
@@ -42,9 +42,9 @@ class PixelConvLayer(layers.Layer):
         self.conv = layers.Conv2D(**kwargs)
 
     def build(self, input_shape):
-        # build the conv2d layer to initialize kernel variables
+        # Build the conv2d layer to initialize kernel variables
         self.conv.build(input_shape)
-        # use said initialized kernel to develop the mask
+        # Use the initialized kernel to create the mask
         kernel_shape = self.conv.kernel.get_shape()
         self.mask = np.zeros(shape=kernel_shape)
         self.mask[: kernel_shape[0] // 2, ...] = 1.0
@@ -57,27 +57,27 @@ class PixelConvLayer(layers.Layer):
         return self.conv(inputs)
 
 
-# Next we build our residual block layer,
-# this is just a normal residual block but with the PixelConvLayer built in
+# Next, we build our residual block layer.
+# This is just a normal residual block, but base don the PixelConvLayer.
 class ResidualBlock(keras.layers.Layer):
     def __init__(self, filters, **kwargs):
         super(ResidualBlock, self).__init__(**kwargs)
-        self.a = keras.layers.ReLU()
-        self.b = keras.layers.Conv2D(filters=filters, kernel_size=1, activation="relu")
-        self.c = PixelConvLayer(
+        self.activation = keras.layers.ReLU()
+        self.conv1 = keras.layers.Conv2D(filters=filters, kernel_size=1, activation="relu")
+        self.pixel_conv = PixelConvLayer(
             mask_type="B",
             filters=filters // 2,
             kernel_size=3,
             activation="relu",
             padding="same",
         )
-        self.d = keras.layers.Conv2D(filters=filters, kernel_size=1, activation="relu")
+        self.conv2 = keras.layers.Conv2D(filters=filters, kernel_size=1, activation="relu")
 
     def call(self, inputs):
-        x = self.a(inputs)
-        x = self.b(x)
-        x = self.c(x)
-        x = self.d(x)
+        x = self.activation(inputs)
+        x = self.conv1(x)
+        x = self.pixel_conv(x)
+        x = self.conv2(x)
         return keras.layers.add([inputs, x])
 
 
@@ -108,18 +108,18 @@ out = keras.layers.Conv2D(
     filters=1, kernel_size=1, strides=1, activation="sigmoid", padding="valid"
 )(x)
 
-PixelCNN = keras.Model(inputs, out)
+pixel_cnn = keras.Model(inputs, out)
 adam = keras.optimizers.Adam(learning_rate=0.0001)
-PixelCNN.compile(optimizer=adam, loss="binary_crossentropy")
+pixel_cnn.compile(optimizer=adam, loss="binary_crossentropy")
 
-PixelCNN.summary()
-PixelCNN.fit(x=data, y=data, batch_size=64, epochs=50, validation_split=0.1)
+pixel_cnn.summary()
+pixel_cnn.fit(x=data, y=data, batch_size=64, epochs=50, validation_split=0.1)
 
 """
 ## Demonstration
 
-The PixelCNN cannot create the full image at once and must instead create each pixel in
-order, append the next created pixel to current image, and feed the image back into the
+The PixelCNN cannot generate the full image at once, and must instead generate each pixel in
+order, append the last generated pixel to the current image, and feed the image back into the
 model to repeat the process.
 """
 
@@ -147,7 +147,7 @@ for row in tqdm(range(rows)):
 
 
 def deprocess_image(x):
-    # stack the single channeled black and white image to rgb values.
+    # Stack the single channeled black and white image to rgb values.
     x = np.stack((x, x, x), 2)
     # Undo preprocessing
     x *= 255.0
