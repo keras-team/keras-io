9781118529276289bb12a9ab906e12fd88b80a4f
Jacob Chapman <jacob.chapman@kindredgroup.com>
2020-05-25 19:17:04 +0100

    Move comments and only RMSprop update every 4 actions

--- examples/rl/deep_q_network_breakout.py
+++ examples/rl/deep_q_network_breakout.py
@@ -8,23 +8,28 @@ Description: Implement Deep Q Network in Breakout Atari environment.
 """
 ## Introduction
 
-This script shows an implementation of Deep Q Network method on CartPole-V0 environment.
+This script shows an implementation of Deep Q Network method on BreakoutNoFrameskip-v4 environment.
 
 ### Deep Q Network Method
 
 As an agent takes actions and moves through an environment, it learns to map
-the observed state of the environment to an action. A Q-Learning Agent learns to
-perform its tasks, such that the recommended action maximizes the potential rewards.
+the observed state of the environment to an action. An agent will choose an action 
+in a given state based on a Q quantity, which is a weighted reward based on the 
+expected highest long term reward. This method is considered an Off-Policy method, 
+meaning its Q values are updated assuming that the best action was chosen, even 
+if the best action was not chosen. Thus A Q-Learning Agent learns to perform its 
+tasks, such that the recommended action maximizes the potential rewards.
 
-### CartPole-V0
 
-A pole is attached to a cart placed on a frictionless track. The agent has to apply
-force to move the cart. It is rewarded for for every time step the pole
-remains upright. The agent, therefore, must learn to keep the pole from falling over.
+### BreakoutNoFrameskip-v4
+
+A board moves along the bottom of the screen returning a ball that will destroy blocks
+at the top of the screen. The aim of the game is to remove all blocks and breakout of the
+level. The agent must learn to control the board by moving left and right, returning the 
+ball and removing all the blocks without the ball passing the board.   
 
 ### References
 
-- [CartPole](http://www.derongliu.org/adp/adp-cdrom/Barto1983.pdf)
 - [Q-Learning](https://link.springer.com/content/pdf/10.1007/BF00992698.pdf)
 - [Deep Q-Learning](https://deepmind.com/research/publications/human-level-control-through-deep-reinforcement-learning)
 """
@@ -37,7 +42,6 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-import os
 
 # Configuration paramaters for the whole setup
 seed = 42
@@ -69,8 +73,10 @@ is choose by selecting the larger of the two q-values predicted in the output la
 
 num_actions = 4
 
+# Network defined by the Deepmind paper
 inputs = layers.Input(shape=(84, 84, 4,))
 
+# Convolutions on the frames on the screen
 layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
 layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
 layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
@@ -87,7 +93,7 @@ model = keras.Model(inputs=inputs, outputs=action)
 ## Train
 """
 
-optimizer = keras.optimizers.Adam(learning_rate=0.01)
+optimizer = keras.optimizers.RMSprop(learning_rate=0.00025, momentum=0.95)
 mse_loss = keras.losses.MeanSquaredError()
 model.compile(loss=mse_loss, optimizer=optimizer)
 
@@ -100,9 +106,10 @@ done_history = []
 running_reward = 0
 episode_count = 0
 frame_count = 0
-epsilon_random_frames = 50000  # number of frames to take random action and observe output
-epsilon_greedy_frames = 1000000.0  # number of frames for exploration
-max_memory_length = 1000000  # maximum replay length
+epsilon_random_frames = 50000  # Number of frames to take random action and observe output
+epsilon_greedy_frames = 1000000.0  # Number of frames for exploration
+max_memory_length = 1000000  # Maximum replay length
+update_after_actions = 4
 
 while True:  # Run until solved
     state = np.array(env.reset())
@@ -144,12 +151,12 @@ while True:  # Run until solved
         rewards_history.append(reward if not done else -1.0)
         state = state_next
 
-        if len(done_history) > batch_size:
-            # Sample from history for replay
+        # Update every fourth frame and once batch size is over 32
+        if frame_count % update_after_actions == 0 and len(done_history) > batch_size:
+            # Get indices of samples for replay buffers
             indices = np.random.choice(range(len(done_history)), size=batch_size)
 
-            # using list comprehension to
-            # elements from list
+            # Using list comprehension to sample from replay buffer
             state_sample = np.array([state_history[i] for i in indices])
             state_next_sample = np.array([state_next_history[i] for i in indices])
             rewards_sample = [rewards_history[i] for i in indices]
@@ -175,24 +182,24 @@ while True:  # Run until solved
                 # If final frame take the last reward as the new Q value
                 q_update = sampled_reward
                 if not sampled_done:
-                    # Calculate the new q value
-                    # q value = reward + discount factor * expected future reward
+                    # Calculate the new Q value
+                    # Q value = reward + discount factor * expected future reward
                     q_update = sampled_reward + gamma * np.amax(future_reward)
 
-                # replace old q value with the updated value
+                # Replace old Q value with the updated value
                 q_value[sampled_action] = q_update
                 updated_q_values.append(q_value)
 
-            # train the model on the states and updated q values
+            # Train the model on the states and updated Q values
             model.fit(state_sample, np.array(updated_q_values), verbose=0)
 
-            # limit the state and reward history
-            if len(rewards_history) > max_memory_length:
-                del rewards_history[:1]
-                del state_history[:1]
-                del state_next_history[:1]
-                del action_history[:1]
-                del done_history[:1]
+        # Limit the state and reward history
+        if len(rewards_history) > max_memory_length:
+            del rewards_history[:1]
+            del state_history[:1]
+            del state_next_history[:1]
+            del action_history[:1]
+            del done_history[:1]
 
         if done:
             break
