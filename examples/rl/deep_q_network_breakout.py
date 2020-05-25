61565a1730db323f89d1460eec73eeb597220073
Jacob Chapman <jacob.chapman@kindredgroup.com>
2020-05-25 18:50:28 +0100

    formatting

--- examples/rl/deep_q_network_breakout.py
+++ examples/rl/deep_q_network_breakout.py
@@ -3,7 +3,7 @@ Title: Deep Q Network Method
 Author: [Jacob Chapman](https://twitter.com/jacoblchapman)
 Date created: 2020/05/23
 Last modified: 2020/05/23
-Description: Implement Deep Q Network in CartPole environment.
+Description: Implement Deep Q Network in Breakout Atari environment.
 """
 """
 ## Introduction
@@ -26,6 +26,7 @@ remains upright. The agent, therefore, must learn to keep the pole from falling 
 
 - [CartPole](http://www.derongliu.org/adp/adp-cdrom/Barto1983.pdf)
 - [Q-Learning](https://link.springer.com/content/pdf/10.1007/BF00992698.pdf)
+- [Deep Q-Learning](https://deepmind.com/research/publications/human-level-control-through-deep-reinforcement-learning)
 """
 """
 ## Setup
@@ -44,15 +45,17 @@ gamma = 0.99  # Discount factor for past rewards
 epsilon = 1.0  # Epsilon greedy parameter
 epsilon_min = 0.01  # Minimum epsilon greedy parameter
 epsilon_max = 1.0  # Minimum epsilon greedy parameter
-epsilon_interval = epsilon_max - epsilon_min  # Rate at which to reduce chance of random action being taken
+epsilon_interval = (
+    epsilon_max - epsilon_min
+)  # Rate at which to reduce chance of random action being taken
 batch_size = 32  # Size of batch taken from replay buffer
 max_steps_per_episode = 10000
 
+# Use the Baseline Atari environment because of Deepmind helper functions
 env = make_atari("BreakoutNoFrameskip-v4")
+# warp the frames, grey scale, stake four frame and scale to smaller ratio
 env = wrap_deepmind(env, frame_stack=True, scale=True)
 env.seed(seed)
-eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
-
 
 """
 ## Implement Deep Q Network
@@ -68,15 +71,15 @@ num_actions = 4
 
 inputs = layers.Input(shape=(84, 84, 4,))
 
-layer1 = layers.Conv2D(32, 8, strides=4, activation='relu')(inputs)
-layer2 = layers.Conv2D(64, 4, strides=2, activation='relu')(layer1)
-layer3 = layers.Conv2D(64, 3, strides=1, activation='relu')(layer2)
+layer1 = layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+layer2 = layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+layer3 = layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
 
 layer4 = layers.Flatten()(layer3)
 
-layer5 = layers.Dense(512, activation='relu')(layer4)
-layer6 = layers.Dense(512, activation='relu')(layer5)
-action = layers.Dense(num_actions, activation='linear')(layer6)
+layer5 = layers.Dense(512, activation="relu")(layer4)
+layer6 = layers.Dense(512, activation="relu")(layer5)
+action = layers.Dense(num_actions, activation="linear")(layer6)
 
 model = keras.Model(inputs=inputs, outputs=action)
 
@@ -89,10 +92,6 @@ mse_loss = keras.losses.MeanSquaredError()
 model.compile(loss=mse_loss, optimizer=optimizer)
 
 
-model_file = "model.h5"
-if os.path.isfile(model_file):
-    model = keras.models.load_model("model.h5")
-
 action_history = []
 state_history = []
 state_next_history = []
@@ -101,9 +100,9 @@ done_history = []
 running_reward = 0
 episode_count = 0
 frame_count = 0
-epsilon_random_frames = 50000
-epsilon_greedy_frames = 1000000.0
-max_memory_length = 1000000
+epsilon_random_frames = 50000  # number of frames to take random action and observe output
+epsilon_greedy_frames = 1000000.0  # number of frames for exploration
+max_memory_length = 1000000  # maximum replay length
 
 while True:  # Run until solved
     state = np.array(env.reset())
@@ -114,20 +113,20 @@ while True:  # Run until solved
         # of the agent in a pop up window.
         frame_count += 1
 
-        # use epsilon-greedy for exploration
+        # Use epsilon-greedy for exploration
         if frame_count < epsilon_random_frames or epsilon > np.random.rand(1)[0]:
-            # take random action
+            # Take random action
             action = np.random.choice(num_actions)
         else:
             # Predict action q-values
-            # from environment state
+            # From environment state
             state_tensor = tf.convert_to_tensor(state)
             state_tensor = tf.expand_dims(state_tensor, 0)
             action_probs = model(state_tensor)
-            # take best action
+            # Take best action
             action = np.argmax(action_probs[0])
 
-        # decay probability of taking random action
+        # Decay probability of taking random action
         epsilon -= epsilon_interval / epsilon_greedy_frames
         epsilon = max(epsilon, epsilon_min)
 
@@ -137,7 +136,7 @@ while True:  # Run until solved
 
         episode_reward += reward
 
-        # save actions and states in replay buffer
+        # Save actions and states in replay buffer
         action_history.append(action)
         state_history.append(state)
         state_next_history.append(state_next)
@@ -146,7 +145,7 @@ while True:  # Run until solved
         state = state_next
 
         if len(done_history) > batch_size:
-            # sample from history for replay
+            # Sample from history for replay
             indices = np.random.choice(range(len(done_history)), size=batch_size)
 
             # using list comprehension to
@@ -160,12 +159,20 @@ while True:  # Run until solved
             future_rewards = model(state_next_sample)
             q_values = np.array(model(state_sample))
 
-            history = zip(rewards_sample,
-                          done_sample, action_sample,future_rewards, q_values)
+            history = zip(
+                rewards_sample, done_sample, action_sample, future_rewards, q_values
+            )
 
+            # Build the new updated Q values for the sampled states and actions
             updated_q_values = []
-            for sampled_reward, sampled_done, sampled_action, future_reward, q_value in history:
-
+            for (
+                sampled_reward,
+                sampled_done,
+                sampled_action,
+                future_reward,
+                q_value,
+            ) in history:
+                # If final frame take the last reward as the new Q value
                 q_update = sampled_reward
                 if not sampled_done:
                     # Calculate the new q value
@@ -197,10 +204,7 @@ while True:  # Run until solved
     if episode_count % 500 == 0:
         template = "running reward: {:.2f} at episode {}, frame count {}, epsilon {}"
         print(template.format(running_reward, episode_count, frame_count, epsilon))
-        model.save(model_file)
 
     if running_reward > 2000:  # Condition to consider the task solved
         print("Solved at episode {}!".format(episode_count))
         break
-
-
