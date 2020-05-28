3796a181d66281bdf74bdbbc246d6a0f3c304668
Voyta Krizek <vkdot@users.noreply.github.com>
2020-05-28 07:30:25 +0100

    Inlined function call

--- examples/vision/image_classification_from_scratch.py
+++ examples/vision/image_classification_from_scratch.py
@@ -61,21 +61,18 @@ occurence. Let's filter out badly-encoded images that do not feature the string 
 
 import os
 
-
-def is_jfif(path):
-    try:
-        fobj = open(fpath, "rb")
-        return tf.compat.as_bytes("JFIF") in fobj.peek(10)
-    finally:
-        fobj.close()
-
-
 num_skipped = 0
 for folder_name in ("Cat", "Dog"):
     folder_path = os.path.join("PetImages", folder_name)
     for fname in os.listdir(folder_path):
         fpath = os.path.join(folder_path, fname)
-        if not is_jfif(fpath):
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
             num_skipped += 1
             # Delete corrupted image
             os.remove(fpath)
