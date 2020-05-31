2728b4c3088ebe595db3051a71c7bfbba8fa9256
Apoorv Nandan <apoorv.nandan@gmail.com>
2020-05-31 20:44:03 +0530

    Updated mini gpt example

--- examples/nlp/text_generation_with_miniature_gpt.py
+++ examples/nlp/text_generation_with_miniature_gpt.py
@@ -6,18 +6,38 @@ Last modified: 2020/05/29
 Description: Implement miniature version of GPT and learn to generate text.
 """
 """
+## Introduction
+
+This example demonstrates autoregressive language modelling using a
+a miniature version of GPT model.
+The model consists of a single transformer block with causal masking
+in the its attention layer.
+We use the text from IMDB sentiment classification dataset for training
+and generate new movie reviews for a given prompt.
+When using this script with your own data, make sure it has atleast
+1M words.
+
+**References:**
+
+- [GPT](https://s3-us-west-2.amazonaws.com/openai-assets/research-covers/language-unsupervised/language_understanding_paper.pdf)
+- [GPT-2](https://d4mucfpksywv.cloudfront.net/better-language-models/language_models_are_unsupervised_multitask_learners.pdf)
+- [GPT-3](https://arxiv.org/pdf/2005.14165.pdf)
+"""
+"""
 ## Setup
 """
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 import numpy as np
+import os
 
 """
-## Self attention with causal masking
+## Self-attention with causal masking
 
-We compute self attention as usual, but prevent any information to flow
-from future tokens by masking the upper half of scaled dot product matrix.
+We compute self-attention as usual, but prevent any information to flow
+from future tokens by masking the upper half of the scaled dot product matrix.
 """
 
 
@@ -123,9 +143,9 @@ class TransformerBlock(layers.Layer):
         self.dropout2 = layers.Dropout(rate)
 
     def call(self, inputs, training):
-        attn_output = self.att(inputs)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
+        attention_output = self.att(inputs)
+        attention_output = self.dropout1(attention_output, training=training)
+        out1 = self.layernorm1(inputs + attention_output)
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
@@ -159,20 +179,20 @@ vocab_size = 20000  # Only consider the top 20k words
 maxlen = 200  # Max sequence size
 embed_dim = 32  # Embedding size for each token
 num_heads = 2  # Number of attention heads
-ff_dim = 32  # Hidden layer size in feed forward network inside transformer
+feed_forward_dim = 32  # Hidden layer size in feed forward network inside transformer
 
 
 def create_model():
-    inputs = layers.Input(shape=(maxlen,))
+    inputs = layers.Input(shape=(maxlen,), dtype=tf.int32)
     embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
     x = embedding_layer(inputs)
-    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    transformer_block = TransformerBlock(embed_dim, num_heads, feed_forward_dim)
     x = transformer_block(x)
     outputs = layers.Dense(vocab_size, activation="softmax")(x)
     model = keras.Model(inputs=inputs, outputs=[outputs, x])
     model.compile(
-        "adam", loss=["sparse_categorical_crossentropy", None],
-    )
+        "adam", loss=["sparse_categorical_crossentropy", None],  
+    )  # No loss and optimization based on word embeddings from transformer block
     return model
 
 
@@ -184,56 +204,54 @@ text generation task.
 """
 
 
-(x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=vocab_size)
-print(len(x_train), "Training sequences")
-print(len(x_val), "Validation sequences")
+!curl -O https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
+!tar -xf aclImdb_v1.tar.gz
 
-x_train = np.hstack([x_train, x_val])  # Combine training and validation reviews
+batch_size = 16
 
-print(len(x_train), "Total sequences")
+# The dataset contains each review in a separate text file
+# The text files are present in four different folders
+# Create a list all files
+filenames = []
+directories = ['aclImdb/train/pos', 
+               'aclImdb/train/neg', 
+               'aclImdb/test/pos', 
+               'aclImdb/test/neg']
+for dir in directories:
+    for f in os.listdir(dir):
+        filenames.append(os.path.join(dir, f))
 
-# Dictionary with words mapped to their vocabulary index
-word_index = keras.datasets.imdb.get_word_index()
+print(f'{len(filenames)} files')
 
-# Dictionary with token index mapped to words
-# Special tokens:
-# 0 : padding
-# 1 : start of review
-# 2 : Out of vocabulary
-index_word = {}
-for word in word_index:
-    index = word_index[word]
-    index_word[index + 3] = word
+# Create dataset from text files
+text_ds = tf.data.TextLineDataset(filenames)
+text_ds = text_ds.batch(batch_size)
 
-index_word[0] = "<pad>"
-index_word[1] = "<s>"
-index_word[2] = "<oov>"
+# Create vectcorization layer and adapt it to the text
+vectorize_layer = TextVectorization(
+    standardize=None,
+    max_tokens=vocab_size-1,
+    output_mode="int",
+    output_sequence_length=maxlen+1,
+)
+vectorize_layer.adapt(text_ds)
+vocab = vectorize_layer.get_vocabulary()  # To get words back from token indices
 
 
-def prepare_lm_inputs_labels(tokenized_sentences, max_len):
+def prepare_lm_inputs_labels(text):
     """
     Shift word sequences by 1 position so that the target for position (i) is
     word at position (i+1). The model will use all words up till position (i)
     to predict the next word.
     """
-    x_lm, y_lm = [], []
-    for sentence in tokenized_sentences:
-        x = sentence[:-1]
-        y = sentence[1:]
-        pad_len = max_len - len(x)
-        if pad_len > 0:
-            x = x + [0] * pad_len
-            y = y + [0] * pad_len
-        elif pad_len < 0:
-            x = x[:max_len]
-            y = y[:max_len]
-        x_lm.append(x)
-        y_lm.append(y)
-    return np.array(x_lm), np.array(y_lm)
+    text = tf.expand_dims(text, -1)
+    tokenized_sentences = vectorize_layer(text)
+    x = tokenized_sentences[:,:-1]
+    y = tokenized_sentences[:,1:]
+    return x, y
 
-
-x_lm, y_lm = prepare_lm_inputs_labels(x_train, max_len=maxlen)
-x_lm.shape
+text_ds = text_ds.map(prepare_lm_inputs_labels)
+text_ds = text_ds.repeat().prefetch(tf.data.experimental.AUTOTUNE)
 
 
 """
@@ -243,23 +261,22 @@ x_lm.shape
 
 class TextGenerator(keras.callbacks.Callback):
     """
-    Feed some starting prompt to the model
-    Predict probabilities for next token
-    Sample next token and add it to the next input
+    Callback to generate text from trained model.
+    1. Feed some starting prompt to the model
+    2. Predict probabilities for next token
+    3. Sample next token and add it to the next input
+    
+    # Arguments
+        max_tokens: Integer, the number of tokens to be generated after prompt.
+        start_tokens: List of integers, the token indices for the starting prompt.
+        index_to_word: List of strings, obatined from TextVectorization layer.
+        temp: Float, dictates the variation during sampling.
+            high temp = less probable tokens are also likely to be sampled
+            low temp = highly probable tokens get picked up
+        print_every: Integer, print after this many epochs.
     """
 
-    def __init__(
-        self, max_tokens, start_tokens, index_to_word, temp=1.0, print_every=5
-    ):
-        """
-        :param max_tokens: number of tokens to be generated
-        :param start_tokens: starting prompt
-        :param index_to_word: Dict mapping token index to word
-        :param temp: temperature dictates the variation during sampling
-                     high temp = less probable tokens are also likely to be sampled
-                     low temp = highly probable tokens get picked up
-        :param print_every: print after this many epochs
-        """
+    def __init__(self, max_tokens, start_tokens, index_to_word, temp=1.0, print_every=5):
         self.max_tokens = max_tokens
         self.start_tokens = start_tokens
         self.index_to_word = index_to_word
@@ -274,40 +291,56 @@ class TextGenerator(keras.callbacks.Callback):
         probas = np.random.multinomial(1, preds, 1)
         return np.argmax(probas)
 
+    def detokenize(self, number):
+        if number == 0:
+            return '[PAD]'
+        if number == 1:
+            return '[OOV]'
+        return self.index_to_word[number-2].decode("utf-8") 
+
     def on_epoch_end(self, epoch, logs=None):
         if (epoch + 1) % self.print_every != 0:
             return
-        print(f"\n\nepoch = {epoch+1}")
+        print(f'\n\nepoch = {epoch+1}')
         num_tokens_generated = 0
         tokens_generated = []
-        while num_tokens_generated < self.max_tokens:
+        while num_tokens_generated <= self.max_tokens:
             pad_len = maxlen - len(self.start_tokens)
-            if pad_len < 0:  # Truncate start token sequence
+            if pad_len < 0:
                 x = self.start_tokens[:maxlen]
-            elif pad_len > 0:  # Pad start token sequence
+            elif pad_len > 0:
                 x = self.start_tokens + [0] * pad_len
             else:
                 x = self.start_tokens
             x = np.array([x])
-            y, _ = self.model.predict(x)
-            sample_token = self.sample_from(y[0][len(self.start_tokens) - 1])
+            assert x.shape == (1,200), x.shape
+            y,_ = self.model.predict(x)
+            sample_token = self.sample_from(y[0][len(self.start_tokens)-1])
             tokens_generated.append(sample_token)
             self.start_tokens.append(sample_token)
             num_tokens_generated = len(tokens_generated)
-        txt = " ".join([index_word[_] for _ in self.start_tokens + tokens_generated])
-        print(f"{txt}\n\n")
+        txt = ' '.join([self.detokenize(_) for _ in self.start_tokens + tokens_generated])
+        print(f'{txt}\n\n')
 
 
-start_prompt = "this film"
-start_tokens = [1] + [word_index[_] + 3 for _ in start_prompt.split()]
+# Tokenize starting prompt
+word_to_index = {}
+for index, word in enumerate(vocab):
+    word_to_index[word.decode('utf-8')] = index + 2  # PAD and OOV at index 0 and 1
+
+start_prompt = 'this movie is'
+start_tokens = [word_to_index.get(_, 1) for _ in start_prompt.split()]
 num_tokens_generated = 20
-text_gen_callback = TextGenerator(num_tokens_generated, start_tokens, index_word)
+text_gen_callback = TextGenerator(num_tokens_generated, start_tokens, vocab)
 
 
 """
 ## Create model
+
+This code should preferably be run on Google Colab TPU runtime.
+With Colab TPUs, each epoch will take 20-25 seconds.
 """
-use_tpu = True
+use_tpu = False
 if use_tpu:
     # Create distribution strategy
     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
@@ -327,5 +360,5 @@ else:
 """
 
 model.fit(
-    x_lm, y_lm, batch_size=64, verbose=2, epochs=50, callbacks=[text_gen_callback]
+    text_ds, verbose=1, steps_per_epoch=781, epochs=15, callbacks=[text_gen_callback]
 )
