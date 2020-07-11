fda9c4a141116bd9ef5a3089d0758018f04f85c0
Abhay Baiju <abhay0198@gmail.com>
2020-07-11 04:17:43 -0700

    Fixes required lines
    Fixes line 300

--- guides/functional_api.py
+++ guides/functional_api.py
@@ -297,7 +297,7 @@ ensemble_model = keras.Model(inputs=inputs, outputs=outputs)
 ### Models with multiple inputs and outputs
 
 The functional API makes it easy to manipulate multiple inputs and outputs.
-This cannot be handled by the `Sequential` API.
+This cannot be handled with the `Sequential` API.
 
 For example, if you're building a system for ranking customer issue tickets by
 priority and routing them to the correct department,
