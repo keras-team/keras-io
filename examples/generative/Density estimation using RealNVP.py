c951bc2e26eb58ea5758b161c1055c4ebe39325d
François Chollet <francois.chollet@gmail.com>
2020-08-11 15:49:31 -0700

    Copyediting

--- examples/generative/Density estimation using RealNVP.py
+++ examples/generative/Density estimation using RealNVP.py
@@ -1,29 +1,31 @@
 """
 Title: Density estimation using Real NVP
 Authors: [Mandolini Giorgio Maria](https://www.linkedin.com/in/giorgio-maria-mandolini-a2a1b71b4/), [Sanna Daniele](https://www.linkedin.com/in/daniele-sanna-338629bb/), [Zannini Quirini Giorgio](https://www.linkedin.com/in/giorgio-zannini-quirini-16ab181a0/)
-Date created: 2020/8/10
-Last modified: 2020/8/10
-Description: Estimating the density distribution of multi-cluster dataset.
+Date created: 2020/08/10
+Last modified: 2020/08/10
+Description: Estimating the density distribution of multi-cluster datasets.
 """
 
 """
 ## Introduction
-The aim of this work consists in mapping a simple distribution - which is easy to sample
-and whose density is simple to estimate - to a more complex one learned from the data;
-these kind of generative models is also knowm as "normalizing flows".
 
-In order to obtain the aforementioned result the model is trained by means of the maximum
-likelihood principle, resorting to the "change of variable formula".
+The aim of this work is to map a simple distribution - which is easy to sample
+and whose density is simple to estimate - to a more complex one learned from the data.
+This kind of generative model is also known as "normalizing flow".
 
-An affine coupling layer function is built. It is created such that its inverse and the determinant
-of the jacobian is easy to obtain (more details in the referenced paper).
+In order to do this, the model is trained via the maximum
+likelihood principle, using the "change of variable formula".
 
-requirements:
+We will use an affine coupling function. We create it such that its inverse, as well as
+the determinant of the Jacobian, are easy to obtain (more details in the referenced paper).
 
-* Tensorflow 2.3.0
-* Tensorflow probability 0.11.0
+**Requirements:**
 
-References:
+* Tensorflow 2.3 or higher
+* Tensorflow probability 0.11.0 or higher
+
+**Reference:**
+
 [Density estimation using Real NVP](https://arxiv.org/pdf/1605.08803.pdf)
 """
 
@@ -32,7 +34,6 @@ References:
 
 """
 
-# libraries to import
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
