8b685b388d2b9d1c6cccaf83b4809bf3e829af95
anish9 <josh.anish1@gmai.com>
2020-08-24 00:50:27 +0530

    Backbone Update

--- examples/vision/IIC_cluster.py
+++ examples/vision/IIC_cluster.py
@@ -1,22 +1,20 @@
 """
-Title: Invariant information clustering 
-Author: [Anish](https://twitter.com/anishhacko)
-Date created: 2020/08/03
-Last modified: 2020/08/13
-Description: Unsupervised information learning
+Title: Invariant Information Clustering(IIC)
+Author: [Anish Josh](https://twitter.com/anishhacko)
+Date created: 2020/08/05
+Last modified: 2020/06/18
+Description: Implementation of unsupervised learning example using IIC algorithm.
 """
 """
 
 
 ## Introduction
-This example helps to get started with unsupervised learning, code implemented here is
-based on [Invariant Information Clustering](https://arxiv.org/pdf/1807.06653.pdf).<br>In
-a nutshell 
-this algorithm tries to maximize the mutal information between two images, most
-importantly we will<br>
-use fit method to implement in keras which makes it more simpler to break and understand
-the algorithm.
-
+This example helps to get started with unsupervised learning methods. Code implemented
+here is
+based on paper [Invariant Information Clustering](https://arxiv.org/abs/1807.06653).In a
+nutshell 
+this algorithm tries to maximize the mutual information between two images. Most
+excitingly we will use fit method to run the experiment.
 """
 
 """
@@ -26,127 +24,116 @@ the algorithm.
 import requests
 import numpy as np
 from glob import glob
-from tqdm import tqdm
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.resnet50 import ResNet50
 
 """
 ## Tuning Paramters
-#### Reading the paper would help in more detail in choosing these parameters.
-1. subheads - total number of subheads for clustering.
-2. classes  - total number classes to classify (guess the value)
-3. aux_classes - Number of auxilary classses for overclusteing(can be ignored during
-inference)
+#### Reading the paper would help in more detail, choosing these parameters.
+1. subheads - Total number of subheads for clustering.
+2. main_classes  - Total number classes to classify (guess the value).
+3. auxilary_classes - Number of auxilary classses for overclusteing(can be ignored during
+inference).
 """
 
-sub_heads = 3
-classes_ = 4
-aux_classes = 8
-imh, imw = 224, 224
-BATCH_SIZE = 8
-BUFFER = 1024
-LR_RATE = 1e-5
-EPOCHS = 180
+sub_heads = 4
+main_classes = 6
+auxilary_classes = 10
+image_h, image_w = 256, 256
+batch_size = 8
+learning_rate = 5e-6
+epochs = 200
+dataset = "toy_data"
+logdir = "./logs" #tensorboard directory path
 
 """
-## Download the dataset.
-##### Download and unzip it
+## Downloading Dataset
+##### Download and unzip it.
 """
 
-"""shell
-!wget https://github.com/anish9/Tutorials/raw/master/toy.zip
-!unzip toy.zip
-"""
+# !wget https://github.com/anish9/Tutorials/raw/master/toy_data.zip
+# !unzip toy_data.zip
 
 """
  ## Build Data Pipeline
- It just requires original image and its augumented version,
- apply some random transformations to generate real and its augumented version.
 """
 
-
-def x_gen(im):
-    # random crops
-    if np.random.randint(1, 8, 1)[0] > 4:
-        image = tf.image.random_crop(im, (imh // 2, imw // 2, 3))
-    if np.random.randint(1, 8, 1)[0] > 4:
-        image = tf.image.random_brightness(im, 0.4)  # random brightness
-        image = tf.image.rot90(image, k=1)
-    if np.random.randint(1, 8, 1)[0] > 4:
-        image = tf.image.random_contrast(im, 0.2, 0.7)  # random contrast
-        image = tf.image.rot90(image, k=3)
-    else:
-        image = tf.image.random_flip_up_down(im)  # random flips vertical
-    image = tf.image.resize(im, (imh, imw))
-    return image
-
-
-def read_file(im):
-    image = tf.io.read_file(im)
-    image = tf.io.decode_image(image, channels=3, expand_animations=False)
-    image = tf.image.resize(image, (imh * 2, imw * 2))
-    image_ = x_gen(image)
-    image = tf.image.resize(image, (imh, imw))
-    return image / 255.0, image_ / 255.0
-
-
-image_path = sorted(glob("toy/*"))
-TRAIN = tf.data.Dataset.from_tensor_slices((image_path))
-TRAIN = TRAIN.map(read_file, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-TRAIN = TRAIN.batch(BATCH_SIZE, drop_remainder=True).shuffle(BUFFER)
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    dataset, label_mode=None, batch_size=batch_size, image_size=(image_h, image_w)
+)
+train_ds = train_ds.prefetch(32).repeat()
+test_path = sorted(glob(dataset + "/copys/*"))
 
 """
-## Visualize the data
+## Visualizing The Loaded Dataset
 """
 
 plt.figure(figsize=(12, 8))
-for batch in TRAIN.take(1):
-    img = batch[0]
-    for w in range(1, BATCH_SIZE + 1):
+for img in train_ds.take(1):
+    for w in range(1, batch_size + 1):
         plt.subplot(4, 4, w)
-        image = img[w - 1, :, :, :] * 255.0
+        image = img[w - 1, :, :, :]
         image = image.numpy().astype(np.uint8)
         plt.imshow(image)
+        plt.axis("off")
 
 """
-## Building backbone network and cluster model
-VGG 16 is used as backbone network
+## Building Model: 
+#### 1 - Defining Augumentation Layers (only target image is augumented, source is
+scaled).
+#### 2 - Defining Backbone Network.
+#### 3 - Defining Clustering Network.
 """
 
+# Source augument has only normalizing effect.
+source_augument = keras.Sequential(
+    [keras.layers.experimental.preprocessing.Rescaling(1.0 / 255),]
+)
 
-def VGG16_bb(subheads, classes, aux_branch=True, aux_classes=15):
-    base_full = VGG16(include_top=False, input_shape=(imh, imw, 3))
+# Target augment can contain multiple augumentations with respect to the dataset.
+target_augment = keras.Sequential(
+    [
+        keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+        keras.layers.experimental.preprocessing.RandomRotation(0.1),
+        keras.layers.experimental.preprocessing.RandomZoom(0.4),
+        keras.layers.experimental.preprocessing.Rescaling(1.0 / 255),
+    ]
+)
+
+# Backbone Network
+def resnet_backbone(subheads, classes, auxilary_branch=True, auxilary_classes=10):
+    base_full = ResNet50(include_top=False, input_shape=(image_h, image_w, 3))
     base_outp = base_full.output
     gpool = keras.layers.GlobalAveragePooling2D()(base_outp)
     outmaps = []
     auxmaps = []
     for w in range(subheads):
-        outpu = keras.layers.Dense(classes, activation="softmax")(gpool)
-        outmaps.append(outpu)
-    if aux_branch:
-
+        main_out = keras.layers.Dense(classes, activation="softmax")(gpool)
+        outmaps.append(main_out)
+    if auxilary_branch:
         for w in range(subheads):
-            auxou = keras.layers.Dense(aux_classes, activation="softmax")(gpool)
-            auxmaps.append(auxou)
-        fmodel = keras.models.Model(base_full.input, [outmaps, auxmaps], name="IIC_db")
-        return fmodel
+            aux_out = keras.layers.Dense(auxilary_classes, activation="softmax")(gpool)
+            auxmaps.append(aux_out)
+        fmodel = keras.models.Model(
+            base_full.input, [outmaps, auxmaps], name="IIC_aux_node"
+        )
+        return fmodel, auxilary_branch
 
     else:
-        fmodel = keras.models.Model(base_full.input, outmaps, name="IIC_sb")
-        return fmodel
+        fmodel = Model(base_full.input, outmaps, name="IIC_main_node")
+        return fmodel, auxilary_branch
 
 
+# Clustering Network
 class cluster_IIC(keras.models.Model):
-    def __init__(self, backbone, class_, aux_class, aux=False):
+    def __init__(self, backbone, auxilary_flag=False):
         super(cluster_IIC, self).__init__()
         self.backbone = backbone
-        self.aux = aux
-        self.class_ = class_
-        self.aux_class = aux_class
+        self.auxilary = auxilary_flag
 
     def compile(self, optimizer, loss):
         super(cluster_IIC, self).compile()
@@ -154,9 +141,9 @@ class cluster_IIC(keras.models.Model):
         self.loss_fn = loss
 
     def train_step(self, x):
-        xin, xin_ = x
+        xin, xin_ = source_augument(x), target_augment(x)
         with tf.GradientTape() as t:
-            if self.aux:
+            if self.auxilary:
                 # trains with auxilary branch combined
                 m1, a1 = self.backbone(xin)
                 m2, a2 = self.backbone(xin_)
@@ -174,7 +161,7 @@ class cluster_IIC(keras.models.Model):
 
 
 """
-## Building custom loss function
+## Building Loss Function
 """
 
 
@@ -186,14 +173,12 @@ class IIC_loss(keras.losses.Loss):
 
     def call(self, x, x_):
         c = tf.shape(x)[-1]  # class labels
-        epsilon = self.epsilon
-        lamb = self.lamb
         i = tf.reshape(x, (-1, c, 1))
         j = tf.reshape(x_, (-1, 1, c))
         j_prob = tf.matmul(i, j)  # compute joint probability
         j_prob = tf.reduce_sum(j_prob, axis=0)  # sum the sub_heads
         j_prob = (j_prob + tf.transpose(j_prob)) / 2  # impose invariant compute
-        j_prob = tf.clip_by_value(j_prob, epsilon, 1e9)
+        j_prob = tf.clip_by_value(j_prob, self.epsilon, 1e9)
         j_prob /= tf.reduce_sum(j_prob)
         pi_ = tf.reduce_sum(j_prob, axis=0)
         pi_ = tf.reshape(pi_, (-1, 1))
@@ -208,8 +193,8 @@ class IIC_loss(keras.losses.Loss):
 
 
 """
-## Custom callback
-custom callback to save only inference model
+## Custom Callback
+Custom callback to save only Inference Model
 """
 
 
@@ -227,45 +212,47 @@ class custom_checkpoint(keras.callbacks.Callback):
 
 
 """
-## Initialization and training
+## Initialization and Training
 """
 
-backbone = VGG16_bb(subheads=sub_heads, classes=classes_, aux_classes=aux_classes)
-clu = cluster_IIC(backbone, classes_, aux_classes, aux=True)
-loss_c = IIC_loss()
-
-logdir = "./logs"
-tb = keras.callbacks.TensorBoard(logdir)
+backbone, auxilary_flag = resnet_backbone(
+    subheads=sub_heads,
+    classes=main_classes,
+    auxilary_branch=True,
+    auxilary_classes=auxilary_classes,
+)
+cluster_model = cluster_IIC(backbone, auxilary_flag=auxilary_flag)
+loss = IIC_loss()
+tensor_board = keras.callbacks.TensorBoard(logdir)
 ckpt = custom_checkpoint(backbone)
-
-optim = tf.keras.optimizers.Adam(LR_RATE)
-clu.compile(optimizer=optim, loss=loss_c)
-history = clu.fit(
-    TRAIN,
-    batch_size=BATCH_SIZE,
-    epochs=5,
-    steps_per_epoch=len(image_path) // BATCH_SIZE,
-    callbacks=[tb, ckpt],
+optimizer = tf.keras.optimizers.Adam(learning_rate)
+cluster_model.compile(optimizer=optimizer, loss=loss)
+history = cluster_model.fit(
+    train_ds,
+    batch_size=batch_size,
+    epochs=epochs,
+    steps_per_epoch=len(test_path) // batch_size,
+    callbacks=[tensor_board, ckpt],
 )
 
 """
-## Infernce and Visualization
- This section holds prediction time clustering and visualization codes
+## Inference and Visualization
+ This section holds codes for prediction time clustering and visualization.
 """
 
 
 class infernce:
     def __init__(self, model, path, mode):
-        self.model = model  # bakcbone as infernce model
-        self.path = path  # image_dir path
-        self.mode = mode  # predict using aux or main branch
+        self.model = model  # Load bakcbone as infernce model
+        self.path = path  # Image_directory path
+        self.mode = mode  # Predict using auxilary or main branch
         self.clusters = defaultdict(list)
 
     def predict(self, image_name):
         """for each image predict their clusters"""
         if self.mode not in {"aux", "main"}:
             raise ValueError("allowed modes {aux or main}")
-        im = load_img(image_name, target_size=(imh, imw))
+        im = load_img(image_name, target_size=(image_h, image_w))
         im = img_to_array(im) / 255.0
         im = np.expand_dims(im, axis=0)
         pred1, pred2 = self.model.predict(im)
@@ -273,7 +260,7 @@ class infernce:
             predf = np.array(pred2)
         if self.mode == "main":
             predf = np.array(pred1)
-        predf = np.sum(predf, axis=0)  # sum in branch axis
+        predf = np.sum(predf, axis=0)
         predf = np.argmax(predf, axis=-1)
         return predf[0]
 
@@ -291,9 +278,9 @@ class infernce:
         predicted cluster branch values"""
         predictions = self.clusters[cluster_id]
         plt.figure(figsize=(8, 12))
-        for w in range(1, BATCH_SIZE + 1):
+        for w in range(1, batch_size + 1):
             try:
-                image = load_img(predictions[w], target_size=(imh, imw))
+                image = load_img(predictions[w], target_size=(image_h, image_w))
                 image = img_to_array(image, dtype=np.uint8)
                 plt.subplot(4, 4, w)
                 plt.imshow(image)
@@ -301,13 +288,13 @@ class infernce:
                 None
 
 
-cluster_inf = infernce(backbone, image_path, "main")
+cluster_inf = infernce(backbone, test_path, "main")  #
 keys_out = cluster_inf.get_cluster_keys()
 
 """
-## Visualize the clusters using predicted cluster keys
+### Visualize using predicted cluster keys
 """
 
-cluster_inf.visualize_cluster(0)
-cluster_inf.visualize_cluster(1)
 cluster_inf.visualize_cluster(3)
+# cluster_inf.visualize_cluster(1)
+# cluster_inf.visualize_cluster(7)
