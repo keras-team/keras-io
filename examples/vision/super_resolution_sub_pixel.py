8d6d43761366563edda02ba387517d1896c05b45
François Chollet <francois.chollet@gmail.com>
2020-08-28 10:26:05 -0700

    Fix imports

--- examples/vision/super_resolution_sub_pixel.py
+++ examples/vision/super_resolution_sub_pixel.py
@@ -30,10 +30,10 @@ import numpy as np
 
 from tensorflow import keras
 from tensorflow.keras import layers
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import array_to_img
-from keras.preprocessing.image import img_to_array
-from keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import array_to_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 from IPython.display import display
 
