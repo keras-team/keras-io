6d71921c70a49369be2e5f2f66e5469ee5c863a5
amifunny <hemantsingh.buenodev@gmail.com>
2020-09-04 13:33:09 +0530

    Enable the BatchNorm

--- examples/rl/ddpg_pendulum.py
+++ examples/rl/ddpg_pendulum.py
@@ -187,9 +187,9 @@ class Buffer:
         # Training and updating Actor & Critic networks.
         # See Pseudo Code.
         with tf.GradientTape() as tape:
-            target_actions = target_actor(next_state_batch)
-            y = reward_batch + gamma * target_critic([next_state_batch, target_actions])
-            critic_value = critic_model([state_batch, action_batch])
+            target_actions = target_actor(next_state_batch,training=True)
+            y = reward_batch + gamma * target_critic([next_state_batch, target_actions],training=True)
+            critic_value = critic_model([state_batch, action_batch],,training=True)
             critic_loss = tf.math.reduce_mean(tf.math.square(y - critic_value))
 
         critic_grad = tape.gradient(critic_loss, critic_model.trainable_variables)
@@ -199,7 +199,7 @@ class Buffer:
 
         with tf.GradientTape() as tape:
             actions = actor_model(state_batch)
-            critic_value = critic_model([state_batch, actions])
+            critic_value = critic_model([state_batch, actions],training=True)
             # Used `-value` as we want to maximize the value given
             # by the critic for our actions
             actor_loss = -tf.math.reduce_mean(critic_value)
@@ -293,7 +293,7 @@ exploration.
 
 
 def policy(state, noise_object):
-    sampled_actions = tf.squeeze(actor_model(state))
+    sampled_actions = tf.squeeze(actor_model(state,training=True))
     noise = noise_object()
     # Adding noise to action
     sampled_actions = sampled_actions.numpy() + noise
