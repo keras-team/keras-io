ab946f578ac1d90ff8bbaa986d5f7a929091d615
Amit Portnoy <1131991+amitport@users.noreply.github.com>
2020-10-12 10:38:58 +0300

    fix order when creating validation from train

--- guides/writing_a_training_loop_from_scratch.py
+++ guides/writing_a_training_loop_from_scratch.py
@@ -152,19 +152,22 @@ loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
 val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
 
-# Prepare the training dataset.
+# Prepare the data.
 batch_size = 64
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
-# Prepare the validation dataset.
 # Reserve 10,000 samples for validation.
 x_val = x_train[-10000:]
 y_val = y_train[-10000:]
 x_train = x_train[:-10000]
 y_train = y_train[:-10000]
+
+# Prepare the training dataset.
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+
+# Prepare the validation dataset.
 val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_dataset = val_dataset.batch(64)
+val_dataset = val_dataset.batch(batch_size)
 
 """
 Here's our training & evaluation loop:
