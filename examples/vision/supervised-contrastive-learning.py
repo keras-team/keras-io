e213499dc82c1981c622d74397f809f9f0e24862
Khalid Salama <khalid.m.salama@gmail.com>
2020-11-12 19:50:07 +0000

    Update supervised-contrastive-learning.py

--- examples/vision/supervised-contrastive-learning.py
+++ examples/vision/supervised-contrastive-learning.py
@@ -8,12 +8,17 @@ Description: Using supervised contrastive learning for image classification.
 
 ## Introduction
 
-[Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362) (Prannay Khosla et al.) is a training methodology that outperforms cross-entropy on supervised learning tasks. 
+[Supervised Contrastive Learning](https://arxiv.org/abs/2004.11362)
+(Prannay Khosla et al.) is a training methodology that outperforms
+ cross-entropy on supervised learning tasks.
 
-Essentially, training an image classification model with Supervised Contrastive Learning is performed in two phases: 
+Essentially, training an image classification model with Supervised Contrastive
+Learning is performed in two phases:
 
-1. Pre-training an encoder to generate feature vectors for input images such that feature vectors of images in the same class will be more similar compared feature vectors of images in other classes.
-2. Training a classifier on top of the freezed encoder.
+1. Pre-training an encoder to generate feature vectors for input images such
+that feature vectors of images in the same class will be more similar compared
+feature vectors of images in other classes.
+2. Training a classifier on top of the frozen encoder.
 
 ## Setup
 """
@@ -22,11 +27,14 @@ Essentially, training an image classification model with Supervised Contrastive 
 pip install -q tensorflow-addons
 """
 
+!pip install -q tensorflow-addons
+
 import tensorflow as tf
 import tensorflow_addons as tfa
 import numpy as np
-import keras
+import keras 
 from keras import layers
+from keras.preprocessing.image import ImageDataGenerator
 
 """## Prepare the data"""
 
@@ -36,55 +44,56 @@ input_shape = (32, 32, 3)
 # Load the train and test data splits
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
+# Display shapes of train and test datasets
+print(f'x_train shape: {x_train.shape} - y_train shape: {y_train.shape}')
+print(f'x_test shape: {x_test.shape} - y_test shape: {y_test.shape}')
 
-print("x_train shape:", x_train.shape)
-print("y_train shape:", y_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+"""## Using image data augmentation"""
+
+data_generator = ImageDataGenerator(
+  featurewise_center=True,
+  featurewise_std_normalization=True,
+  rotation_range=20,
+  horizontal_flip=True,
+  width_shift_range=0.2,
+  height_shift_range=0.2
+)
+
+data_generator.fit(x_train)
 
 """## Build the encoder model
 
-The encoder model takes the image as an input and produce a 128-dimension feature vector.
+The encoder model takes the image as an input and produce a 128-dimension
+feature vector.
 """
 
 def create_encoder():
+  densenet = keras.applications.ResNet50V2(
+      include_top=False,
+      weights=None,
+      input_shape=input_shape,
+      pooling='avg')
+
   inputs = keras.Input(shape=input_shape)
-
-  x = layers.Conv2D(32, 3)(inputs)
-  x = layers.BatchNormalization()(x)
-  x = layers.Activation("relu")(x)
-  x = layers.Conv2D(64, 3)(x)
-  x = layers.BatchNormalization()(x)
-  x = layers.Activation("relu")(x)
-  x = layers.MaxPooling2D(pool_size=2)(x)
-
-  x = layers.Conv2D(128, 3)(x)
-  x = layers.BatchNormalization()(x)
-  x = layers.Activation("relu")(x)
-  x = layers.Conv2D(256, 3)(x)
-  x = layers.BatchNormalization()(x)
-  x = layers.Activation("relu")(x)
-  x = layers.MaxPooling2D(pool_size=2)(x)
-
-  outputs = layers.GlobalAveragePooling2D()(x)
-
+  outputs = densenet(inputs)
   model = keras.Model(inputs=inputs, outputs=outputs, name="cifar10-encoder")
   return model
 
 encoder = create_encoder()
 encoder.summary()
 
-batch_size = 256
-num_epochs = 25
+learning_rate = 0.001
+batch_size = 265
+hidden_units = 512
+projection_units = 128
+num_epochs = 50
 dropout_rate = 0.5
 temperature = 0.05
 
 """## Build the classification model
 
-The classification model adds a fully-connected layer on top of the encoder, plus a softmax layer with the target classes.
+The classification model adds a fully-connected layer on top of the encoder,
+plus a softmax layer with the target classes.
 """
 
 def create_classifier(encoder, trainable=True):
@@ -94,11 +103,16 @@ def create_classifier(encoder, trainable=True):
 
   inputs = keras.Input(shape=input_shape)
   features = encoder(inputs)
+  features = layers.BatchNormalization()(features)
+  features = layers.Dropout(dropout_rate)(features)
+  features = layers.Dense(hidden_units, 'relu')(features)
+  features = layers.BatchNormalization()(features)
   features = layers.Dropout(dropout_rate)(features)
   outputs =  layers.Dense(num_classes, activation="softmax")(features)
+  
   model = keras.Model(inputs=inputs, outputs=outputs, name="cifar10-classifier")
   model.compile(
-    optimizer=keras.optimizers.Adam(),
+    optimizer=keras.optimizers.Adam(learning_rate),
     loss=keras.losses.SparseCategoricalCrossentropy(),
     metrics=keras.metrics.SparseCategoricalAccuracy()
   )
@@ -106,7 +120,9 @@ def create_classifier(encoder, trainable=True):
 
 """## Experiment 1: Train the baseline classification model 
 
-In this experiment, a baseline classifier is trained normally, i.e., the encoder and the classifier parts are trained together as a single model to minimize cross-entropy loss.
+In this experiment, a baseline classifier is trained normally, i.e., the
+encoder and the classifier parts are trained together as a single model
+to minimize cross-entropy loss.
 """
 
 encoder = create_encoder()
@@ -114,18 +130,21 @@ classifier = create_classifier(encoder)
 classifier.summary()
 
 history = classifier.fit(
-    x=x_train, 
-    y=y_train, 
-    batch_size=batch_size, 
-    epochs=num_epochs)
+  data_generator.flow(x_train, y_train, batch_size),
+  epochs=num_epochs)
 
-accuracy = classifier.evaluate(x_test, y_test)[1]
+accuracy = classifier.evaluate(data_generator.flow(x_test, y_test))[1]
 print(f'Test accuracy: {round(accuracy * 100, 2)}%')
 
-"""We get to ~70.5% test accuracy.
+"""We get to ~76.5% test accuracy.
 
 ## Experiment 2: Use supervised contrastive learning
-In this experiment, the model is trained in two phases. In the first phase, the encoder is pretrained to optimize the supervised contrastive loss, described in [Prannay Khosla et al.](https://arxiv.org/abs/2004.11362). In the second phase, the classifier is trained using the trained encoder with its weights freezed; only the weights of fully-connected layers with the softmax are optimized.
+In this experiment, the model is trained in two phases. In the first phase,
+the encoder is pretrained to optimize the supervised contrastive loss,
+described in [Prannay Khosla et al.](https://arxiv.org/abs/2004.11362).
+In the second phase, the classifier is trained using the trained encoder with
+its weights freezed; only the weights of fully-connected layers with the
+softmax are optimized.
 
 ### 1. Supervised contrastive learning loss function
 """
@@ -147,31 +166,41 @@ class SupervisedContrastiveLoss(keras.losses.Loss):
     )
     return tfa.losses.npairs_loss(tf.squeeze(labels), logits)
 
+def add_projection_head(encoder):
+  inputs = keras.Input(shape=input_shape)
+  features = encoder(inputs)
+  outputs = layers.Dense(projection_units, 'relu')(features)
+  outputs = layers.BatchNormalization()(features)
+  model = keras.Model(inputs=inputs, outputs=outputs, 
+                      name='cifar-encoder_with_projection-head')
+  return model
+
 """### 2. Pretrain the encoder"""
 
 encoder = create_encoder()
-encoder.compile(
-    optimizer=keras.optimizers.Adam(),
+
+encoder_with_projection_head = add_projection_head(encoder)
+encoder_with_projection_head.compile(
+    optimizer=keras.optimizers.Adam(learning_rate),
     loss=SupervisedContrastiveLoss(temperature)
 )
 
-history = encoder.fit(
-    x=x_train, 
-    y=y_train, 
-    batch_size=batch_size, 
-    epochs=num_epochs)
+encoder_with_projection_head.summary()
 
-"""### 3. Train the classifier with the freezed encoder"""
+history = encoder_with_projection_head.fit(
+  data_generator.flow(x_train, y_train, batch_size),
+  epochs=num_epochs)
+
+"""### 3. Train the classifier with the frozen encoder"""
 
 classifier = create_classifier(encoder, trainable=False)
-history = classifier.fit(
-    x=x_train, 
-    y=y_train, 
-    batch_size=batch_size, 
-    epochs=num_epochs)
 
-accuracy = classifier.evaluate(x_test, y_test)[1]
+history = classifier.fit(
+  data_generator.flow(x_train, y_train, batch_size), 
+  epochs=num_epochs,
+  )
+
+accuracy = classifier.evaluate(data_generator.flow(x_test, y_test))[1]
 print(f'Test accuracy: {round(accuracy * 100, 2)}%')
 
-"""We get to ~79.1% test accuracy."""
-
+"""We get to ~81.3% test accuracy."""
