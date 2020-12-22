8ce54bd7dafd9edb7ccbc11eec90048e29327902
Khalid Salama <khalid.m.salama@gmail.com>
2020-12-22 14:19:00 +0000

    Create deep_neural_decision_forests.py

---
+++ examples/structured_data/deep_neural_decision_forests.py
@@ -0,0 +1,444 @@
+"""
+Title: Classification with Neural Decision Forests
+Author: [Khalid Salama](https://www.linkedin.com/in/khalid-salama-24403144/)
+Date created: 2021/01/15
+Last modified: 2021/01/15
+Description: Structured data classification with Deep Neural Decision Forests.
+"""
+
+"""
+## Introduction
+
+This example provides an implementation of the [Deep Neural Decision Forest](https://www.cv-foundation.org/openaccess/content_iccv_2015/papers/Kontschieder_Deep_Neural_Decision_ICCV_2015_paper.pdf)
+model, introduced by P. Kontschieder et al., for structured data classification.
+This model introduced a stochastic and differentiable decision tree model to unity
+classification trees with the deep representation learning, in a joint training routine.
+
+## The dataset
+
+This example uses the [United States Census Income
+Dataset](https://archive.ics.uci.edu/ml/datasets/census+income) provided by the
+[UC Irvine Machine Learning
+Repository](https://archive.ics.uci.edu/ml/index.php). The task is binary classification
+to determine whether a person makes over 50K a year.
+
+The dataset includes 48,842 instances with 14 input features, 5 of which numerical,
+and the other 9 are categorical.
+"""
+
+"""
+## Setup
+"""
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from tensorflow import keras
+from tensorflow.keras import layers
+import math
+
+"""
+## Prepare the data
+"""
+
+CSV_HEADER = [
+    "age",
+    "workclass",
+    "fnlwgt",
+    "education",
+    "education_num",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "gender",
+    "capital_gain",
+    "capital_loss",
+    "hours_per_week",
+    "native_country",
+    "income_bracket",
+]
+
+train_data_url = (
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+)
+train_data = pd.read_csv(train_data_url, header=None, names=CSV_HEADER)
+
+test_data_url = (
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test"
+)
+test_data = pd.read_csv(test_data_url, header=None, names=CSV_HEADER)
+
+print(f"Train dataset shape: {train_data.shape}")
+print(f"Test dataset shape: {test_data.shape}")
+
+train_data.head().T
+
+"""
+We remove the first record as it is not a valid data example, and we remove a tailing
+'dot' in the class labels.
+"""
+
+test_data = test_data[1:]
+test_data.income_bracket = test_data.income_bracket.apply(
+    lambda value: value.replace(".", "")
+)
+
+"""
+Now we store the train and test data splits locally to CSV files.
+"""
+
+train_data_file = "train_data.csv"
+test_data_file = "test_data.csv"
+
+train_data.to_csv(train_data_file, index=False, header=False)
+test_data.to_csv(test_data_file, index=False, header=False)
+
+"""
+## Define dataset metadata
+"""
+
+NUMERIC_FEATURE_NAMES = [
+    "age",
+    "education_num",
+    "capital_gain",
+    "capital_loss",
+    "hours_per_week",
+]
+
+CATEGORICAL_FEATURES_WITH_VOCABULARY = {
+    "workclass": list(train_data["workclass"].unique()),
+    "education": list(train_data["education"].unique()),
+    "marital_status": list(train_data["marital_status"].unique()),
+    "occupation": list(train_data["occupation"].unique()),
+    "relationship": list(train_data["relationship"].unique()),
+    "race": list(train_data["race"].unique()),
+    "gender": list(train_data["gender"].unique()),
+    "native_country": list(train_data["native_country"].unique()),
+}
+
+IGNORE_COLUMN_NAMES = ["fnlwgt"]
+
+CATEGORICAL_FEATURE_NAMES = list(CATEGORICAL_FEATURES_WITH_VOCABULARY.keys())
+
+FEATURE_NAMES = NUMERIC_FEATURE_NAMES + CATEGORICAL_FEATURE_NAMES
+
+COLUMN_DEFAULTS = [
+    [0.0] if feature_name in NUMERIC_FEATURE_NAMES + IGNORE_COLUMN_NAMES else ["NA"]
+    for feature_name in CSV_HEADER
+]
+
+TARGET_FEATURE_NAME = "income_bracket"
+
+TARGET_LABELS = [" <=50K", " >50K"]
+
+"""
+## Create tf.data.Dataset for training and evaluation
+
+We create an input function to read and parse the file, and convert features and labels
+into a [`tf.data.Dataset`](https://www.tensorflow.org/guide/datasets)
+for training or evaluation. We also preprocess the input by mapping the target label
+to an index.
+"""
+
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+
+taget_label_lookup = StringLookup(
+    vocabulary=TARGET_LABELS, mask_token=None, num_oov_indices=0
+)
+
+
+def get_dataset_from_csv(csv_file_path, num_epochs=None, shuffle=False, batch_size=128):
+    def process(features, target):
+        target_index = taget_label_lookup(target)
+        return features, target_index
+
+    dataset = tf.data.experimental.make_csv_dataset(
+        csv_file_path,
+        batch_size=batch_size,
+        column_names=CSV_HEADER,
+        column_defaults=COLUMN_DEFAULTS,
+        label_name=TARGET_FEATURE_NAME,
+        num_epochs=num_epochs,
+        header=False,
+        na_value="?",
+        shuffle=shuffle,
+    ).map(process)
+
+    return dataset
+
+
+"""
+## Create model inputs
+"""
+
+
+def create_model_inputs():
+    inputs = {}
+    for feature_name in FEATURE_NAMES:
+        if feature_name in NUMERIC_FEATURE_NAMES:
+            inputs[feature_name] = layers.Input(
+                name=feature_name, shape=(), dtype=tf.float32
+            )
+        else:
+            inputs[feature_name] = layers.Input(
+                name=feature_name, shape=(), dtype=tf.string
+            )
+    return inputs
+
+
+"""
+## Encode input features
+"""
+
+from tensorflow.keras.layers.experimental.preprocessing import CategoryEncoding
+from tensorflow.keras.layers.experimental.preprocessing import StringLookup
+
+
+def encode_inputs(inputs, use_embedding=False):
+    encoded_features = []
+    for feature_name in inputs:
+        if feature_name in CATEGORICAL_FEATURE_NAMES:
+            vocabulary = CATEGORICAL_FEATURES_WITH_VOCABULARY[feature_name]
+            # Create a lookup to convert a string values to an integer indices.
+            index = StringLookup(
+                vocabulary=vocabulary, mask_token=None, num_oov_indices=0
+            )
+            # Convert the string input values into integer indices.
+            value_index = index(inputs[feature_name])
+            if use_embedding:
+                embedding_dims = int(math.sqrt(len(vocabulary)))
+                # Create an embedding layer with the specified dimensions.
+                embedding_ecoder = layers.Embedding(
+                    input_dim=len(vocabulary), output_dim=embedding_dims
+                )
+                # Convert the index values to embedding representations.
+                encoded_feature = embedding_ecoder(value_index)
+            else:
+                # Create a one-hot encoder.
+                onehot_encoder = CategoryEncoding(output_mode="binary")
+                onehot_encoder.adapt(index(vocabulary))
+                # Convert the index values to a one-hot representation.
+                encoded_feature = onehot_encoder(value_index)
+        else:
+            # Use the numerical features as-is.
+            encoded_feature = inputs[feature_name]
+            if inputs[feature_name].shape[-1] is None:
+                encoded_feature = tf.expand_dims(encoded_feature, -1)
+
+        encoded_features.append(encoded_feature)
+
+    encoded_features = layers.concatenate(encoded_features)
+    return encoded_features
+
+
+"""
+## Compile, train, and evaluate the model
+"""
+
+train_size = 32561
+learning_rate = 0.01
+batch_size = 265
+num_epochs = 10
+
+train_steps_per_epoch = train_size // batch_size
+hidden_units = [64, 64]
+
+
+def run_experiment(model):
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=learning_rate),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=[keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    print("Start training the model...")
+    train_dataset = get_dataset_from_csv(
+        train_data_file, shuffle=True, batch_size=batch_size
+    )
+
+    model.fit(train_dataset, epochs=num_epochs, steps_per_epoch=train_steps_per_epoch)
+    print("Model training finished")
+
+    print("Evaluating the model on the test data...")
+    test_dataset = get_dataset_from_csv(
+        test_data_file, num_epochs=1, batch_size=batch_size
+    )
+
+    _, accuracy = model.evaluate(test_dataset)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+
+
+"""
+## Deep Neural Decision Tree
+"""
+
+
+class NeuralDecisionTree(keras.Model):
+    def __init__(self, depth, num_features, used_features_rate, num_classes):
+        super(NeuralDecisionTree, self).__init__()
+
+        self.depth = depth
+        self.num_leaves = 2 ** depth
+        self.num_classes = num_classes
+
+        # Create a mask for the randomly selected features.
+        num_used_features = int(num_features * used_features_rate)
+        one_hot = np.eye(num_features)
+        sampled_feature_indicies = np.random.choice(
+            np.arange(num_features), num_used_features, replace=False
+        )
+        self.used_features_mask = one_hot[sampled_feature_indicies]
+
+        # Initialise the weights of the classes in leaves.
+        self.pi = tf.Variable(
+            initial_value=tf.random_normal_initializer()(
+                shape=[self.num_leaves, self.num_classes]
+            ),
+            dtype="float32",
+            trainable=True,
+        )
+
+        # Initialise the stochastic routing layer.
+        self.decision_fn = layers.Dense(
+            units=self.num_leaves, activation="sigmoid", name="decision"
+        )
+
+    def call(self, features):
+
+        batch_size = tf.shape(features)[0]
+
+        # Apply the feature mask to the input features.
+        features = tf.matmul(
+            features, self.used_features_mask, transpose_b=True
+        )  # [batch_size, num_used_features]
+        # Compute the logits of the classes in the leaves.
+        decisions = tf.expand_dims(
+            self.decision_fn(features), axis=2
+        )  # [batch_size, num_leaves, 1]
+        # Concatenate the logits with their complements.
+        decisions = layers.concatenate(
+            [decisions, 1 - decisions], axis=2
+        )  # [batch_size, num_leaves, 2]
+
+        mu = tf.ones([batch_size, 1, 1])
+
+        begin_idx = 1
+        end_idx = 2
+        # Traverse the tree in breadth-first order.
+        for level in range(self.depth):
+            mu = tf.reshape(mu, [batch_size, -1, 1])  # [batch_size, 2 ** level, 1]
+            mu = tf.tile(mu, (1, 1, 2))  # [batch_size, 2 ** level, 2]
+            level_decisions = decisions[
+                :, begin_idx:end_idx, :
+            ]  # [batch_size, 2 ** level, 2]
+            mu = mu * level_decisions  # [batch_size, 2**level, 2]
+            begin_idx = end_idx
+            end_idx = begin_idx + 2 ** (level + 1)
+
+        mu = tf.reshape(mu, [batch_size, self.num_leaves])  # [batch_size, num_leaves]
+        probabilities = keras.activations.softmax(self.pi)  # [num_leaves, num_classes]
+        outputs = tf.matmul(mu, probabilities)  # [batch_size, num_classes]
+        return outputs
+
+
+"""
+## Experiment 1: train a tree model
+In this experiment, we train a single neural decision tree model, where we use all
+the input features.
+"""
+
+num_trees = 10
+depth = 10
+used_features_rate = 1.0
+num_classes = len(TARGET_LABELS)
+
+
+def create_tree_model():
+    inputs = create_model_inputs()
+    features = encode_inputs(inputs, use_embedding=True)
+    features = layers.BatchNormalization()(features)
+    num_features = features.shape[1]
+
+    tree = NeuralDecisionTree(depth, num_features, used_features_rate, num_classes)
+
+    outputs = tree(features)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+tree_model = create_tree_model()
+keras.utils.plot_model(tree_model, show_shapes=True)
+
+run_experiment(tree_model)
+
+"""
+You should achieve around 85.19% accuracy on the test data.
+"""
+
+"""
+## Deep Neural Decision Forest
+
+The neural decision forest model consists of a set of neural decision trees that are
+trained simultaneously. The output of the forest model is the average outputs of its trees.
+"""
+
+
+class NeuralDecisionForest(keras.Model):
+    def __init__(self, num_trees, depth, num_features, used_features_rate, num_classes):
+        super(NeuralDecisionForest, self).__init__()
+        self.trees = []
+        for _ in range(num_trees):
+            self.trees.append(
+                NeuralDecisionTree(depth, num_features, used_features_rate, num_classes)
+            )
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        outputs = tf.zeros([batch_size, num_classes])
+
+        for tree in self.trees:
+            outputs += tree(inputs)
+
+        outputs /= len(self.trees)
+        return outputs
+
+
+"""
+## Experiment 2: train a forest model
+
+In this experiment, we train neural decision forest, which consists of `num_trees` trees,
+and each tree uses randomly selected 50% of the input features. You can control the number
+of features to be used in each tree by setting the `used_features_rate` variable.
+In addition, we set the depth to 5 instead of 10 that was used in the previous experiment.
+"""
+
+num_trees = 50
+depth = 5
+used_features_rate = 0.5
+
+
+def create_forest_model():
+    inputs = create_model_inputs()
+    features = encode_inputs(inputs, use_embedding=True)
+    features = layers.BatchNormalization()(features)
+    num_features = features.shape[1]
+
+    forest_model = NeuralDecisionForest(
+        num_trees, depth, num_features, used_features_rate, num_classes
+    )
+
+    outputs = forest_model(features)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+forest_model = create_forest_model()
+keras.utils.plot_model(forest_model, show_shapes=True)
+
+run_experiment(forest_model)
+
+"""
+You should achieve around 85.69% accuracy on the test data.
+"""
