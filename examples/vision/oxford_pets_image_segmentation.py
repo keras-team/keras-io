68a4201d24280fc54ffa6fd5d2857295e20598d8
Ali Haider <m.allihaider@gmail.com>
2021-01-07 00:06:27 +0500

    Update oxford_pets_image_segmentation.py
    For the two curl commands in lines 13 and 14, using URLs with the http protocol downloads files that are not recognized by the subsequent tar commands as tar archives. Using the https protocol solves the issue.

--- examples/vision/oxford_pets_image_segmentation.py
+++ examples/vision/oxford_pets_image_segmentation.py
@@ -10,8 +10,8 @@ Description: Image segmentation model trained from scratch on the Oxford Pets da
 """
 
 """shell
-curl -O http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
-curl -O http://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
+curl -O https://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
+curl -O https://www.robots.ox.ac.uk/~vgg/data/pets/data/annotations.tar.gz
 tar -xf images.tar.gz
 tar -xf annotations.tar.gz
 """
