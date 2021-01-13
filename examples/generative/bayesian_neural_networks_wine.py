d4c024d2b7ad805982d939b8f97944a6358f31fe
Khalid Salama <khalid.m.salama@gmail.com>
2021-01-13 22:47:46 +0000

    Update bayesian_neural_networks_wine.py

--- examples/generative/bayesian_neural_networks_wine.py
+++ examples/generative/bayesian_neural_networks_wine.py
@@ -68,10 +68,15 @@ split, and the rest as the test split.
 
 
 def get_train_and_test_splits(train_size, batch_size=1):
-    dataset = tfds.load(name="wine_quality", as_supervised=True, split="train").map(
-        lambda x, y: (x, tf.cast(y, tf.float32))
+    # We prefetch with a buffer the same size as the dataset because th dataset
+    # is very small and fits into memory.
+    dataset = (
+        tfds.load(name="wine_quality", as_supervised=True, split="train")
+        .map(lambda x, y: (x, tf.cast(y, tf.float32)))
+        .prefetch(buffer_size=dataset_size)
+        .cache()
     )
-
+    # We shuffle with a buffer the same size as the dataset.
     train_dataset = (
         dataset.take(train_size).shuffle(buffer_size=train_size).batch(batch_size)
     )
