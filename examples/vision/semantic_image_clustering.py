2fc4c3866c0695fc2ea41f68d69aba95e714019f
Khalid Salama <khalid.m.salama@gmail.com>
2021-02-15 19:35:45 +0000

    Create semantic_image_clustering.py

---
+++ examples/vision/semantic_image_clustering.py
@@ -0,0 +1,562 @@
+"""
+Title: Semantic Image Clustering
+Author: [Khalid Salama](https://www.linkedin.com/in/khalid-salama-24403144/)
+Date created: 2021/02/28
+Last modified: 2021/02/28
+Description: Semantic Clustering by Adopting Nearest neighbors (SCAN) algorithm.
+"""
+
+"""
+## Introduction
+
+This example demonstrates applying the [Semantic Clustering by Adopting Nearest neighbors
+(SCAN)](https://arxiv.org/abs/2005.12320) algorithm, by Wouter Van Gansbeke et. al, on the
+[CIFAR-10](https://www.cs.toronto.edu/~kriz/cifar.html) images dataset. The algorithm,
+consists of three phases:
+1. Self-supervised visual representation learning of images, in which we use the
+[simCLR](https://arxiv.org/abs/2002.05709?ref=hackernoon.com) technique.
+2. Clustering of the learnt visual representation vectors that maximizes the agreement
+between the cluster assignments of the neighbours.
+"""
+"""
+## Setup
+"""
+
+from collections import defaultdict
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+"""
+## Prepare the data
+"""
+
+num_classes = 10
+input_shape = (32, 32, 3)
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+x_data = np.concatenate([x_train, x_test])
+y_data = np.concatenate([y_train, y_test])
+
+print("x_data shape:", x_data.shape, " - y_data shape:", {y_data.shape})
+
+class_labels = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+
+"""
+## Define hyperparameters
+"""
+
+target_size = 64  # Resize the input images.
+learning_rate = 0.0004  # Learning rate for the Adam optimizer.
+representation_dim = 512  # The dimensions of the features vector.
+projection_units = 128  # The projection head of the representation learner.
+num_augumentations = 2  # Number of augmented images to generate for each input.
+num_clusters = 10  # Number of clusters.
+k_neighbours = 5  # Number of neighbours to consider during cluster learning.
+tune_encoder_during_clustering = False  # Freeze the encoder in the cluster learning.
+
+"""
+## Implement data preprocessing
+
+The data preprocessing resizes the input images to the desired `target_size`, and applies
+feature-wise normalization. Note that, when using `keras.applications.ResNet50V2` as the
+visual encoder, resizing the input images towards 255 x 255 would lead to better results,
+but would require longer time to train. In this example, we only resize the input images
+to 64 x 64.
+"""
+
+data_preprocessing = keras.Sequential(
+    [
+        layers.experimental.preprocessing.Resizing(target_size, target_size),
+        layers.experimental.preprocessing.Normalization(),
+    ]
+)
+# Compute the mean and the variance from the data for normalization.
+data_preprocessing.layers[-1].adapt(x_train)
+
+"""
+## Data augmentation
+Unlink simCLR, which randomly picks a single data augmentation function to apply to an input
+image, we apply a set of data augmentation functions randomly to the input image.
+You can experiment with other [image augmentation functinoality](https://www.tensorflow.org/tutorials/images/data_augmentation).
+"""
+
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomTranslation(
+            (-0.2, 0.2), (-0.2, 0.2), fill_mode="nearest"
+        ),
+        layers.experimental.preprocessing.RandomFlip(mode="horizontal"),
+        layers.experimental.preprocessing.RandomRotation(
+            factor=0.15, fill_mode="nearest"
+        ),
+        layers.experimental.preprocessing.RandomZoom(
+            height_factor=(-0.3, 0.1), width_factor=(-0.3, 0.1), fill_mode="nearest"
+        ),
+        layers.experimental.preprocessing.RandomContrast(factor=0.4),
+        layers.Lambda(lambda images: tf.image.random_brightness(images, max_delta=0.4)),
+        layers.Lambda(
+            lambda images: tf.image.random_saturation(images, lower=0, upper=0.4)
+        ),
+        layers.Lambda(lambda images: tf.image.random_hue(images, max_delta=0.1)),
+    ]
+)
+
+"""
+Display a random image
+"""
+
+image_idx = np.random.choice(range(x_data.shape[0]))
+image = x_data[image_idx]
+image_class = class_labels[y_data[image_idx][0]]
+plt.figure(figsize=(3, 3))
+plt.imshow(x_data[image_idx].astype("uint8"))
+plt.title(image_class)
+_ = plt.axis("off")
+
+"""
+Display a sample of augmented versions of the image
+"""
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+    augmented_images = data_augmentation(np.array([image]))
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(augmented_images[0].numpy().astype("uint8"))
+    plt.axis("off")
+
+"""
+## Self-supervised representation learning
+"""
+
+"""
+### Implement vision encoder
+"""
+
+
+def create_encoder(representation_dim):
+    encoder = keras.Sequential(
+        [
+            keras.applications.ResNet50V2(
+                include_top=False, weights=None, pooling="avg"
+            ),
+            layers.Dense(representation_dim),
+        ]
+    )
+    return encoder
+
+
+"""
+### Implement unsupervised contrastive loss
+"""
+
+
+class ContrastiveLoss(keras.losses.Loss):
+    def __init__(self, temperature=1.0, l2_normalize=False):
+        super(ContrastiveLoss, self).__init__()
+        self.temperature = temperature
+        self.l2_normalize = l2_normalize
+
+    def __call__(self, labels, feature_vectors, sample_weight=None):
+        # feature_vectors shape is [num_augmentations * batch_size, projection_units].
+        # Convert labels to an identiy matrix of shape [batch_size, batch_size].
+        labels = tf.eye(tf.shape(labels)[0])
+        if self.l2_normalize:
+            feature_vectors = tf.math.l2_normalize(feature_vectors, -1)
+        # logits shape: [num_augmentations * batch_size, num_augmentations * batch_size].
+        logits = (
+            tf.linalg.matmul(feature_vectors, feature_vectors, transpose_b=True)
+            / self.temperature
+        )
+        # Apply log-max trick for numerical stability.
+        logits_max = tf.math.reduce_max(logits, axis=1)
+        logits = logits - logits_max
+        # Compute num_augmentations.
+        num_augmentations = tf.shape(feature_vectors)[0] // tf.shape(labels)[0]
+        # targets shape: [num_augmentations * batch_size, num_augmentations * batch_size].
+        targets = tf.tile(labels, [num_augmentations, num_augmentations])
+        # Compute cross entropy loss
+        loss = keras.losses.categorical_crossentropy(
+            y_true=targets, y_pred=logits, from_logits=True
+        )
+        return loss
+
+
+"""
+### Implement representation learner
+"""
+
+
+def create_representation_learner(encoder, num_augumentations, projection_units):
+
+    inputs = keras.Input(shape=input_shape)
+    # Preprocess input images.
+    preprocessed = data_preprocessing(inputs)
+    # Create augmented versions of the images.
+    augmented = []
+    for _ in range(num_augumentations):
+        augmented.append(data_augmentation(preprocessed))
+    augmented = layers.Concatenate(axis=0)(augmented)
+    # Generate embedding representations of the images.
+    features = encoder(augmented)
+    # Add a projection head.
+    features = layers.Dropout(rate=0.1)(features)
+    x = layers.Dense(units=projection_units, use_bias=False)(features)
+    x = layers.BatchNormalization()(x)
+    outputs = layers.ReLU()(x)
+    # Create the model.
+    model = keras.Model(inputs=inputs, outputs=outputs, name="representation_learner")
+    return model
+
+
+"""
+### Train model
+"""
+
+encoder = create_encoder(representation_dim)
+representation_learner = create_representation_learner(
+    encoder, num_augumentations, projection_units
+)
+representation_learner.summary()
+
+# Since this self-supervised learning, we don't
+# use the y_data as our labels.
+labels = tf.ones(shape=(x_data.shape[0]))
+
+representation_learner.compile(
+    optimizer=keras.optimizers.Adam(learning_rate),
+    loss=ContrastiveLoss(temperature=0.1),
+)
+
+history = representation_learner.fit(
+    x=x_data,
+    y=labels,
+    batch_size=512,
+    epochs=50,  # for better results, increase the number of epochs to 500.
+)
+
+"""
+Plot training loss
+"""
+
+plt.plot(history.history["loss"])
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.show()
+
+"""
+## Compute nearest neighbours
+"""
+
+"""
+### Generate the embeddings for the images
+"""
+
+batch_size = 500
+# Get the feature vector representations of the images.
+feature_vectors = encoder.predict(x_data, batch_size=batch_size, verbose=1)
+# Normalize the feature vectores.
+feature_vectors = tf.math.l2_normalize(feature_vectors, -1)
+
+"""
+### Find the *k* nearest neighbours for each embedding
+"""
+
+neighbours = []
+num_batches = feature_vectors.shape[0] // batch_size
+for batch_idx in tqdm(range(num_batches)):
+    start_idx = batch_idx * batch_size
+    end_idx = start_idx + batch_size
+    current_batch = feature_vectors[start_idx:end_idx]
+    # Compute the dot similarity.
+    similarities = tf.linalg.matmul(current_batch, feature_vectors, transpose_b=True)
+    # Get the indices of most similar vectors.
+    _, indices = tf.math.top_k(similarities, k=k_neighbours + 1, sorted=True)
+    # Add the indices to the neighbours.
+    neighbours.append(indices[..., 1:])
+
+neighbours = np.reshape(np.array(neighbours), (-1, k_neighbours))
+
+"""
+Let's display some neighbours on each row
+"""
+
+nrows = 4
+ncols = k_neighbours + 1
+
+plt.figure(figsize=(12, 12))
+position = 1
+for _ in range(nrows):
+    anchor_idx = np.random.choice(range(x_data.shape[0]))
+    neighbour_indicies = neighbours[anchor_idx]
+    indices = [anchor_idx] + neighbour_indicies.tolist()
+    for j in range(ncols):
+        plt.subplot(nrows, ncols, position)
+        plt.imshow(x_data[indices[j]].astype("uint8"))
+        plt.title(class_labels[y_data[indices[j]][0]])
+        plt.axis("off")
+        position += 1
+
+"""
+You notice that images on each row are visually similar, and belong to similar classes.
+"""
+
+"""
+## Semantic clustering with nearest neighbours
+"""
+
+"""
+### Implement clustering consistency loss
+
+This loss tries to make sure that neighbours have the same clustering assignments.
+"""
+
+
+class ClustersConsistencyLoss(keras.losses.Loss):
+    def __init__(self):
+        super(ClustersConsistencyLoss, self).__init__()
+
+    def __call__(self, target, similarity, sample_weight=None):
+        # Set targets to be ones.
+        target = tf.ones_like(similarity)
+        # Compute cross entropy loss.
+        loss = keras.losses.binary_crossentropy(
+            y_true=target, y_pred=similarity, from_logits=True
+        )
+        return tf.math.reduce_mean(loss)
+
+
+"""
+### Implement the clusters entropy loss
+
+This loss tries to make sure that cluster distribution is roughly uniformed, to avoid
+assigning most of the instances to one cluster.
+"""
+
+
+class ClustersEntropyLoss(keras.losses.Loss):
+    def __init__(self, entropy_loss_weight=1.0):
+        super(ClustersEntropyLoss, self).__init__()
+        self.entropy_loss_weight = entropy_loss_weight
+
+    def __call__(self, target, cluster_probabilities, sample_weight=None):
+        # Ideal entropy = log(num_clusters).
+        num_clusters = tf.cast(tf.shape(cluster_probabilities)[-1], tf.dtypes.float32)
+        target = tf.math.log(num_clusters)
+        # Compute the overall clusters distribution.
+        cluster_probabilities = tf.math.reduce_mean(cluster_probabilities, axis=0)
+        # Replacing zero probabilities - if any - with a very small value.
+        cluster_probabilities = tf.clip_by_value(
+            cluster_probabilities, clip_value_min=1e-8, clip_value_max=1.0
+        )
+        # Compute the entropy over the clusters.
+        entropy = -tf.math.reduce_sum(
+            cluster_probabilities * tf.math.log(cluster_probabilities)
+        )
+        # Compute the difference between the target and the actual.
+        loss = target - entropy
+        return loss
+
+
+"""
+### Implement clustering model
+
+This model takes a raw image as an input, generated its feature vector using the trained
+encoder, and produces a probability distribution of the clusters given the feature vector
+as the cluster assignments.
+"""
+
+
+def create_clustering_model(encoder, num_clusters, name=None):
+    inputs = keras.Input(shape=input_shape)
+    # Preprocess the input images.
+    preprocessed = data_preprocessing(inputs)
+    # Apply data augmentation to the images.
+    augmented = data_augmentation(preprocessed)
+    # Generate embedding representations of the images.
+    features = encoder(augmented)
+    # Assign the images to clusters.
+    outputs = layers.Dense(units=num_clusters, activation="softmax")(features)
+    # Create the model.
+    model = keras.Model(inputs=inputs, outputs=outputs, name=name)
+    return model
+
+
+"""
+### Implement clustering learner
+
+This model receives the input `anchor` image and its `neighbours`, produces the clusters
+assignments for them using the `clustering_model`, and produces two outputs:
+1. `similarity`: the similarity between the cluster assignments of the `anchor` image and
+its `neighbours`. This output is fed to the `ClustersConsistencyLoss`.
+2. `anchor_clustering`: cluster assignments of the `anchor` images. This is fed to the `ClustersEntropyLoss`.
+"""
+
+
+def create_clustering_learner(clustering_model):
+    anchor = keras.Input(shape=input_shape, name="anchors")
+    neighbours = keras.Input(
+        shape=tuple([k_neighbours]) + input_shape, name="neighbours"
+    )
+    # Changes neighbours shape to [batch_size * k_neighbours, width, height, channels]
+    neighbours_reshaped = tf.reshape(neighbours, shape=tuple([-1]) + input_shape)
+    # anchor_clustering shape: [batch_size, num_clusters]
+    anchor_clustering = clustering_model(anchor)
+    # neighbours_clustering shape: [batch_size * k_neighbours, num_clusters]
+    neighbours_clustering = clustering_model(neighbours_reshaped)
+    # Convert neighbours_clustering shape to [batch_size, k_neighbours, num_clusters]
+    neighbours_clustering = tf.reshape(
+        neighbours_clustering,
+        shape=(-1, k_neighbours, tf.shape(neighbours_clustering)[-1]),
+    )
+    # similarity shape: [batch_size, 1, k_neighbours]
+    similarity = tf.linalg.einsum(
+        "bij,bkj->bik", tf.expand_dims(anchor_clustering, axis=1), neighbours_clustering
+    )
+    # similarity shape:  [batch_size, k_neighbours]
+    similarity = layers.Lambda(lambda x: tf.squeeze(x, axis=1), name="similarity")(
+        similarity
+    )
+    # Create the model.
+    model = keras.Model(
+        inputs=[anchor, neighbours],
+        outputs=[similarity, anchor_clustering],
+        name="clustering_learner",
+    )
+    return model
+
+
+"""
+### Train model
+"""
+
+# If tune_encoder_during_clustering is set to False,
+# then freeze the encoder weights.
+for layer in encoder.layers:
+    layer.trainable = tune_encoder_during_clustering
+
+clustering_model = create_clustering_model(encoder, num_clusters, name="clustering")
+clustering_learner = create_clustering_learner(clustering_model)
+clustering_learner.summary()
+
+losses = [ClustersConsistencyLoss(), ClustersEntropyLoss(entropy_loss_weight=5)]
+
+inputs = {"anchors": x_data, "neighbours": tf.gather(x_data, neighbours)}
+labels = tf.ones(shape=(x_data.shape[0]))
+
+clustering_learner.compile(optimizer=keras.optimizers.Adam(learning_rate), loss=losses)
+
+clustering_learner.fit(x=inputs, y=labels, batch_size=512, epochs=50)
+
+"""
+Plot training loss
+"""
+
+plt.plot(history.history["loss"])
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.show()
+
+"""
+## Cluster analysis
+"""
+
+"""
+### Assign images to clusters
+"""
+
+# Get the cluster probability distribution of the input images.
+clustering_probs = clustering_model.predict(x_data, batch_size=batch_size, verbose=1)
+# Get the cluster of the highest probability.
+cluster_assignments = tf.math.argmax(clustering_probs, axis=-1).numpy()
+# Store the clustering confidence.
+# Images with the highest clustering confidence are considered the 'prototypes'
+# of the clusters.
+cluster_confidence = tf.math.reduce_max(clustering_probs, axis=-1).numpy()
+
+"""
+Let's compute the cluster sizes
+"""
+
+clusters = defaultdict(list)
+for idx, c in enumerate(cluster_assignments):
+    clusters[c].append((idx, cluster_confidence[idx]))
+
+for c in range(num_clusters):
+    print("cluster", c, ":", {len(clusters[c])})
+
+"""
+Notice that the clusters have roughly balanced sizes.
+"""
+
+"""
+### Visualize cluster images
+
+We display the *prototypes* of each cluster, which are the instances with the highest
+clustering confidence.
+"""
+
+num_images = 8
+plt.figure(figsize=(15, 15))
+position = 1
+for c in range(num_clusters):
+    cluster_instances = sorted(clusters[c], key=lambda kv: kv[1], reverse=True)
+
+    for j in range(num_images):
+        image_idx = cluster_instances[j][0]
+        plt.subplot(num_clusters, num_images, position)
+        plt.imshow(x_data[image_idx].astype("uint8"))
+        plt.title(class_labels[y_data[image_idx][0]])
+        plt.axis("off")
+        position += 1
+
+"""
+### Compute clustering accuracy
+
+First, we assign a label for each cluster based on the majority label of its images.
+Then, we compute the accuracy of each cluster by dividing the number of image with the
+majority label by the size of the cluster.
+"""
+
+cluster_label_counts = dict()
+
+for c in range(num_clusters):
+    cluster_label_counts[c] = [0] * num_classes
+    instances = clusters[c]
+    for i in instances:
+        cluster_label_counts[c][y_data[i][0]] += 1
+
+    cluster_label_idx = np.argmax(cluster_label_counts[c])
+    correct_count = np.max(cluster_label_counts[c])
+    cluster_size = len(clusters[c])
+    accuracy = (
+        np.round((correct_count / cluster_size) * 100, 2) if cluster_size > 0 else 0
+    )
+    cluster_label = class_labels[cluster_label_idx]
+    print("cluster", c, "label is:", cluster_label, " -  accuracy:", {accuracy}, "%")
+
+"""
+## Concluding remarks
+
+To improve the accuracy results, you can perform the a fine-tuning step through self-labeling,
+as described in the [paper](https://arxiv.org/abs/2005.12320), besides increasing the number
+of epochs in the representation learning phase and the clustering phase.
+Note that such a technique is not expected to outperform the accuracy of supervised image
+classification techniques, rather showing that it can learn the semantics of the images and
+group them into clusters that are similar to their original classes.
+"""
