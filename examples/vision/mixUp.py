2fcd58d74f9e47c2f755fc169ce4508800cb1ab3
Sayak Paul <spsayakpaul@gmail.com>
2021-03-11 08:01:33 +0530

    feedback roun 4

--- examples/vision/mixUp.py
+++ examples/vision/mixUp.py
@@ -1,5 +1,5 @@
 """
-Title: mixup augmentation for image classification
+Title: MixUp augmentation for image classification
 Author: [Sayak Paul](https://twitter.com/RisingSayak)
 Date created: 2021/03/06
 Last modified: 2021/03/06
@@ -10,7 +10,7 @@ Description: Data augmentation using the mixup technique for image classificatio
 """
 
 """
-mixup is a *domain-agnostic* data augmentation technique proposed in [mixup: Beyond Empirical Risk Minimization](https://arxiv.org/abs/1710.09412) 
+_mixup_ is a *domain-agnostic* data augmentation technique proposed in [mixup: Beyond Empirical Risk Minimization](https://arxiv.org/abs/1710.09412) 
 by Zhang et al. It's implemented with the following formulas:
 
 ![](https://i.ibb.co/DRyHYww/image.png)
@@ -45,12 +45,9 @@ pip install tensorflow-probability
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-import tensorflow.keras.layers as L
-from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import layers
 
-import tensorflow_probability as tfp
-
-tf_distribution = tfp.distributions
+tf_distribution = tf.compat.v1.distributions
 
 """
 ## Prepare the dataset
@@ -63,11 +60,11 @@ be used for other classification datasets as well.
 
 x_train = x_train.astype("float32") / 255.0
 x_train = np.reshape(x_train, (-1, 28, 28, 1))
-y_train = to_categorical(y_train, 10)
+y_train = tf.one_hot(y_train, 10)
 
 x_test = x_test.astype("float32") / 255.0
 x_test = np.reshape(x_test, (-1, 28, 28, 1))
-y_test = to_categorical(y_test, 10)
+y_test = tf.one_hot(y_test, 10)
 
 """
 ## Define hyperparameters
@@ -81,19 +78,26 @@ EPOCHS = 5
 ## Convert the data into TensorFlow `Dataset` objects
 """
 
+# Put aside a few samples to create our validation set
+val_samples = 2000
+x_val, y_val = x_train[:val_samples], y_train[:val_samples]
+new_x_train, new_y_train = x_train[val_samples:], y_train[val_samples:]
+
 train_ds_one = (
-    tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    tf.data.Dataset.from_tensor_slices((new_x_train, new_y_train))
     .shuffle(BATCH_SIZE * 100)
     .batch(BATCH_SIZE)
 )
 train_ds_two = (
-    tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    tf.data.Dataset.from_tensor_slices((new_x_train, new_y_train))
     .shuffle(BATCH_SIZE * 100)
     .batch(BATCH_SIZE)
 )
 # Because we will be mixing up the images and their corresponding labels, we will be
 # combining two shuffled datasets from the same training data.
 train_ds = tf.data.Dataset.zip((train_ds_one, train_ds_two))
+
+val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val)).batch(BATCH_SIZE)
 
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(BATCH_SIZE)
 
@@ -157,14 +161,14 @@ for i, (image, label) in enumerate(zip(sample_images[:9], sample_labels[:9])):
 def get_training_model():
     model = tf.keras.Sequential(
         [
-            model.add(L.Conv2D(16, (5, 5), activation="relu", input_shape=(28, 28, 1))),
-            model.add(L.MaxPooling2D(pool_size=(2, 2))),
-            model.add(L.Conv2D(32, (5, 5), activation="relu")),
-            model.add(L.MaxPooling2D(pool_size=(2, 2))),
-            model.add(L.Dropout(0.2)),
-            model.add(L.GlobalAvgPool2D()),
-            model.add(L.Dense(128, activation="relu")),
-            model.add(L.Dense(10, activation="softmax")),
+            layers.Conv2D(16, (5, 5), activation="relu", input_shape=(28, 28, 1)),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(32, (5, 5), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Dropout(0.2),
+            layers.GlobalAvgPool2D(),
+            layers.Dense(128, activation="relu"),
+            layers.Dense(10, activation="softmax"),
         ]
     )
     return model
@@ -185,7 +189,7 @@ initial_model.save_weights("initial_weights.h5")
 model = get_training_model()
 model.load_weights("initial_weights.h5")
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(train_ds_mu, validation_data=test_ds, epochs=EPOCHS)
+model.fit(train_ds_mu, validation_data=val_ds, epochs=EPOCHS)
 _, test_acc = model.evaluate(test_ds)
 print("Test accuracy: {:.2f}%".format(test_acc * 100))
 
@@ -197,7 +201,7 @@ model = get_training_model()
 model.load_weights("initial_weights.h5")
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 # Notice that we are NOT using the mixed up dataset here
-model.fit(train_ds_one, validation_data=test_ds, epochs=EPOCHS)
+model.fit(train_ds_one, validation_data=val_ds, epochs=EPOCHS)
 _, test_acc = model.evaluate(test_ds)
 print("Test accuracy: {:.2f}%".format(test_acc * 100))
 
