e64417cd83795d8ab3566f297eaa69faa37ea44f
Sayak Paul <spsayakpaul@gmail.com>
2021-03-16 22:59:39 +0530

    modified py_function

--- examples/vision/randaugment.py
+++ examples/vision/randaugment.py
@@ -112,16 +112,12 @@ performant.
 
 train_ds_rand = (
     tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    .shuffle(BATCH_SIZE * 100)
+    .shuffle(BATCH_SIZE*100)
     .batch(BATCH_SIZE)
-    .map(lambda x, y: (tf.image.resize(x, (72, 72)), y), num_parallel_calls=AUTO)
-    .map(
-        lambda x, y: (tf.py_function(augment, [x], [tf.float32]), y),
-        num_parallel_calls=AUTO,
-    )
     # The returned output contains an unncessary axis of
     # 1-D and we need to remove it.
-    .map(lambda x, y: (tf.squeeze(x), y), num_parallel_calls=AUTO)
+    .map(lambda x, y: (tf.py_function(augment, [x], [tf.float32])[0], y),
+                            num_parallel_calls=AUTO)
     .prefetch(AUTO)
 )
 
