7aeab84d08ca0803ee673108434259b9df33eb84
Sayan Nath <sayannath235@gmail.com>
2021-03-25 23:07:58 +0530

    black format fixed

--- examples/vision/cutmix.py
+++ examples/vision/cutmix.py
@@ -73,7 +73,18 @@ For this example, we will be using the
 y_train = pd.get_dummies(y_train.flatten()).values.astype(np.float32)
 y_test = pd.get_dummies(y_test.flatten()).values.astype(np.float32)
 
-class_names = ["Airplane", "Automobile", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse", "Ship", "Truck"]
+class_names = [
+    "Airplane",
+    "Automobile",
+    "Bird",
+    "Cat",
+    "Deer",
+    "Dog",
+    "Frog",
+    "Horse",
+    "Ship",
+    "Truck",
+]
 
 """## Define hyperparameters"""
 
@@ -83,98 +94,119 @@ IMG_SHAPE = 32
 
 """## Dataloader"""
 
+
 def preprocess_image(image, label):
-  image = tf.image.resize(image, (IMG_SHAPE, IMG_SHAPE))
-  image = tf.image.convert_image_dtype(image, tf.float32) / 255.
-  return image, label
+    image = tf.image.resize(image, (IMG_SHAPE, IMG_SHAPE))
+    image = tf.image.convert_image_dtype(image, tf.float32) / 255.0
+    return image, label
+
 
 """## Convert the data into TensorFlow `Dataset` objects"""
 
-train_ds_one = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1024).map(preprocess_image, num_parallel_calls=AUTO)
-train_ds_two = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(1024).map(preprocess_image, num_parallel_calls=AUTO)
+train_ds_one = (
+    tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    .shuffle(1024)
+    .map(preprocess_image, num_parallel_calls=AUTO)
+)
+train_ds_two = (
+    tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    .shuffle(1024)
+    .map(preprocess_image, num_parallel_calls=AUTO)
+)
 
 train_ds_simple = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test))
 
-train_ds_simple = (train_ds_simple.map(preprocess_image, num_parallel_calls=AUTO).batch(BATCH_SIZE).prefetch(AUTO))
+train_ds_simple = (
+    train_ds_simple.map(preprocess_image, num_parallel_calls=AUTO)
+    .batch(BATCH_SIZE)
+    .prefetch(AUTO)
+)
 
 # We will be combining two shuffled datasets from the same training data.
 train_ds = tf.data.Dataset.zip((train_ds_one, train_ds_two))
 
-test_ds = (test_ds.map(preprocess_image, num_parallel_calls=AUTO).batch(BATCH_SIZE).prefetch(AUTO))
+test_ds = (
+    test_ds.map(preprocess_image, num_parallel_calls=AUTO)
+    .batch(BATCH_SIZE)
+    .prefetch(AUTO)
+)
 
 """##Define the CutMix Technique Function
 
 cutmix function takes two `image` and `label` pair and performs cutmix augmentation. It samples `λ(l)` from a [Beta Distribution](https://en.wikipedia.org/wiki/Beta_distribution) and we get the bounding box from get_box function. We crop the image2 and pad this image in the final padded image at the same location. 
 """
 
+
 @tf.function
 def get_box(l):
-  cut_rat = tf.math.sqrt(1.-l)
+    cut_rat = tf.math.sqrt(1.0 - l)
 
-  cut_w = IMG_SHAPE*cut_rat #rw
-  cut_w = tf.cast(cut_w, tf.int32)
-  
-  cut_h = IMG_SHAPE*cut_rat #rh
-  cut_h = tf.cast(cut_h, tf.int32)
-  
-  cx = tf.random.uniform((1,), minval=0, maxval=IMG_SHAPE, dtype=tf.int32) #rx
-  cy = tf.random.uniform((1,), minval=0, maxval=IMG_SHAPE, dtype=tf.int32) #ry
-  
-  bbx1 = tf.clip_by_value(cx[0] - cut_w // 2, 0, IMG_SHAPE)
-  bby1 = tf.clip_by_value(cy[0] - cut_h // 2, 0, IMG_SHAPE)
-  bbx2 = tf.clip_by_value(cx[0] + cut_w // 2, 0, IMG_SHAPE)
-  bby2 = tf.clip_by_value(cy[0] + cut_h // 2, 0, IMG_SHAPE)
-  
-  target_h = bby2-bby1
-  if target_h ==0:
-    target_h+=1
+    cut_w = IMG_SHAPE * cut_rat  # rw
+    cut_w = tf.cast(cut_w, tf.int32)
 
-  target_w = bbx2-bbx1
-  if target_w ==0:
-    target_w+=1
+    cut_h = IMG_SHAPE * cut_rat  # rh
+    cut_h = tf.cast(cut_h, tf.int32)
 
-  return bbx1, bby1, target_h, target_w
+    cx = tf.random.uniform((1,), minval=0, maxval=IMG_SHAPE, dtype=tf.int32)  # rx
+    cy = tf.random.uniform((1,), minval=0, maxval=IMG_SHAPE, dtype=tf.int32)  # ry
+
+    bbx1 = tf.clip_by_value(cx[0] - cut_w // 2, 0, IMG_SHAPE)
+    bby1 = tf.clip_by_value(cy[0] - cut_h // 2, 0, IMG_SHAPE)
+    bbx2 = tf.clip_by_value(cx[0] + cut_w // 2, 0, IMG_SHAPE)
+    bby2 = tf.clip_by_value(cy[0] + cut_h // 2, 0, IMG_SHAPE)
+
+    target_h = bby2 - bby1
+    if target_h == 0:
+        target_h += 1
+
+    target_w = bbx2 - bbx1
+    if target_w == 0:
+        target_w += 1
+
+    return bbx1, bby1, target_h, target_w
+
 
 @tf.function
 def cutmix(a, b):
-  
-  (image1, label1), (image2, label2) = a, b
 
-  alpha = [1.]
-  beta = [1.]
-  
-  ## Get sample from beta distribution
-  dist = tfd.Beta(alpha, beta)
-  ## Lambda
-  l = dist.sample(1)[0][0]
+    (image1, label1), (image2, label2) = a, b
 
-  ## Get bbox ofsets and heights and widths
-  bbx1, bby1, target_h, target_w = get_box(l)
+    alpha = [1.0]
+    beta = [1.0]
 
-  ## Get patch from image2
-  crop2 = tf.image.crop_to_bounding_box(image2, bby1, bbx1, target_h, target_w)
-  ## Pad the patch with same offset
-  image2 = tf.image.pad_to_bounding_box(crop2, bby1, bbx1, IMG_SHAPE, IMG_SHAPE)
-  ## Get patch from image1
-  crop1 = tf.image.crop_to_bounding_box(image1, bby1, bbx1, target_h, target_w)
-  ## Pad the patch with same offset
-  img1 = tf.image.pad_to_bounding_box(crop1, bby1, bbx1, IMG_SHAPE, IMG_SHAPE)
+    ## Get sample from beta distribution
+    dist = tfd.Beta(alpha, beta)
+    ## Lambda
+    l = dist.sample(1)[0][0]
 
-  ## Subtract the patch from image1 so that patch from image2 can be put on instead
-  image1 = image1-img1
-  ## Add modified image1 and image2 to get cutmix image
-  image = image1+image2
+    ## Get bbox ofsets and heights and widths
+    bbx1, bby1, target_h, target_w = get_box(l)
 
-  ## Adjust lambda according to pixel ration
-  l = 1 - (target_w * target_h) / (IMG_SHAPE * IMG_SHAPE)
-  l = tf.cast(l, tf.float32)
+    ## Get patch from image2
+    crop2 = tf.image.crop_to_bounding_box(image2, bby1, bbx1, target_h, target_w)
+    ## Pad the patch with same offset
+    image2 = tf.image.pad_to_bounding_box(crop2, bby1, bbx1, IMG_SHAPE, IMG_SHAPE)
+    ## Get patch from image1
+    crop1 = tf.image.crop_to_bounding_box(image1, bby1, bbx1, target_h, target_w)
+    ## Pad the patch with same offset
+    img1 = tf.image.pad_to_bounding_box(crop1, bby1, bbx1, IMG_SHAPE, IMG_SHAPE)
 
-  ## Combine labels
-  label = l*label1+(1-l)*label2
+    ## Subtract the patch from image1 so that patch from image2 can be put on instead
+    image1 = image1 - img1
+    ## Add modified image1 and image2 to get cutmix image
+    image = image1 + image2
 
-  return image, label
+    ## Adjust lambda according to pixel ration
+    l = 1 - (target_w * target_h) / (IMG_SHAPE * IMG_SHAPE)
+    l = tf.cast(l, tf.float32)
+
+    ## Combine labels
+    label = l * label1 + (1 - l) * label2
+
+    return image, label
+
 
 """**Note** that here , we are combining two images to create a single one.
 
@@ -182,33 +214,43 @@ def cutmix(a, b):
 """
 
 # Create the new dataset using our `mix_up` utility
-train_ds_cmu = (train_ds.shuffle(1024).map(cutmix, num_parallel_calls=AUTO).batch(BATCH_SIZE).prefetch(AUTO))
+train_ds_cmu = (
+    train_ds.shuffle(1024)
+    .map(cutmix, num_parallel_calls=AUTO)
+    .batch(BATCH_SIZE)
+    .prefetch(AUTO)
+)
 
 # Let's preview 9 samples from the dataset
 image_batch, label_batch = next(iter(train_ds_cmu))
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 for i in range(9):
-    ax = plt.subplot(3 ,3 ,i + 1)
+    ax = plt.subplot(3, 3, i + 1)
     plt.title(class_names[np.argmax(label_batch[i])])
     plt.imshow(image_batch[i])
-    plt.axis('off')
+    plt.axis("off")
 
 """## Define the Model"""
 
-def resnet_layer(inputs,
-                 num_filters=16,
-                 kernel_size=3,
-                 strides=1,
-                 activation='relu',
-                 batch_normalization=True,
-                 conv_first=True):
 
-    conv = keras.layers.Conv2D(num_filters,
-                  kernel_size=kernel_size,
-                  strides=strides,
-                  padding='same',
-                  kernel_initializer='he_normal',
-                  kernel_regularizer=keras.regularizers.l2(1e-4))
+def resnet_layer(
+    inputs,
+    num_filters=16,
+    kernel_size=3,
+    strides=1,
+    activation="relu",
+    batch_normalization=True,
+    conv_first=True,
+):
+
+    conv = keras.layers.Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        strides=strides,
+        padding="same",
+        kernel_initializer="he_normal",
+        kernel_regularizer=keras.regularizers.l2(1e-4),
+    )
 
     x = inputs
     if conv_first:
@@ -225,10 +267,11 @@ def resnet_layer(inputs,
         x = conv(x)
     return x
 
+
 def resnet_v20(input_shape, depth, num_classes=10):
 
     if (depth - 2) % 6 != 0:
-        raise ValueError('depth should be 6n+2 (eg 20, 32, 44 in [a])')
+        raise ValueError("depth should be 6n+2 (eg 20, 32, 44 in [a])")
     # Start model definition.
     num_filters = 16
     num_res_blocks = int((depth - 2) / 6)
@@ -241,39 +284,39 @@ def resnet_v20(input_shape, depth, num_classes=10):
             strides = 1
             if stack > 0 and res_block == 0:  # first layer but not first stack
                 strides = 2  # downsample
-            y = resnet_layer(inputs=x,
-                             num_filters=num_filters,
-                             strides=strides)
-            y = resnet_layer(inputs=y,
-                             num_filters=num_filters,
-                             activation=None)
+            y = resnet_layer(inputs=x, num_filters=num_filters, strides=strides)
+            y = resnet_layer(inputs=y, num_filters=num_filters, activation=None)
             if stack > 0 and res_block == 0:  # first layer but not first stack
                 # linear projection residual shortcut connection to match
                 # changed dims
-                x = resnet_layer(inputs=x,
-                                 num_filters=num_filters,
-                                 kernel_size=1,
-                                 strides=strides,
-                                 activation=None,
-                                 batch_normalization=False)
+                x = resnet_layer(
+                    inputs=x,
+                    num_filters=num_filters,
+                    kernel_size=1,
+                    strides=strides,
+                    activation=None,
+                    batch_normalization=False,
+                )
             x = keras.layers.add([x, y])
-            x = keras.layers.Activation('relu')(x)
+            x = keras.layers.Activation("relu")(x)
         num_filters *= 2
 
     # Add classifier on top.
     # v1 does not use BN after last shortcut connection-ReLU
     x = keras.layers.AveragePooling2D(pool_size=8)(x)
     y = keras.layers.Flatten()(x)
-    outputs = keras.layers.Dense(num_classes,
-                    activation='softmax',
-                    kernel_initializer='he_normal')(y)
+    outputs = keras.layers.Dense(
+        num_classes, activation="softmax", kernel_initializer="he_normal"
+    )(y)
 
     # Instantiate model.
     model = keras.models.Model(inputs=inputs, outputs=outputs)
     return model
 
+
 def training_model():
-  return resnet_v20((32,32,3), 20)
+    return resnet_v20((32, 32, 3), 20)
+
 
 initial_model = training_model()
 initial_model.save_weights("initial_weights.h5")
@@ -283,7 +326,7 @@ initial_model.save_weights("initial_weights.h5")
 model = training_model()
 model.load_weights("initial_weights.h5")
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(train_ds_cmu,validation_data=test_ds, epochs=15)
+model.fit(train_ds_cmu, validation_data=test_ds, epochs=15)
 
 test_loss, test_accuracy = model.evaluate(test_ds)
 print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
@@ -293,7 +336,7 @@ print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
 model = training_model()
 model.load_weights("initial_weights.h5")
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(train_ds_simple,validation_data=test_ds, epochs=15)
+model.fit(train_ds_simple, validation_data=test_ds, epochs=15)
 
 test_loss, test_accuracy = model.evaluate(test_ds)
 print("Test accuracy: {:.2f}%".format(test_accuracy * 100))
