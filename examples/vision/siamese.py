03b08c100cfb2fdd5f5b465676716120e86cca03
s-mrb <syedmehdirazab@gmail.com>
2021-05-14 12:03:53 +0500

    separate_val

--- examples/vision/siamese.py
+++ examples/vision/siamese.py
@@ -31,6 +31,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 """
 ## Load the MNIST dataset
@@ -41,6 +43,13 @@ import matplotlib.pyplot as plt
 # Change the data type to a floating point format
 x_train = x_train.astype("float32")
 x_test = x_test.astype("float32")
+
+
+"""
+Use train_test_split to split train data into `train` and `val`
+"""
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.10)
 
 
 """
@@ -60,7 +69,7 @@ repeat this process for the remaining classes for the rest of the digits (from
 
 def make_pairs(x, y):
     """
-    Creates a tuple containing image pairs with coreesponding label
+    Creates a tuple containing image pairs with corresponding label
 
     Parameters
     ----------
@@ -72,8 +81,8 @@ def make_pairs(x, y):
 
     Returns
     -------
-    Tuple containing two numpy arrays as (pair_of_samples, labels),
-    where pair of samples' shape is (2len(x), 2,n_features_dims) and
+    Tuple containing two numpy arrays as (pairs_of_samples, labels),
+    where pairs_of_samples' shape is (2len(x), 2,n_features_dims) and
     labels are a binary array of shape (2len(x))
     """
 
@@ -107,8 +116,13 @@ def make_pairs(x, y):
     return np.array(pairs), np.array(labels).astype("float32")
 
 
-
+# make train pairs
 pairs_train, labels_train = make_pairs(x_train, y_train)
+
+# make validation pairs
+pairs_val, labels_val = make_pairs(x_val, y_val)
+
+# make test pairs
 pairs_test, labels_test = make_pairs(x_test, y_test)
 
 """
@@ -134,12 +148,27 @@ contain 120K pairs, whereas each `pair within pairs_train` have two axis,
 each for one image of a pair.
 """
 
+"""
+Separate train pairs
+"""
+
 x_train_1 = pairs_train[:, 0]
 x_train_2 = pairs_train[:, 1]
 # x_train_1.shape = (120000, 28, 28)
 
-x_test_1, x_test_2 = pairs_test[:, 0], pairs_test[:, 1]
+"""
+Separate validation pairs
+"""
+x_val_1 = pairs_val[:, 0]
+x_val_2 = pairs_val[:, 1]
+# x_val_1.shape = (12000, 28, 28)
+"""
+Separate test pairs
+"""
 
+x_test_1 = pairs_test[:, 0]
+x_test_2 = pairs_test[:, 1]
+# x_test_1.shape = (20000, 28, 28)
 
 
 """
@@ -149,7 +178,7 @@ x_test_1, x_test_2 = pairs_test[:, 0], pairs_test[:, 1]
 
 def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False):
     """
-    Creates a plot pairs with label and prediction if it's test dataset
+    Creates a plot of pairs and labels, and prediction if it's test dataset
 
     Parameters
     ----------
@@ -163,8 +192,10 @@ def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False)
               and incremented if if it is less then num_col.
     num_col : int
               Number of images in one row - (default is 3)
+              For test and train respectively, it should not exceed 3 and 7
     predictions : list
-                  Array of predictions with shape (to_show, 1) - (default None)
+                  Array of predictions with shape (to_show, 1) -
+                  (default is None)
                   Must be passed when test=True
     test  : boolean
             Whether the dataset being visualized is train dataset or
@@ -204,9 +235,8 @@ def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False)
         else:
             ax = axes[i // num_col, i % num_col]
 
-        # images[0][i][:,:,0] -> because it is
-        # (28,28,1) and imshow takes (28,82)
         ax.imshow(tf.concat([pairs[i][0], pairs[i][1]], axis=1), cmap="gray")
+        ax.set_axis_off()
         if test:
             ax.set_title("True: {} | Pred: {:.5f}".format(labels[i], predictions[i][0]))
         else:
@@ -218,9 +248,23 @@ def visualize(pairs, labels, to_show=6, num_col=3, predictions=None, test=False)
     plt.show()
 
 
+"""
+Inspect train pairs
+"""
 
+visualize(pairs_train[0:-1], labels_train[0:-1], to_show=4, num_col=4)
 
-visualize(pairs_train, labels_train, to_show=3)
+"""
+Inspect validation pairs
+"""
+
+visualize(pairs_val[0:-1], labels_val[0:-1], to_show=4, num_col=4)
+
+"""
+Inspect test pairs
+"""
+
+visualize(pairs_test[0:-1], labels_test[0:-1], to_show=4, num_col=4)
 
 """
 ## Define the model
@@ -251,7 +295,6 @@ def euclidean_distance(vects):
     x, y = vects
     sum_square = tf.math.reduce_sum(tf.math.square(x - y), axis=1, keepdims=True)
     return tf.math.sqrt(tf.math.maximum(sum_square, tf.keras.backend.epsilon()))
-
 
 
 input = layers.Input((28, 28, 1))
@@ -313,7 +356,6 @@ def contrastive_loss(y_true, y_pred, margin=1):
     return tf.math.reduce_mean((1 - y_true) * square_pred + (y_true) * margin_square)
 
 
-
 """
 Compile the model with constrastive loss
 """
@@ -329,7 +371,11 @@ Train the model
 
 # Rarely it stucks at local optima, in that case just try again
 siamese.fit(
-    [x_train_1, x_train_2], labels_train, batch_size=16, validation_split=0.1, epochs=6
+    [x_train_1, x_train_2],
+    labels_train,
+    validation_data=([x_val_1, x_val_2], labels_val),
+    batch_size=16,
+    epochs=10,
 )
 
 
@@ -347,5 +393,5 @@ print("test loss, test acc:", results)
 predictions = siamese.predict([x_test_1, x_test_2])
 
 visualize(
-    pairs_test, labels_test, to_show=3, num_col=3, predictions=predictions, test=True
+    pairs_test, labels_test, to_show=3, predictions=predictions, test=True
 )
