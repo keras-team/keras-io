cf3191190f3adb0e321df15fe4a03e8d6fe6ab4c
Sayak Paul <spsayakpaul@gmail.com>
2021-05-15 09:20:58 +0530

    feedback round III

--- examples/vision/keypoint_detection.py
+++ examples/vision/keypoint_detection.py
@@ -369,12 +369,15 @@ head for predicting coordinates.
 
 
 def get_model():
+    # Load the pre-trained weights of MobileNetV2 and freeze the weights
     backbone = keras.applications.MobileNetV2(
         weights="imagenet", include_top=False, input_shape=(IMG_SIZE, IMG_SIZE, 3)
     )
+    backbone.trainable = False
+    
     inputs = layers.Input((IMG_SIZE, IMG_SIZE, 3))
     x = keras.applications.mobilenet_v2.preprocess_input(inputs)
-    x = backbone(x, training=False)
+    x = backbone(x)
     x = layers.Dropout(0.3)(x)
     x = layers.SeparableConv2D(
         NUM_KEYPOINTS, kernel_size=5, strides=1, activation="relu"
@@ -383,7 +386,7 @@ def get_model():
         NUM_KEYPOINTS, kernel_size=3, strides=1, activation="sigmoid"
     )(x)
 
-    return tf.keras.Model(inputs, outputs, name="keypoint_detector")
+    return keras.Model(inputs, outputs, name="keypoint_detector")
 
 
 """
