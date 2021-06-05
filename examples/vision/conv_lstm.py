7446ade60be87d2f24c40ddbc92395e0079bc63f
amogh7joshi <joshi.amoghn@gmail.com>
2021-06-05 11:27:50 -0400

    Updated formatting and changed name to conv_lstm.py

--- examples/vision/conv_lstm.py
+++ examples/vision/conv_lstm.py
@@ -43,11 +43,11 @@ For next-frame prediction, our model will be using a previous frame,
 which we'll call `f_n`, to predict a new frame, called `f_(n + 1)`.
 To allow the model to create these predictions, we'll need to process
 the data such that we have "shifted" inputs and outputs, where the
-input data is frame `X_n`, being used to predict frame `y_(n + 1)`.
+input data is frame `x_n`, being used to predict frame `y_(n + 1)`.
 """
 
 # Download and load the dataset.
-fpath = tf.keras.utils.get_file(
+fpath = keras.utils.get_file(
     "moving_mnist.npy",
     "http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy",
 )
@@ -61,8 +61,7 @@ dataset = dataset[:1000, ...]
 dataset = np.expand_dims(dataset, axis=-1)
 
 # Split into train and validation sets using indexing to optimize memory.
-train_index, val_index = train_test_split(
-    np.arange(dataset.shape[0]), train_size=0.9)
+train_index, val_index = train_test_split(np.arange(dataset.shape[0]), train_size=0.9)
 train_dataset = dataset[train_index]
 val_dataset = dataset[val_index]
 
@@ -71,19 +70,20 @@ train_dataset = train_dataset / 255
 val_dataset = val_dataset / 255
 
 # We'll define a helper function to shift the frames, where
-# `X` is frames 0 to n - 1, and `y` is frames 1 to n.
+# `x` is frames 0 to n - 1, and `y` is frames 1 to n.
 def create_shifted_frames(data):
-    X = data[:, 0 : data.shape[1] - 1, :, :]
+    x = data[:, 0 : data.shape[1] - 1, :, :]
     y = data[:, 1 : data.shape[1], :, :]
-    return X, y
+    return x, y
+
 
 # Apply the processing function to the datasets.
-X_train, y_train = create_shifted_frames(train_dataset)
-X_val, y_val = create_shifted_frames(val_dataset)
+x_train, y_train = create_shifted_frames(train_dataset)
+x_val, y_val = create_shifted_frames(val_dataset)
 
 # Inspect the dataset.
-print("Training Dataset Shapes: " + str(X_train.shape) + ", " + str(y_train.shape))
-print("Validation Dataset Shapes: " + str(X_val.shape) + ", " + str(y_val.shape))
+print("Training Dataset Shapes: " + str(x_train.shape) + ", " + str(y_train.shape))
+print("Validation Dataset Shapes: " + str(x_val.shape) + ", " + str(y_val.shape))
 
 """
 ## Data Visualization
@@ -121,7 +121,7 @@ build the model:
 """
 
 # Construct the input layer with no definite frame size.
-inp = layers.Input(shape=(None, *X_train.shape[2:]))
+inp = layers.Input(shape=(None, *x_train.shape[2:]))
 
 # We will construct 3 `ConvLSTM2D` layers with batch normalization,
 # followed by a `Conv3D` layer for the spatiotemporal outputs.
@@ -174,16 +174,16 @@ batch_size = 5
 
 # Fit the model to the training data.
 model.fit(
-    X_train,
+    x_train,
     y_train,
     batch_size=batch_size,
     epochs=epochs,
-    validation_data=(X_val, y_val),
+    validation_data=(x_val, y_val),
     callbacks=[early_stopping, reduce_lr],
 )
 
 """
-## Test Predictions
+## Frame Prediction Visualizations
 
 With our model now constructed and trained, we can create
 some example frame predictions based on the input data.
@@ -229,5 +229,3 @@ for idx, ax in enumerate(axes[1]):
 
 # Display the figure.
 plt.show()
-
-
