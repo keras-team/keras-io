ecb1d2072536e74a8ab06f1d235bad576f6a7547
jacoverster <versterrie@gmail.com>
2021-06-06 18:41:31 +0200

    last minor edits

--- examples/keras_recipes/sample_size_estimate.py
+++ examples/keras_recipes/sample_size_estimate.py
@@ -280,7 +280,7 @@ def train_model(training_data, training_labels):
         epochs=10,
     )
 
-    # Unfreeze model from block 6 onwards
+    # Unfreeze model from block 10 onwards
     model = unfreeze(model, "block_10")
 
     # Compile and train for 20 epochs with a lower learning rate
