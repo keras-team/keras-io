6edfc389db263e3d7d82544c6d0ef1acd4368df4
amogh7joshi <joshi.amoghn@gmail.com>
2021-06-05 20:48:23 -0400

    Added prediction GIFs

--- examples/vision/conv_lstm.py
+++ examples/vision/conv_lstm.py
@@ -28,6 +28,11 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+import io
+import imageio
+from IPython.display import Image, display
+from ipywidgets import widgets, Layout, HBox
+
 """
 ## Dataset Construction
 
@@ -170,8 +175,8 @@ With our model and data constructed, we can now train the model.
 early_stopping = keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
 reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5)
 
-# Define modifiable hyperparameters.
-epochs = 5
+# Define modifiable training hyperparameters.
+epochs = 20
 batch_size = 5
 
 # Fit the model to the training data.
@@ -231,3 +236,58 @@ for idx, ax in enumerate(axes[1]):
 
 # Display the figure.
 plt.show()
+
+"""
+## Predicted Videos
+
+Finally, we'll pick a few examples from the validation set
+and construct some GIFs with them to see the model's
+predicted videos.
+"""
+
+# Select a few random examples from the dataset.
+examples = val_dataset[np.random.choice(range(len(val_dataset)), size=5)]
+
+# Iterate over the examples and predict the frames.
+predicted_videos = []
+for example in examples:
+    # Pick the first/last ten frames from the example.
+    frames = example[:10, ...]
+    original_frames = example[10:, ...]
+    new_predictions = np.zeros(shape=(10, *frames[0].shape))
+
+    # Predict a new set of 10 frames.
+    for i in range(10):
+        # Extract the model's prediction and post-process it.
+        frames = example[: 10 + i + 1, ...]
+        new_prediction = model.predict(np.expand_dims(frames, axis=0))
+        new_prediction = np.squeeze(new_prediction, axis=0)
+        predicted_frame = np.expand_dims(new_prediction[-1, ...], axis=0)
+
+        # Extend the set of prediction frames.
+        new_predictions[i] = predicted_frame
+
+    # Create and save GIFs for each of the ground truth/prediction images.
+    for frame_set in [original_frames, new_predictions]:
+        # Construct a GIF from the selected video frames.
+        current_frames = np.squeeze(frame_set)
+        current_frames = current_frames[..., np.newaxis] * np.ones(3)
+        current_frames = (current_frames * 255).astype(np.uint8)
+        current_frames = list(current_frames)
+
+        # Construct a GIF from the frames.
+        with io.BytesIO() as gif:
+            imageio.mimsave(gif, current_frames, "GIF", fps=5)
+            predicted_videos.append(gif.getvalue())
+
+# Display the videos.
+print(" Truth\tPrediction")
+for i in range(0, len(predicted_videos), 2):
+    # Construct and display an `HBox` with the ground truth and prediction.
+    box = HBox(
+        [
+            widgets.Image(value=predicted_videos[i]),
+            widgets.Image(value=predicted_videos[i + 1]),
+        ]
+    )
+    display(box)
