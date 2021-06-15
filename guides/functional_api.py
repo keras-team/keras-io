2cadaba571c8a742738f66d4b870f0d465aa7c0b
Ron Shapiro <ronshapiro@google.com>
2021-06-15 15:23:12 +0300

    Use dictionaries for both `loss` and `loss_weights` consistently

--- guides/functional_api.py
+++ guides/functional_api.py
@@ -390,7 +390,10 @@ model.compile(
         "priority": keras.losses.BinaryCrossentropy(from_logits=True),
         "department": keras.losses.CategoricalCrossentropy(from_logits=True),
     },
-    loss_weights=[1.0, 0.2],
+    loss_weights={
+        "priority": 1.0,
+        "department": 0.2,
+    },
 )
 
 """
