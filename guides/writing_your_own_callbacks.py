a3afb1fecfc5c593db4587e1f013865ef80f5fdc
Étienne <etienneboucher@hotmail.ca>
2021-07-08 00:08:53 -0400

    Correct description of logs on batch end
    The description of both methods on batch end were corrected.
    The description of method on epoch end was restored.

--- guides/writing_your_own_callbacks.py
+++ guides/writing_your_own_callbacks.py
@@ -209,14 +209,14 @@ epoch. Example includes the loss and mean absolute error.
 
 class LossAndErrorPrintingCallback(keras.callbacks.Callback):
     def on_train_batch_end(self, batch, logs=None):
-        print("For batch {}, loss is {:7.2f}.".format(batch, logs["loss"]))
+        print("Up to batch {}, the average loss is {:7.2f}.".format(batch, logs["loss"]))
 
     def on_test_batch_end(self, batch, logs=None):
-        print("For batch {}, loss is {:7.2f}.".format(batch, logs["loss"]))
+        print("Up to batch {}, the average loss is {:7.2f}.".format(batch, logs["loss"]))
 
     def on_epoch_end(self, epoch, logs=None):
         print(
-            "The loss of the last step of epoch {} is {:7.2f} "
+            "The average loss for epoch {} is {:7.2f} "
             "and mean absolute error is {:7.2f}.".format(
                 epoch, logs["loss"], logs["mean_absolute_error"]
             )
