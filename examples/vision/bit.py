42c967e669421366988b8b4c65a384da7e52ba82
Sayan Nath <sayannath235@gmail.com>
2021-08-29 00:22:44 +0530

    Feedback Round I

--- examples/vision/bit.py
+++ examples/vision/bit.py
@@ -1,8 +1,8 @@
 """
 Title: Image Classification using BigTransfer(BiT)
 Author: [Sayan Nath](https://twitter.com/sayannath2350)
-Date created: 2021/08/19 
-Last modified: 2021/08/19
+Date created: 2021/08/29 
+Last modified: 2021/08/29
 Description: BigTransfer (BiT) State-of-the-art transfer learning for image classification.
 """
 
@@ -10,7 +10,7 @@ Description: BigTransfer (BiT) State-of-the-art transfer learning for image clas
 ## Introduction
 
 
-BigTransfer known as BiT is a State-of-the-art transfer learning method for Image Classification Tasks. a set of pre-trained image models that can be transferred to obtain excellent performance on new datasets, even with only a few examples per class. BiT performs well across a surprisingly wide range of data regimes — from 1 example per class to 1 M total examples. BiT achieves 87.5% top-1 accuracy on ILSVRC-2012, 99.4% on CIFAR-10, and 76.3% on the 19 task Visual Task Adaptation Benchmark (VTAB). On small datasets, BiT attains 76.8% on ILSVRC-2012 with 10 examples per class, and 97.0% on CIFAR-10 with 10 examples per class. A detailed analysis of the main components are described [here](https://arxiv.org/pdf/1912.11370.pdf) which leads to high transfer performance.
+BigTransfer known as BiT is a State-of-the-art transfer learning method for Image Classification Tasks. a set of pre-trained image models that can be transferred to obtain excellent performance on new datasets, even with only a few examples per class. BiT performs well across a surprisingly wide range of data regimes — from 1 example per class to 1M total examples. A detailed analysis of the main components are described [here](https://arxiv.org/pdf/1912.11370.pdf) which leads to high transfer performance.
 """
 
 """
@@ -20,6 +20,7 @@ BigTransfer known as BiT is a State-of-the-art transfer learning method for Imag
 import re
 import numpy as np
 import pandas as pd
+import requests, zipfile, io
 import matplotlib.pyplot as plt
 from imutils import paths
 from pprint import pprint
@@ -42,11 +43,16 @@ tf.random.set_seed(SEEDS)
 We will be using Image-Scene-Classification dataset which comprises of 30 classes.
 """
 
-"""shell
-wget -q http://data.vision.ee.ethz.ch/ihnatova/camera_scene_detection_train.zip
-unzip -qq camera_scene_detection_train.zip
-rm -rf camera_scene_detection_train.zip
-"""
+
+def download_dataset(url):
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("./")  # Extracting the Data from the zip file
+
+
+download_dataset(
+    "http://data.vision.ee.ethz.ch/ihnatova/camera_scene_detection_train.zip"
+)
 
 """
 ## Data Parsing
@@ -68,7 +74,7 @@ class_count = Counter(labels)
 pprint(class_count)
 
 """
-Imbalance Dataset
+As there is an unequal distribution of classes in the dataset. It is classified as imbalance dataset
 """
 
 """
@@ -87,12 +93,12 @@ STEPS_PER_EPOCH = 140
 ## Splitting the Dataset
 """
 
-i = int(len(image_paths) * TRAIN_SPLIT)
+TRAIN_LENGTH = int(len(image_paths) * TRAIN_SPLIT)
 
-train_paths = image_paths[:i]
-train_labels = labels[:i]
-validation_paths = image_paths[i:]
-validation_labels = labels[i:]
+train_paths = image_paths[:TRAIN_LENGTH]
+train_labels = labels[:TRAIN_LENGTH]
+validation_paths = image_paths[TRAIN_LENGTH:]
+validation_labels = labels[TRAIN_LENGTH:]
 
 print(len(train_paths), len(validation_paths))
 
@@ -100,21 +106,21 @@ print(len(train_paths), len(validation_paths))
 ## Encding the Labels
 """
 
-le = LabelEncoder()
-train_labels_le = le.fit_transform(train_labels)
-validation_labels_le = le.transform(validation_labels)
+label_encoder = LabelEncoder()
+train_labels_le = label_encoder.fit_transform(train_labels)
+validation_labels_le = label_encoder.transform(validation_labels)
 print(train_labels_le[:5])
 
 """
 Since the dataset has class imbalance issue, it's good to supply class weights while training the model.
 """
 
-trainLabels = to_categorical(train_labels_le)
-classTotals = trainLabels.sum(axis=0)
-classWeight = dict()
+train_labels = to_categorical(train_labels_le)
+class_totals = train_labels.sum(axis=0)
+class_weight = dict()
 # loop over all classes and calculate the class weight
-for i in range(0, len(classTotals)):
-    classWeight[i] = classTotals.max() / classTotals[i]
+for i in range(0, len(class_totals)):
+    class_weight[i] = class_totals.max() / class_totals[i]
 
 """
 ## Set dataset-dependent hyperparameters
@@ -151,7 +157,7 @@ def preprocess_train(image_path, label):
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image, channels=3)
     image = tf.image.resize(image, (RESIZE_TO, RESIZE_TO))
-    image = tf.image.random_crop(image, [CROP_TO, CROP_TO, 3])
+    image = tf.image.random_crop(image, (CROP_TO, CROP_TO, 3))
     image = tf.cast(image, tf.float32) / 255.0
     return (image, label)
 
@@ -178,18 +184,22 @@ pipeline_train = (
         int(SCHEDULE_LENGTH * BATCH_SIZE / DATASET_NUM_TRAIN_EXAMPLES * STEPS_PER_EPOCH)
         + 1
         + 50
-    )  # repeat dataset_size / num_steps
-    .map(preprocess_train, num_parallel_calls=8)
+    )  # Repeat dataset_size / num_steps
+    .map(preprocess_train, num_parallel_calls=AUTO)
     .batch(BATCH_SIZE)
-    .prefetch(2)
+    .prefetch(AUTO)
 )
 
 # Testing Pipeline
 pipeline_test = (
-    val_ds.map(preprocess_test, num_parallel_calls=1).batch(BATCH_SIZE).prefetch(2)
+    val_ds.map(preprocess_test, num_parallel_calls=AUTO)
+    .batch(BATCH_SIZE)
+    .prefetch(AUTO)
 )
 
-"""## Visualise the Dataset"""
+"""
+## Visualise the Dataset
+"""
 
 image_batch, label_batch = next(iter(pipeline_train))
 
@@ -210,13 +220,12 @@ module = hub.KerasLayer(model_url, trainable=True)
 ## BiT Model
 """
 
-
-class MyBiTModel(tf.keras.Model):
+class MyBiTModel(keras.Model):
     def __init__(self, num_classes, module):
         super().__init__()
 
         self.num_classes = num_classes
-        self.head = tf.keras.layers.Dense(num_classes, kernel_initializer="zeros")
+        self.head = keras.layers.Dense(num_classes, kernel_initializer="zeros")
         self.bit_model = module
 
     def call(self, images):
@@ -231,15 +240,21 @@ model = MyBiTModel(num_classes=NUM_CLASSES, module=module)
 ## Define optimiser and loss
 """
 
-lr = 0.003 * BATCH_SIZE / 512
+learning_rate = 0.003 * BATCH_SIZE / 512
 
 # Decay learning rate by a factor of 10 at SCHEDULE_BOUNDARIES.
-lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-    boundaries=SCHEDULE_BOUNDARIES, values=[lr, lr * 0.1, lr * 0.01, lr * 0.001]
+lr_schedule = keras.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries=SCHEDULE_BOUNDARIES,
+    values=[
+        learning_rate,
+        learning_rate * 0.1,
+        learning_rate * 0.01,
+        learning_rate * 0.001,
+    ],
 )
-optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9)
+optimizer = keras.optimizers.SGD(learning_rate=lr_schedule, momentum=0.9)
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 """
 ## Compile the Model
@@ -247,12 +262,10 @@ loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 model.compile(optimizer=optimizer, loss=loss_fn, metrics=["accuracy"])
 
-"""
-## Setting the Callback
-"""
+"""## Setting the Callback"""
 
 train_callbacks = [
-    tf.keras.callbacks.EarlyStopping(
+    keras.callbacks.EarlyStopping(
         monitor="val_accuracy", patience=2, restore_best_weights=True
     )
 ]
@@ -266,7 +279,7 @@ history = model.fit(
     batch_size=BATCH_SIZE,
     steps_per_epoch=STEPS_PER_EPOCH,
     epochs=int(SCHEDULE_LENGTH / STEPS_PER_EPOCH),
-    class_weight=classWeight,
+    class_weight=class_weight,
     validation_data=pipeline_test,
     callbacks=train_callbacks,
 )
@@ -274,7 +287,6 @@ history = model.fit(
 """
 ## Plot the Model
 """
-
 
 def plot_hist(hist):
     plt.plot(hist.history["accuracy"])
@@ -302,7 +314,7 @@ print("Accuracy: {:.2f}%".format(accuracy))
 """
 
 """
-In this example, we trained our model for 5 epochs. In our experiment, the BigTransfer(BiT) Model performs amazing by giving us a good validation accuracy.
+In this example, we trained our model for 5 epochs. In our experiment, the BigTransfer(BiT) Model performs amazing by giving us a good validation accuracy. BiT performs well across a surprisingly wide range of data regimes -- from 1 example per class to 1M total examples. BiT achieves 87.5% top-1 accuracy on ILSVRC-2012, 99.4% on CIFAR-10, and 76.3% on the 19 task Visual Task Adaptation Benchmark (VTAB). On small datasets, BiT attains 76.8% on ILSVRC-2012 with 10 examples per class, and 97.0% on CIFAR-10 with 10 examples per class.
 
 You can experiment further with the BigTransfer Method by following the [original paper](https://arxiv.org/pdf/1912.11370.pdf).
 """
