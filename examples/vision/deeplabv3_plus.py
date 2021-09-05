c79c1cb545f2804020347cc86ad547e9cd2099cb
soumik12345 <19soumik.rakshit96@gmail.com>
2021-09-05 15:16:33 +0530

    Updated example

--- examples/vision/deeplabv3_plus.py
+++ examples/vision/deeplabv3_plus.py
@@ -7,16 +7,20 @@ Description: Implement DeepLabV3+ architecture for Multi-class Semantic Segmenta
 """
 """
 ## Introduction
-"""
 
-"""
 Semantic segmentation with the goal to assign semantic labels to every pixel in an image
 is one of the fundamental topics in computer vision. Deep convolutional neural networks
 based on the Fully Convolutional Neural Network show striking improvement over systems
 relying on hand-crafted features on benchmark tasks. In this example, we would implement
-the **DeepLabV3+** model as proposed by the paper [Encoder-Decoder with Atrous Separable
-Convolution for Semantic Image Segmentation](https://arxiv.org/pdf/1802.02611.pdf) for
-multi-class semantic segmentation.
+the **DeepLabV3+** model for multi-class semantic segmentation.
+
+References:
+
+- [Encoder-Decoder with Atrous Separable Convolution for Semantic Image Segmentation](https://arxiv.org/pdf/1802.02611.pdf)
+
+- [Rethinking Atrous Convolution for Semantic Image Segmentation](https://arxiv.org/abs/1706.05587)
+
+- [DeepLab: Semantic Image Segmentation with Deep Convolutional Nets, Atrous Convolution, and Fully Connected CRFs](https://arxiv.org/abs/1606.00915)
 """
 
 """
@@ -24,27 +28,25 @@ multi-class semantic segmentation.
 """
 
 import os
+from re import X
 import cv2
 import numpy as np
 from glob import glob
-import tensorflow as tf
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
 """shell
-!gdown https://drive.google.com/uc?id=1B9A9UCJYMwTL4oBEo4RZfbMZMaZhKJaz
-!unzip -q instance-level-human-parsing.zip
-"""
-
-"""
-
+gdown https://drive.google.com/uc?id=1B9A9UCJYMwTL4oBEo4RZfbMZMaZhKJaz
+unzip -q instance-level-human-parsing.zip
 """
 
 """
 ## Building Tensorflow Dataset
-"""
 
-"""
 For this example, we would be using 200 images from the instance-level human parsing
 dataset to train our model.
 """
@@ -65,12 +67,11 @@ def read_image(image_path, mask=False):
         image = tf.image.decode_png(image, channels=1)
         image.set_shape([None, None, 1])
         image = tf.image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
-        image = tf.cast(image, tf.float32)
     else:
         image = tf.image.decode_png(image, channels=3)
         image.set_shape([None, None, 3])
         image = tf.image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
-        image = tf.cast(image, tf.float32) / 127.5 - 1
+        image = image / 127.5 - 1
     return image
 
 
@@ -84,8 +85,6 @@ def data_generator(image_list, mask_list):
     dataset = tf.data.Dataset.from_tensor_slices((image_list, mask_list))
     dataset = dataset.map(load_data, num_parallel_calls=tf.data.AUTOTUNE)
     dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
-    dataset = dataset.repeat(1)
-    dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
 
 
@@ -109,10 +108,10 @@ along object boundaries.
 """
 
 """
-**Atrous Convolution:** With Atrous convolution, as we go deeper, we can keep the stride
-constant but with larger field-of-view without increasing the number of parameters or the
-amount of computation. And finally, we can have larger output feature map which is good
-for semantic segmentation.
+**Dilated Convolution:** With Dilated convolution, as we go deeper, we can keep the
+stride constant but with larger field-of-view without increasing the number of parameters
+or the amount of computation. And finally, we can have larger output feature map which is
+good for semantic segmentation.
 
 The reason for using **Atrous Spatial Pyramid Pooling** is that it is discovered as the
 sampling rate becomes larger, the number of valid filter weights (i.e., the weights that
@@ -122,47 +121,45 @@ are applied to the valid feature region, instead of padded zeros) becomes smalle
 
 def convolution_block(
     block_input,
-    n_filters=256,
+    num_filters=256,
     kernel_size=3,
     dilation_rate=1,
     padding="same",
     use_bias=False,
 ):
 
-    x = tf.keras.layers.Conv2D(
-        n_filters,
-        kernel_size=1,
-        dilation_rate=1,
+    x = layers.Conv2D(
+        num_filters,
+        kernel_size=kernel_size,
+        dilation_rate=dilation_rate,
         padding="same",
         use_bias=use_bias,
-        kernel_initializer=tf.keras.initializers.HeNormal(),
+        kernel_initializer=keras.initializers.HeNormal(),
     )(block_input)
-    x = tf.keras.layers.BatchNormalization()(x)
+    x = layers.BatchNormalization()(x)
     return tf.nn.relu(x)
 
 
 def AtrousSpatialPyramidPooling(aspp_input):
 
-    dims = tf.keras.backend.int_shape(aspp_input)
+    dims = aspp_input.shape
 
-    layer = tf.keras.layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(aspp_input)
-    layer = convolution_block(layer, kernel_size=1, use_bias=True)
+    x = layers.AveragePooling2D(pool_size=(dims[-3], dims[-2]))(aspp_input)
+    x = convolution_block(x, kernel_size=1, use_bias=True)
 
-    out_pool = tf.keras.layers.UpSampling2D(
-        size=(dims[-3] // layer.shape[1], dims[-2] // layer.shape[2]),
+    out_pool = layers.UpSampling2D(
+        size=(dims[-3] // x.shape[1], dims[-2] // x.shape[2]),
         interpolation="bilinear",
-    )(layer)
+    )(x)
 
     out_1 = convolution_block(aspp_input, kernel_size=1, dilation_rate=1)
     out_6 = convolution_block(aspp_input, kernel_size=3, dilation_rate=6)
     out_12 = convolution_block(aspp_input, kernel_size=3, dilation_rate=12)
     out_18 = convolution_block(aspp_input, kernel_size=3, dilation_rate=18)
 
-    layer = tf.keras.layers.Concatenate(axis=-1)(
-        [out_pool, out_1, out_6, out_12, out_18]
-    )
+    x = layers.Concatenate(axis=-1)([out_pool, out_1, out_6, out_12, out_18])
 
-    output = convolution_block(layer, kernel_size=1)
+    output = convolution_block(x, kernel_size=1)
 
     return output
 
@@ -171,41 +168,42 @@ def AtrousSpatialPyramidPooling(aspp_input):
 The encoder features are first bilinearly upsampled by a factor of 4 and then 
 concatenated with the corresponding low-level features from the network backbone that
 have the same spatial resolution. For this example, we would be 
-using Resnet101 pre-trained on ImageNet as the backbone model and we would use 
+using Resnet50 pre-trained on ImageNet as the backbone model and we would use 
 the low-level features from the Conv2 block of the backbone.
 """
 
 
-def DeeplabV3Plus():
-    model_input = tf.keras.Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3))
-    resnet101 = tf.keras.applications.ResNet101(
-        weights='imagenet', include_top=False, input_tensor=model_input)
-    layer = resnet101.get_layer('conv4_block6_2_relu').output
-    layer = AtrousSpatialPyramidPooling(layer)
-    
-    input_a = tf.keras.layers.UpSampling2D(
-        size=(
-            IMAGE_SIZE // 4 // layer.shape[1],
-            IMAGE_SIZE // 4 // layer.shape[2]
-        ), interpolation = 'bilinear'
-    )(layer)
-    
-    input_b = resnet101.get_layer('conv2_block3_2_relu').output
-    input_b = convolution_block(input_b, n_filters=48, kernel_size=1)
-    
-    layer = tf.keras.layers.Concatenate(axis=-1)([input_a, input_b])
-    layer = convolution_block(layer)
-    layer = convolution_block(layer)
-    layer = tf.keras.layers.UpSampling2D(
-        size=(
-            IMAGE_SIZE // layer.shape[1],
-            IMAGE_SIZE // layer.shape[2]
-        ), interpolation='bilinear')(layer)
-    
-    model_output = tf.keras.layers.Conv2D(
-        NUM_CLASSES, kernel_size=(1, 1), padding='same')(layer)
-    
-    return tf.keras.Model(inputs=model_input, outputs=model_output)
+def DeeplabV3Plus(image_size, num_classes):
+    model_input = keras.Input(shape=(image_size, image_size, 3))
+    resnet101 = keras.applications.ResNet101(
+        weights="imagenet", include_top=False, input_tensor=model_input
+    )
+    x = resnet101.get_layer("conv4_block6_2_relu").output
+    x = AtrousSpatialPyramidPooling(x)
+
+    input_a = layers.UpSampling2D(
+        size=(image_size // 4 // x.shape[1], image_size // 4 // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+
+    input_b = resnet101.get_layer("conv2_block3_2_relu").output
+    input_b = convolution_block(input_b, num_filters=48, kernel_size=1)
+
+    x = layers.Concatenate(axis=-1)([input_a, input_b])
+    x = convolution_block(x)
+    x = convolution_block(x)
+    x = layers.UpSampling2D(
+        size=(image_size // x.shape[1], image_size // x.shape[2]),
+        interpolation="bilinear",
+    )(x)
+
+    model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same")(x)
+
+    return keras.Model(inputs=model_input, outputs=model_output)
+
+
+model = DeeplabV3Plus(image_size=IMAGE_SIZE, num_classes=NUM_CLASSES)
+model.summary()
 
 """
 ## Training
@@ -216,13 +214,13 @@ We would train the model using Sparse Categorical Cross-entropy as the loss func
 use Adam as the optimizer.
 """
 
-loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
     loss=loss,
     metrics=["accuracy"],
 )
-model.fit(dataset, epochs=25)
+history = model.fit(dataset, epochs=25)
 
 plt.plot(history.history["loss"])
 plt.title("Training Loss")
@@ -240,6 +238,7 @@ plt.show()
 ## Inference using Colormap Overlay
 """
 
+# Loading the Colormap
 colormap = loadmat(
     "./instance-level_human_parsing/instance-level_human_parsing/human_colormap.mat"
 )["colormap"]
@@ -254,7 +253,7 @@ def infer(model, image_tensor):
     return predictions
 
 
-def decode_segmask(mask, colormap, n_classes):
+def decode_segmentation_masks(mask, colormap, n_classes):
     r = np.zeros_like(mask).astype(np.uint8)
     g = np.zeros_like(mask).astype(np.uint8)
     b = np.zeros_like(mask).astype(np.uint8)
@@ -288,7 +287,7 @@ def plot_predictions(images_list, colormap, model):
     for image_file in images_list:
         image_tensor = read_image(image_file)
         prediction_mask = infer(image_tensor=image_tensor, model=model)
-        prediction_colormap = decode_segmask(prediction_mask, colormap, 20)
+        prediction_colormap = decode_segmentation_masks(prediction_mask, colormap, 20)
         overlay = get_overlay(image_tensor, prediction_colormap)
         plot_samples_matplotlib(
             [image_tensor, overlay, prediction_colormap], figsize=(18, 14)
