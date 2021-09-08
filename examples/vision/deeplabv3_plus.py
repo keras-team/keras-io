c4f5070e5331aa46b730a5f9b23bb2293d12e62a
soumik12345 <19soumik.rakshit96@gmail.com>
2021-09-08 05:03:47 +0000

    Updated generated files

--- examples/vision/deeplabv3_plus.py
+++ examples/vision/deeplabv3_plus.py
@@ -155,7 +155,7 @@ The encoder features are first bilinearly upsampled by a factor 4, and then
 concatenated with the corresponding low-level features from the network backbone that
 have the same spatial resolution. For this example, we
 use a ResNet101 pretrained on ImageNet as the backbone model, and we use
-the low-level features from the `conv2_block3` block of the backbone.
+the low-level features from the `conv4_block6_2_relu` block of the backbone.
 """
 
 
@@ -164,7 +164,7 @@ def DeeplabV3Plus(image_size, num_classes):
     resnet101 = keras.applications.ResNet101(
         weights="imagenet", include_top=False, input_tensor=model_input
     )
-    x = resnet101.get_layer("conv2_block3_2_relu").output
+    x = resnet101.get_layer("conv4_block6_2_relu").output
     x = DilatedSpatialPyramidPooling(x)
 
     input_a = layers.UpSampling2D(
@@ -181,6 +181,7 @@ def DeeplabV3Plus(image_size, num_classes):
         size=(image_size // x.shape[1], image_size // x.shape[2]),
         interpolation="bilinear",
     )(x)
+
     model_output = layers.Conv2D(num_classes, kernel_size=(1, 1), padding="same")(x)
     return keras.Model(inputs=model_input, outputs=model_output)
 
