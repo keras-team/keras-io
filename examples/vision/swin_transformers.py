7c5e45eedda7a6a1aff3bcdf857094ca523a6582
Rishit Dagli <rishit.dagli@gmail.com>
2021-09-11 07:06:26 +0000

    Follow snake case

--- examples/vision/swin_transformers.py
+++ examples/vision/swin_transformers.py
@@ -102,10 +102,10 @@ patches from the image, allow us to merge patches to spatial frames and dropout.
 
 def window_partition(x, window_size):
     _, height, width, channels = x.shape
-    patch_num_H = height // window_size
-    patch_num_W = width // window_size
+    patch_num_y = height // window_size
+    patch_num_x = width // window_size
     x = tf.reshape(
-        x, shape=(-1, patch_num_H, window_size, patch_num_W, window_size, channels)
+        x, shape=(-1, patch_num_y, window_size, patch_num_x, window_size, channels)
     )
     x = tf.transpose(x, (0, 1, 3, 2, 4, 5))
     windows = tf.reshape(x, shape=(-1, window_size, window_size, channels))
@@ -113,11 +113,11 @@ def window_partition(x, window_size):
 
 
 def window_reverse(windows, window_size, height, width, channels):
-    patch_num_H = height // window_size
-    patch_num_W = width // window_size
+    patch_num_y = height // window_size
+    patch_num_x = width // window_size
     x = tf.reshape(
         windows,
-        shape=(-1, patch_num_H, patch_num_W, window_size, window_size, channels),
+        shape=(-1, patch_num_y, patch_num_x, window_size, window_size, channels),
     )
     x = tf.transpose(x, perm=(0, 1, 3, 2, 4, 5))
     x = tf.reshape(x, shape=(-1, height, width, channels))
