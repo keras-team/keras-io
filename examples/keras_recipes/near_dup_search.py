8a5c87a403e1c77751f8bbe05f6a499a65bc09e9
Sayak Paul <spsayakpaul@gmail.com>
2021-09-13 15:19:55 +0530

    fix: lambda -> normalization

--- examples/keras_recipes/near_dup_search.py
+++ examples/keras_recipes/near_dup_search.py
@@ -272,7 +272,7 @@ class BuildLSHTable:
             # Compute embeddings and update the LSH tables.
             # More on `self.concrete_function()` later.
             if self.concrete_function:
-                features = self.prediction_model(tf.constant(image))["lambda"].numpy()
+                features = self.prediction_model(tf.constant(image))["normalization"].numpy()
             else:
                 features = self.prediction_model.predict(image)
             self.lsh.add(id, features, label)
@@ -283,7 +283,7 @@ class BuildLSHTable:
             image = image[None, ...]
 
         if self.concrete_function:
-            features = self.prediction_model(tf.constant(image))["lambda"].numpy()
+            features = self.prediction_model(tf.constant(image))["normalization"].numpy()
         else:
             features = self.prediction_model.predict(image)
 
