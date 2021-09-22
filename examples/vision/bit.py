f8dc155c217166f5384f34649194756303ed7aef
Sayan Nath <sayannath235@gmail.com>
2021-09-22 11:48:16 +0530

    Used TF-Flower dataset

--- examples/vision/bit.py
+++ examples/vision/bit.py
@@ -1,34 +1,36 @@
 """
 Title: Image Classification using BigTransfer(BiT)
 Author: [Sayan Nath](https://twitter.com/sayannath2350)
-Date created: 2021/09/09
-Last modified: 2021/09/09
+Date created: 2021/09/22
+Last modified: 2021/09/22
 Description: BigTransfer (BiT) State-of-the-art transfer learning for image classification.
 """
 
 """
 ## Introduction
+"""
 
-BigTransfer known as BiT is a State-of-the-art transfer learning method for Image Classification Tasks. A set of pre-trained image models that can be transferred to obtain excellent performance on new datasets, even with only a few examples per class. BiT performs well across a surprisingly wide range of data regimes — from 1 example per class to 1M total examples.
+"""
+BigTransfer known as BiT is a State-of-the-art transfer learning method for Image Classification Tasks. 
+A set of pre-trained image models that can be transferred to obtain excellent performance on new datasets, 
+even with only a few examples per class. BiT performs well across a surprisingly wide range of data regimes — 
+from 1 example per class to 1M total examples.
 """
 
 """
 ## Setup
 """
 
-import re
 import numpy as np
 import pandas as pd
-import requests, zipfile, io
 import matplotlib.pyplot as plt
-from imutils import paths
-from pprint import pprint
-from collections import Counter
-from sklearn.preprocessing import LabelEncoder
 
 import tensorflow as tf
 from tensorflow import keras
 import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+tfds.disable_progress_bar()
 
 SEEDS = 42
 
@@ -36,114 +38,44 @@ np.random.seed(SEEDS)
 tf.random.set_seed(SEEDS)
 
 """
-## Download the dataset
-
-We will be using Image-Scene-Classification dataset which comprises of 30 classes.
+## Gather Flower Dataset
 """
 
-
-def download_dataset(url):
-    r = requests.get(url)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    z.extractall("./")  # Extracting the Data from the zip file
-
-
-download_dataset(
-    "http://data.vision.ee.ethz.ch/ihnatova/camera_scene_detection_train.zip"
+train_ds, validation_ds = tfds.load(
+    "tf_flowers",
+    split=["train[:85%]", "train[85%:]"],
+    as_supervised=True,
 )
 
 """
-## Data parsing
+## Visualise the dataset
 """
 
-image_paths = list(paths.list_images("training"))
-np.random.shuffle(image_paths)
-image_paths[:5]
-
-"""
-## Counting number of images per class
-"""
-
-labels = []
-for image_path in image_paths:
-    label = image_path.split("/")[1]
-    labels.append(label)
-class_count = Counter(labels)
-pprint(class_count)
-
-"""
-As there is an unequal distribution of classes in the dataset. It is classified as imbalance dataset
-"""
+plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(9)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
 
 """
 ## Define hyperparameters
 """
 
-RESIZE_TO = 260
+RESIZE_TO = 384
 CROP_TO = 224
-TRAIN_SPLIT = 0.9
-BATCH_SIZE = 128
-AUTO = tf.data.AUTOTUNE
-NUM_CLASSES = 30
-STEPS_PER_EPOCH = 140
-
-"""
-## Splitting the dataset
-"""
-
-TRAIN_LENGTH = int(len(image_paths) * TRAIN_SPLIT)
-
-train_paths = image_paths[:TRAIN_LENGTH]
-train_labels = labels[:TRAIN_LENGTH]
-validation_paths = image_paths[TRAIN_LENGTH:]
-validation_labels = labels[TRAIN_LENGTH:]
-
-print(len(train_paths), len(validation_paths))
-
-"""
-## Encoding the labels
-"""
-
-label_encoder = LabelEncoder()
-train_labels_encoded = label_encoder.fit_transform(train_labels)
-validation_labels_encoded = label_encoder.transform(validation_labels)
-print(train_labels_encoded[:5])
-
-"""
-Since the dataset has class imbalance issue, it's good to supply class weights while training the model.
-"""
-
-train_labels = keras.utils.to_categorical(train_labels_encoded)
-class_totals = train_labels.sum(axis=0)
-class_weight = dict()
-# Loop over all classes and calculate the class weight
-for i in range(0, len(class_totals)):
-    class_weight[i] = class_totals.max() / class_totals[i]
-
-"""
-## Set dataset-dependent hyperparameters
-"""
-
-DATASET_SIZE = "\u003C20k examples"  # @param ["<20k examples", "20k-500k examples", ">500k examples"]
-
-if DATASET_SIZE == "<20k examples":
-    SCHEDULE_LENGTH = 500
-    SCHEDULE_BOUNDARIES = [200, 300, 400]
-elif DATASET_SIZE == "20k-500k examples":
-    SCHEDULE_LENGTH = 10000
-    SCHEDULE_BOUNDARIES = [3000, 6000, 9000]
-else:
-    SCHEDULE_LENGTH = 20000
-    SCHEDULE_BOUNDARIES = [6000, 12000, 18000]
-
-"""
-## Convert the data into TensorFlow Dataset objects
-"""
-
-train_ds = tf.data.Dataset.from_tensor_slices((train_paths, train_labels_encoded))
-val_ds = tf.data.Dataset.from_tensor_slices(
-    (validation_paths, validation_labels_encoded)
+BATCH_SIZE = 64
+STEPS_PER_EPOCH = 10
+AUTO = tf.data.AUTOTUNE  # optimise the pipeline performance
+NUM_CLASSES = 5  # number of classes
+SCHEDULE_LENGTH = (
+    500  # we will train on lower resolution images and will still attain good results
 )
+SCHEDULE_BOUNDARIES = [
+    200,
+    300,
+    400,
+]  # more the dataset size the schedule length increase
 
 """
 ## Preprocessing helper functions
@@ -153,9 +85,8 @@ SCHEDULE_LENGTH = SCHEDULE_LENGTH * 512 / BATCH_SIZE
 
 
 @tf.function
-def preprocess_train(image_path, label):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
+def preprocess_train(image, label):
+    image = tf.image.random_flip_left_right(image)
     image = tf.image.resize(image, (RESIZE_TO, RESIZE_TO))
     image = tf.image.random_crop(image, (CROP_TO, CROP_TO, 3))
     image = image / 255.0
@@ -163,15 +94,14 @@ def preprocess_train(image_path, label):
 
 
 @tf.function
-def preprocess_test(image_path, label):
-    image = tf.io.read_file(image_path)
-    image = tf.image.decode_jpeg(image, channels=3)
+def preprocess_test(image, label):
     image = tf.image.resize(image, (RESIZE_TO, RESIZE_TO))
     image = image / 255.0
     return (image, label)
 
 
-DATASET_NUM_TRAIN_EXAMPLES = len(train_paths)
+DATASET_NUM_TRAIN_EXAMPLES = tf.data.experimental.cardinality(train_ds).numpy()
+
 repeat_count = int(
     SCHEDULE_LENGTH * BATCH_SIZE / DATASET_NUM_TRAIN_EXAMPLES * STEPS_PER_EPOCH
 )
@@ -190,15 +120,15 @@ pipeline_train = (
     .prefetch(AUTO)
 )
 
-# Testing Pipeline
-pipeline_test = (
-    val_ds.map(preprocess_test, num_parallel_calls=AUTO)
+# Validation Pipeline
+pipeline_validation = (
+    validation_ds.map(preprocess_test, num_parallel_calls=AUTO)
     .batch(BATCH_SIZE)
     .prefetch(AUTO)
 )
 
 """
-## Visualise the dataset
+## Visualise the training samples
 """
 
 image_batch, label_batch = next(iter(pipeline_train))
@@ -207,17 +137,18 @@ plt.figure(figsize=(10, 10))
 for n in range(25):
     ax = plt.subplot(5, 5, n + 1)
     plt.imshow(image_batch[n])
+    plt.title(label_batch[n].numpy())
     plt.axis("off")
 
 """
 ## Load model into KerasLayer
 """
 
-model_url = "https://tfhub.dev/google/bit/m-r50x1/1"
-module = hub.KerasLayer(model_url, trainable=True)
+bit_model_url = "https://tfhub.dev/google/bit/m-r50x1/1"
+bit_module = hub.KerasLayer(bit_model_url, trainable=True)
 
 """
-## BiT Model
+## BigTransfer(BiT) model
 """
 
 
@@ -235,10 +166,10 @@ class MyBiTModel(keras.Model):
         return self.head(bit_embedding)
 
 
-model = MyBiTModel(num_classes=NUM_CLASSES, module=module)
+model = MyBiTModel(num_classes=NUM_CLASSES, module=bit_module)
 
 """
-## Define optimiser and loss
+## Define optimizer and loss
 """
 
 learning_rate = 0.003 * BATCH_SIZE / 512
@@ -280,10 +211,9 @@ train_callbacks = [
 history = model.fit(
     pipeline_train,
     batch_size=BATCH_SIZE,
-    steps_per_epoch=STEPS_PER_EPOCH,
     epochs=int(SCHEDULE_LENGTH / STEPS_PER_EPOCH),
-    class_weight=class_weight,
-    validation_data=pipeline_test,
+    steps_per_epoch=STEPS_PER_EPOCH,
+    validation_data=pipeline_validation,
     callbacks=train_callbacks,
 )
 
@@ -310,15 +240,18 @@ plot_hist(history)
 ## Evaluate the Model
 """
 
-accuracy = model.evaluate(pipeline_test)[1] * 100
+accuracy = model.evaluate(pipeline_validation)[1] * 100
 print("Accuracy: {:.2f}%".format(accuracy))
 
 """
 ## Note:
-"""
 
-"""
-In this example, we trained our model for 5 epochs. In our experiment, the BigTransfer(BiT) Model performs amazing by giving us a good validation accuracy. BiT performs well across a surprisingly wide range of data regimes -- from 1 example per class to 1M total examples. BiT achieves 87.5% top-1 accuracy on ILSVRC-2012, 99.4% on CIFAR-10, and 76.3% on the 19 task Visual Task Adaptation Benchmark (VTAB). On small datasets, BiT attains 76.8% on ILSVRC-2012 with 10 examples per class, and 97.0% on CIFAR-10 with 10 examples per class.
+In this example, we trained our model for 5 epochs. In our experiment, the BigTransfer(BiT) Model performs amazing 
+by giving us a good validation accuracy. BiT performs well across a surprisingly wide range of 
+data regimes -- from 1 example per class to 1M total examples. BiT achieves 87.5% top-1 accuracy on ILSVRC-2012, 99.4% on CIFAR-10, 
+and 76.3% on the 19 task Visual Task Adaptation Benchmark (VTAB). On small datasets, BiT attains 76.8% on ILSVRC-2012 with 10 examples per class, 
+and 97.0% on CIFAR-10 with 10 examples per class.
 
 You can experiment further with the BigTransfer Method by following the [original paper](https://arxiv.org/pdf/1912.11370.pdf).
+TensorFlow Team also published on BigTransfer(BiT). You can find it [here](https://blog.tensorflow.org/2020/05/bigtransfer-bit-state-of-art-transfer-learning-computer-vision.html)
 """
