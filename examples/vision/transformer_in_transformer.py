7fc4435ddc7aab2af18372c8c625918ac464c112
czy00000 <92425024+czy00000@users.noreply.github.com>
2021-11-29 15:18:24 +0800

    Update transformer_in_transformer.py(V2)

--- examples/vision/transformer_in_transformer.py
+++ examples/vision/transformer_in_transformer.py
@@ -2,23 +2,27 @@
 Title: Image classification with TNT(Transformer in Transformer)
 Author: [ZhiYong Chang](https://github.com/czy00000)
 Date created: 2021/10/25
-Last modified: 2021/11/06
+Last modified: 2021/11/29
 Description: Implementing the Transformer in Transformer (TNT) model for image classification.
 """
 
+
 """
 ## Introduction
-This example implements the [TNT](https://arxiv.org/abs/2103.00112)
-model for image classification, and demonstrates it on the CIFAR-100 dataset.
+This example implements the [TNT](https://arxiv.org/pdf/2103.00112v2.pdf)
+model for image classification, and demonstrates it's performance on the CIFAR-100
+dataset.
+To keep training time reasonable, We will train and test a smaller model than is in the
+paper(0.66M params vs 23.8M params).
 TNT is a novel model for modeling both patch-level and pixel-level
-representation. In each TNT block, an ***outer*** transformer block is utilized to
-process
+representation. In each TNT block, an ***outer*** transformer block is utilized to process
 patch embeddings, and an ***inner***
 transformer block extracts local features from pixel embeddings. The pixel-level
 feature is projected to the space of patch embedding by a linear transformation layer
 and then added into the patch.
 This example requires TensorFlow 2.5 or higher, as well as
-[TensorFlow Addons](https://www.tensorflow.org/addons/overview) package,
+[TensorFlow Addons](https://www.tensorflow.org/addons/overview) package, it is for the
+AdamW optimizer,
 which can be installed using the following command:
 ```python
 pip install -U tensorflow-addons
@@ -56,34 +60,41 @@ print(f"x_test shape: {x_test.shape} - y_test shape: {y_test.shape}")
 ## Configure the hyperparameters
 """
 
-weight_decay = 0.0001
+weight_decay = 0.0002
 learning_rate = 0.001
 label_smoothing = 0.1
 validation_split = 0.2
 batch_size = 128
-image_size = 96  # resize images to this size
+image_size = (96, 96)  # resize images to this size
+patch_size = (8, 8)
 num_epochs = 50
+outer_block_embedding_dim = 64
+inner_block_embedding_dim = 32
+num_transformer_layer = 5
+outer_block_num_heads = 4
+inner_block_num_heads = 2
+mlp_ratio = 4
+attention_dropout = 0.5
+projection_dropout = 0.5
+first_stride = 4
 
 """
 ## Use data augmentation
 """
 
-data_augmentation = keras.Sequential(
-    [
-        layers.Normalization(),
-        layers.Resizing(image_size, image_size),
-        layers.RandomFlip("horizontal"),
-        layers.RandomRotation(factor=0.1),
-        layers.RandomContrast(factor=0.1),
-        layers.RandomZoom(height_factor=0.2, width_factor=0.2),
-    ],
-    name="data_augmentation",
-)
-# Compute the mean and the variance of the training data for normalization.
-data_augmentation.layers[0].adapt(x_train)
+
+def data_augmentation(inputs):
+    x = layers.Rescaling(scale=1.0 / 255)(inputs)
+    x = layers.Resizing(image_size[0], image_size[1])(x)
+    x = layers.RandomFlip("horizontal")(x)
+    x = layers.RandomRotation(factor=0.1)(x)
+    x = layers.RandomContrast(factor=0.1)(x)
+    x = layers.RandomZoom(height_factor=0.2, width_factor=0.2)(x)
+    return x
+
 
 """
-## Implement the pixel embedding layer
+## Implement the pixel embedding and patch embedding layer
 """
 
 
@@ -97,29 +108,47 @@ class PatchEncoder(layers.Layer):
         )
 
     def call(self, patch):
-        positions = tf.range(start=0, limit=self.num_patches, delta=1)
+        positions = tf.range(start=0, limit=self.num_patches)
         encoded = self.projection(patch) + self.position_embedding(positions)
         return encoded
 
 
-def pixel_embed(x, image_size=96, patch_size=8, in_dim=48, stride=4):
+def pixel_embed(x, image_size=image_size, patch_size=patch_size, in_dim=48, stride=4):
     _, channel, height, width = x.shape
-    image_size = tuple(repeat(image_size, 2))
-    patch_size = tuple(repeat(patch_size, 2))
     num_patches = (image_size[0] // patch_size[0]) * (image_size[1] // patch_size[1])
-    new_patch_size = [math.ceil(ps / stride) for ps in patch_size]
+    inner_patch_size = [math.ceil(ps / stride) for ps in patch_size]
     x = layers.Conv2D(in_dim, kernel_size=7, strides=stride, padding="same")(x)
     # pixel extraction
     x = tf.image.extract_patches(
         images=x,
-        sizes=(1, new_patch_size[0], new_patch_size[1], 1),
-        strides=(1, new_patch_size[0], new_patch_size[1], 1),
+        sizes=(1, inner_patch_size[0], inner_patch_size[1], 1),
+        strides=(1, inner_patch_size[0], inner_patch_size[1], 1),
         rates=(1, 1, 1, 1),
         padding="VALID",
     )
-    x = tf.reshape(x, shape=(-1, new_patch_size[0] * new_patch_size[1], in_dim))
-    x = PatchEncoder(new_patch_size[0] * new_patch_size[1], in_dim)(x)
-    return x, num_patches, new_patch_size
+    x = tf.reshape(x, shape=(-1, inner_patch_size[0] * inner_patch_size[1], in_dim))
+    x = PatchEncoder(inner_patch_size[0] * inner_patch_size[1], in_dim)(x)
+    return x, num_patches, inner_patch_size
+
+
+def patch_embed(
+    pixel_embedding,
+    num_patches,
+    outer_block_embedding_dim,
+    inner_block_embedding_dim,
+    num_pixels,
+):
+    patch_embedding = tf.reshape(
+        pixel_embedding, shape=(-1, num_patches, inner_block_embedding_dim * num_pixels)
+    )
+    patch_embedding = layers.LayerNormalization(epsilon=1e-5)(patch_embedding)
+    patch_embedding = layers.Dense(outer_block_embedding_dim)(patch_embedding)
+    patch_embedding = layers.LayerNormalization(epsilon=1e-5)(patch_embedding)
+    patch_embedding = PatchEncoder(num_patches, outer_block_embedding_dim)(
+        patch_embedding
+    )
+    patch_embedding = layers.Dropout(projection_dropout)(patch_embedding)
+    return patch_embedding
 
 
 """
@@ -127,10 +156,10 @@ def pixel_embed(x, image_size=96, patch_size=8, in_dim=48, stride=4):
 """
 
 
-def mlp(x, mlp_dim, embedding_dim, drop_rate=0.2):
-    x = layers.Dense(mlp_dim, activation=tf.nn.gelu)(x)
+def mlp(x, hidden_dim, output_dim, drop_rate=0.2):
+    x = layers.Dense(hidden_dim, activation=tf.nn.gelu)(x)
     x = layers.Dropout(drop_rate)(x)
-    x = layers.Dense(embedding_dim)(x)
+    x = layers.Dense(output_dim)(x)
     x = layers.Dropout(drop_rate)(x)
     return x
 
@@ -145,12 +174,12 @@ def transformer_in_transformer_block(
     patch_embedding,
     out_embedding_dim,
     in_embedding_dim,
-    num_pixel,
-    out_num_heads=8,
-    in_num_heads=4,
-    mlp_ratio=4,
-    attention_dropout=0,
-    projection_dropout=0,
+    num_pixels,
+    out_num_heads,
+    in_num_heads,
+    mlp_ratio,
+    attention_dropout,
+    projection_dropout,
 ):
     # inner transformer block
     residual_in_1 = pixel_embedding
@@ -168,13 +197,13 @@ def transformer_in_transformer_block(
 
     # outer transformer block
     _, num_patches, channel = patch_embedding.shape
-    # fusion local and global information
-    fusion_embedding = tf.reshape(
-        pixel_embedding, shape=(-1, num_patches, in_embedding_dim * num_pixel)
+    # fuse local and global information
+    fused_embedding = tf.reshape(
+        pixel_embedding, shape=(-1, num_patches, in_embedding_dim * num_pixels)
     )
-    fusion_embedding = layers.LayerNormalization(epsilon=1e-5)(fusion_embedding)
-    fusion_embedding = layers.Dense(out_embedding_dim)(fusion_embedding)
-    patch_embedding = layers.add([patch_embedding, fusion_embedding])
+    fused_embedding = layers.LayerNormalization(epsilon=1e-5)(fused_embedding)
+    fused_embedding = layers.Dense(out_embedding_dim)(fused_embedding)
+    patch_embedding = layers.add([patch_embedding, fused_embedding])
     residual_out_1 = patch_embedding
     patch_embedding = layers.LayerNormalization(epsilon=1e-5)(patch_embedding)
     patch_embedding = layers.MultiHeadAttention(
@@ -199,44 +228,41 @@ and the inner one extracts local structure information of pixel embeddings.
 The local information is added on the patch
 embedding by linearly projecting the pixel embeddings into the space of patch embedding.
 Patch-level and pixel-level position embeddings are introduced in order to
-retain spatial information.In orginal paper, the authors use the class token for
+retain spatial information. In orginal paper, the authors use the class token for
 classification.
-We use the `layers.GlobalAvgPool1D` to fusion patch information.
+We use the `layers.GlobalAvgPool1D` to fuse patch information.
 """
 
 
 def get_model(
-    image_size=96,
-    patch_size=8,
-    outer_block_embedding_dim=64,  # outer transformer block embedding dim
-    inner_block_embedding_dim=16,  # inner transformer block embedding dim
-    num_transformer_layer=5,
-    outer_block_num_heads=4,
-    inner_block_num_heads=2,
-    mlp_ratio=4,
-    attention_dropout=0.2,
-    projection_dropout=0.2,
-    first_stride=4,
+    image_size=image_size,
+    patch_size=patch_size,
+    outer_block_embedding_dim=outer_block_embedding_dim,
+    inner_block_embedding_dim=inner_block_embedding_dim,
+    num_transformer_layer=num_transformer_layer,
+    outer_block_num_heads=outer_block_num_heads,
+    inner_block_num_heads=inner_block_num_heads,
+    mlp_ratio=mlp_ratio,
+    attention_dropout=attention_dropout,
+    projection_dropout=projection_dropout,
+    first_stride=first_stride,
 ):
     inputs = layers.Input(shape=input_shape)
     # Image augment
     x = data_augmentation(inputs)
     # extract pixel embedding
-    pixel_embedding, num_patches, new_patch_size = pixel_embed(
+    pixel_embedding, num_patches, inner_patch_size = pixel_embed(
         x, image_size, patch_size, inner_block_embedding_dim, first_stride
     )
-    num_pixel = new_patch_size[0] * new_patch_size[1]
+    num_pixels = inner_patch_size[0] * inner_patch_size[1]
     # extract patch embedding
-    patch_embedding = tf.reshape(
-        pixel_embedding, shape=(-1, num_patches, inner_block_embedding_dim * num_pixel)
+    patch_embedding = patch_embed(
+        pixel_embedding,
+        num_patches,
+        outer_block_embedding_dim,
+        inner_block_embedding_dim,
+        num_pixels,
     )
-    patch_embedding = layers.LayerNormalization(epsilon=1e-5)(patch_embedding)
-    patch_embedding = layers.Dense(outer_block_embedding_dim)(patch_embedding)
-    patch_embedding = layers.LayerNormalization(epsilon=1e-5)(patch_embedding)
-    patch_embedding = PatchEncoder(num_patches, outer_block_embedding_dim)(
-        patch_embedding
-    )
-    patch_embedding = layers.Dropout(projection_dropout)(patch_embedding)
     # create multiple layers of the TNT block.
     for _ in range(num_transformer_layer):
         pixel_embedding, patch_embedding = transformer_in_transformer_block(
@@ -244,7 +270,7 @@ def get_model(
             patch_embedding,
             outer_block_embedding_dim,
             inner_block_embedding_dim,
-            num_pixel,
+            num_pixels,
             outer_block_num_heads,
             inner_block_num_heads,
             mlp_ratio,
@@ -263,6 +289,7 @@ def get_model(
 """
 
 model = get_model()
+model.summary()
 model.compile(
     loss=keras.losses.CategoricalCrossentropy(label_smoothing=label_smoothing),
     optimizer=tfa.optimizers.AdamW(
@@ -314,13 +341,15 @@ print(f"Test accuracy: {round(accuracy * 100, 2)}%")
 print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
 
 """
-After 50 epochs, the TNT model achieves around 48% accuracy and
-78% top-5 accuracy on the test data. It only has 0.8M parameters.
+After 50 epochs, the TNT model achieves around 42% accuracy and
+73% top-5 accuracy on the test data. It only has 0.6M parameters.
 From the above loss curve, we can find that the model gradually converges,
-but it never achieves state of the art performance. We can apply some data augmentation
-to
-obtain better performance, like RandAugment, MixUp etc. We also can adjust the depth
-of model, learning rate or increase the size of embedding.Compared to the conventional
+but it never achieves state of the art performance. We could apply further data
+augmentation to
+obtain better performance, like [RandAugment](https://arxiv.org/abs/1909.13719),
+[MixUp](https://arxiv.org/abs/1710.09412)
+etc. We also can adjust the depth of model, learning rate or increase the size of
+embedding. Compared to the conventional
 vision transformers [(ViT)](https://arxiv.org/abs/2010.11929) which corrupts the local
 structure
 of the patch, the TNT can better preserve and model the local information
