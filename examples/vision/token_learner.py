716c80da96b2e2f665b46944293970b916dd1ee6
ariG23498 <aritra.born2fly@gmail.com>
2021-12-10 21:58:52 +0530

    fix: remove duplicate link

--- examples/vision/token_learner.py
+++ examples/vision/token_learner.py
@@ -209,7 +209,6 @@ def mlp(x, dropout_rate, hidden_units):
 
 The following figure presents a pictorial overview of the module
 ([source](https://ai.googleblog.com/2021/12/improving-vision-transformer-efficiency.html)).
-([source](https://ai.googleblog.com/2021/12/improving-vision-transformer-efficiency.html)).
 
 ![TokenLearner module GIF](https://blogger.googleusercontent.com/img/a/AVvXsEiylT3_nmd9-tzTnz3g3Vb4eTn-L5sOwtGJOad6t2we7FsjXSpbLDpuPrlInAhtE5hGCA_PfYTJtrIOKfLYLYGcYXVh1Ksfh_C1ZC-C8gw6GKtvrQesKoMrEA_LU_Gd5srl5-3iZDgJc1iyCELoXtfuIXKJ2ADDHOBaUjhU8lXTVdr2E7bCVaFgVHHkmA=w640-h208)
 
