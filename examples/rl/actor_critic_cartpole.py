2183e6ca299c75ab4e2f597acc05bc44790e85e6
Falak <falaktheoptimist@users.noreply.github.com>
2022-01-06 19:02:38 +0530

    Fixing critic model to use action output along with state

--- examples/rl/actor_critic_cartpole.py
+++ examples/rl/actor_critic_cartpole.py
@@ -72,7 +72,7 @@ num_hidden = 128
 inputs = layers.Input(shape=(num_inputs,))
 common = layers.Dense(num_hidden, activation="relu")(inputs)
 action = layers.Dense(num_actions, activation="softmax")(common)
-critic = layers.Dense(1)(common)
+critic = layers.Dense(1)(layers.Concatenate(axis=-1)([common, action]))
 
 model = keras.Model(inputs=inputs, outputs=[action, critic])
 
