4ffde7cb21576e9e363cbd690ed56ff0d5b2d534
ariG23498 <aritra.born2fly@gmail.com>
2022-01-12 23:58:10 +0530

    add: vivit

---
+++ examples/vision/vivit.py
@@ -0,0 +1,445 @@
+"""
+Title: Video Vision Transformer
+Author: [Aritra Roy Gosthipaty](https://twitter.com/ariG23498), [Ayush Thakur](https://twitter.com/ayushthakur0) (equal contribution)
+Date created: 2022/01/12
+Last modified:  2022/01/12
+Description: A pure transformer based architecture for video classification
+"""
+"""
+<a href="https://colab.research.google.com/github/ariG23498/ViViT/blob/ayut/ViViT.ipynb"
+target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg"
+alt="Open In Colab"/></a>
+"""
+
+"""
+## Introduction
+
+Videos are a sequence of images. Let's assume you have an image
+representation model (CNNs, ViTs, etc.) and a sequence model
+(RNNs, LSTMs, etc.) at hand. We ask you to tweak the models for video
+classification. The immediate thought would be to apply the image
+model to individual frames, then use the sequence model to learn the
+order of the image representation. Applying a classification head on
+the learned sequence representation completes the video classification model.
+[Video Classification with a CNN-RNN Architecture](https://keras.io/examples/vision/video_classification/)
+explains this approach in detail. Taking a step ahead, you can also
+build a hybrid Transformer-based model for video classification as shown in
+[Video Classification with Transformers](https://keras.io/examples/vision/video_transformers/).
+
+In this example, we minimally implement
+[ViViT: A Video Vision Transformer](https://arxiv.org/abs/2103.15691)
+by Arnab et al. The authors propose a **pure-transformer** based model
+for video classification. The authors propose a novel embedding scheme
+and many variants of Transformers to model on video clips. We implement
+the embedding scheme and one of the variants of the transformer
+architecture for simplicity.
+
+This example requires TensorFlow 2.6 or higher, and the medmnist
+python package can be installed by running the code cell below. 
+"""
+
+"""shell
+!pip install -qq medmnist
+"""
+
+"""
+## Imports
+"""
+
+import os
+import io
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+from medmnist import INFO
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from IPython.display import Image, display
+from ipywidgets import widgets, HTML
+
+# setting seed for reproducibility
+SEED = 42
+os.environ["TF_CUDNN_DETERMINISTIC"] = "1"
+keras.utils.set_random_seed(SEED)
+
+"""
+## Hyperparameters
+
+The hyperparameters are chosen specifically based on a hyperparameter
+search. You can find more on this in the Conclusion section. 
+"""
+
+# DATA
+DATASET_NAME = "organmnist3d"
+BATCH_SIZE = 32
+AUTO = tf.data.AUTOTUNE
+INPUT_SHAPE = (28, 28, 28, 1)
+NUM_CLASSES = 11
+
+# OPTIMIZER
+LEARNING_RATE = 1e-4
+WEIGHT_DECAY = 1e-5
+
+# TRAINING
+EPOCHS = 60
+
+# TUBELET EMBEDDING
+PATCH_SIZE = (8, 8, 8)
+NUM_PATCHES = (INPUT_SHAPE[0] // PATCH_SIZE[0]) ** 2
+
+# ViViT ARCHITECTURE
+LAYER_NORM_EPS = 1e-6
+PROJECTION_DIM = 128
+NUM_HEADS = 8
+NUM_LAYERS = 8
+
+"""
+## Dataset
+
+For our example we use the
+[MedMNIST v2: A Large-Scale Lightweight Benchmark for 2D and 3D Biomedical Image Classification](https://medmnist.com/)
+dataset. The videos are lightweight and easy to train on.
+"""
+
+
+def download_and_prepare_dataset(data_info: dict):
+    """
+    Utility function to download the dataset and return train/valid/test
+    videos and labels.
+
+    Arguments:
+        data_info (dict): Dataset metadata
+    """
+    data_path = keras.utils.get_file(origin=data_info["url"], md5_hash=data_info["MD5"])
+
+    with np.load(data_path) as data:
+        # Get videos
+        train_videos = data["train_images"]
+        valid_videos = data["val_images"]
+        test_videos = data["test_images"]
+
+        # Get labels
+        train_labels = data["train_labels"].flatten()
+        valid_labels = data["val_labels"].flatten()
+        test_labels = data["test_labels"].flatten()
+
+    return (
+        (train_videos, train_labels),
+        (valid_videos, valid_labels),
+        (test_videos, test_labels),
+    )
+
+
+# Get the metadata of the dataset
+info = INFO[DATASET_NAME]
+
+# Get the dataset
+prepared_dataset = download_and_prepare_dataset(info)
+(train_videos, train_labels) = prepared_dataset[0]
+(valid_videos, valid_labels) = prepared_dataset[1]
+(test_videos, test_labels) = prepared_dataset[2]
+
+"""
+### `tf.data` pipeline
+"""
+
+
+@tf.function
+def preprocess(frames: tf.Tensor, label: tf.Tensor):
+    """
+    Preprocess the frames tensors and parse the labels
+    """
+    # Preprocess images
+    frames = tf.image.convert_image_dtype(
+        frames[
+            ..., tf.newaxis
+        ],  # the new axis is to help for further processing with Conv3D layers
+        tf.float32,
+    )
+
+    # Parse label
+    label = tf.cast(label, tf.float32)
+    return frames, label
+
+
+def prepare_dataloader(
+    videos: np.ndarray,
+    labels: np.ndarray,
+    loader_type: str = "train",
+    batch_size: int = BATCH_SIZE,
+):
+    """
+    Utility function to prepare dataloader.
+    """
+    dataset = tf.data.Dataset.from_tensor_slices((videos, labels))
+
+    if loader_type == "train":
+        dataset = dataset.shuffle(1024)
+
+    dataloader = (
+        dataset.map(preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
+    return dataloader
+
+
+trainloader = prepare_dataloader(train_videos, train_labels, "train")
+validloader = prepare_dataloader(valid_videos, valid_labels, "valid")
+testloader = prepare_dataloader(test_videos, test_labels, "test")
+
+"""
+## Tubelet Embedding
+
+In ViTs an image is divided into patches which is then spatially
+flattened and projected as a tokenization scheme. For a video one can
+repeat this process for individual frames. **Uniform frame sampling**
+as suggested by the authors is a tokenization scheme in which we
+sample frames from the video clip and perform simple ViT tokenization.
+
+| ![uniform frame sampling](https://i.imgur.com/aaPyLPX.png) |
+| :--: |
+| Uniform Frame Sampling [Source](https://arxiv.org/abs/2103.15691) |
+
+**Tubelet Embedding** is different in terms of capturing the temporal
+information. From the video we extract volumes. These volumes contain
+patches of the frame and the temporal information as well. The volumes
+are then flattened and projected to build video tokens.
+
+| ![tubelet embedding](https://i.imgur.com/9G7QTfV.png) |
+| :--: |
+| Tubelet Embedding [Source](https://arxiv.org/abs/2103.15691) |
+"""
+
+
+class TubeletEmbedding(layers.Layer):
+    def __init__(self, embed_dim=PROJECTION_DIM, patch_size=PATCH_SIZE):
+        super().__init__()
+        self.projection = layers.Conv3D(
+            filters=embed_dim,
+            kernel_size=patch_size,
+            strides=patch_size,
+            padding="VALID",
+        )
+        self.flatten = layers.Reshape(target_shape=(-1, embed_dim))
+
+    def call(self, videos):
+        projected_patches = self.projection(videos)
+        flattened_patches = self.flatten(projected_patches)
+        return flattened_patches
+
+
+"""
+## Positional Embedding
+
+This layer adds positional information to encoded video tokens.
+"""
+
+
+class PositionalEncoder(layers.Layer):
+    def __init__(self, embed_dim=PROJECTION_DIM):
+        super().__init__()
+        self.embed_dim = embed_dim
+
+    def build(self, input_shape):
+        _, num_tokens, _ = input_shape
+        self.position_embedding = layers.Embedding(
+            input_dim=num_tokens, output_dim=self.embed_dim
+        )
+        self.positions = tf.range(start=0, limit=num_tokens, delta=1)
+
+    def call(self, encoded_tokens):
+        # encode the positions and add it to the encoded tokens
+        encoded_positions = self.position_embedding(self.positions)
+        encoded_tokens = encoded_tokens + encoded_positions
+        return encoded_tokens
+
+
+"""
+## Video Vision Transformer
+
+The authors suggest 4 variants of Vision Transformer:
+
+- Spatio-temporal attention
+- Factorised encoder
+- Factorised self-attention
+- Factorised dot-product attention
+
+In this example, we will implement the **Spatio-temporal attention**
+model for simplicity. The following code snippet is heavily inspired from
+[Image classification with Vision Transformer](https://keras.io/examples/vision/image_classification_with_vision_transformer/). 
+"""
+
+
+def create_vivit_classifier(
+    tubelet_embedder,
+    positional_encoder,
+    transformer_layers=NUM_LAYERS,
+    num_heads=NUM_HEADS,
+    embed_dim=PROJECTION_DIM,
+):
+
+    # Get the input layer
+    inputs = layers.Input(shape=INPUT_SHAPE)
+    # Create patches.
+    patches = tubelet_embedder(inputs)
+    # Encode patches.
+    encoded_patches = positional_encoder(patches)
+
+    # Create multiple layers of the Transformer block.
+    for _ in range(transformer_layers):
+        # Layer normalization and MHSA
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim // num_heads, dropout=0.1
+        )(x1, x1)
+
+        # Skip connection
+        x2 = layers.Add()([attention_output, encoded_patches])
+
+        # Layer Normalization and MLP
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        x3 = keras.Sequential(
+            [
+                layers.Dense(units=embed_dim, activation=tf.nn.gelu),
+                layers.Dense(units=embed_dim, activation=tf.nn.gelu),
+            ]
+        )(x3)
+
+        # Skip connection
+        encoded_patches = layers.Add()([x3, x2])
+
+    # Layer normalization and Global average pooling.
+    representation = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(encoded_patches)
+    representation = layers.GlobalAvgPool1D()(representation)
+
+    # Classify outputs.
+    outputs = layers.Dense(NUM_CLASSES, activation="softmax")(representation)
+
+    # Create the Keras model.
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+"""
+## Train
+"""
+
+
+def run_experiment():
+    # Initialize model
+    model = create_vivit_classifier(
+        tubelet_embedder=TubeletEmbedding(),
+        positional_encoder=PositionalEncoder(),
+    )
+
+    # Compile the model with the optimizer, loss function
+    # and the metrics.
+    optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+    model.compile(
+        optimizer=optimizer,
+        loss="sparse_categorical_crossentropy",
+        metrics=[
+            keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+            keras.metrics.SparseTopKCategoricalAccuracy(5, name="top-5-accuracy"),
+        ],
+    )
+
+    # Train the model.
+    _ = model.fit(trainloader, epochs=EPOCHS, validation_data=validloader)
+
+    _, accuracy, top_5_accuracy = model.evaluate(testloader)
+    print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+    print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
+
+    return model
+
+
+model = run_experiment()
+
+"""
+## Inference
+"""
+
+NUM_SAMPLES_VIZ = 25
+testsamples, labels = next(iter(testloader))
+testsamples, labels = testsamples[:NUM_SAMPLES_VIZ], labels[:NUM_SAMPLES_VIZ]
+
+ground_truths = []
+preds = []
+videos = []
+
+for i, (testsample, label) in enumerate(zip(testsamples, labels)):
+    # Generate gif
+    with io.BytesIO() as gif:
+        imageio.mimsave(gif, (testsample.numpy() * 255).astype("uint8"), "GIF", fps=5)
+        videos.append(gif.getvalue())
+
+    # Get model prediction
+    output = model.predict(tf.expand_dims(testsample, axis=0))[0]
+    pred = np.argmax(output, axis=0)
+
+    ground_truths.append(label.numpy().astype("int"))
+    preds.append(pred)
+
+
+def make_box_for_grid(image_widget, fit):
+    """
+    Make a VBox to hold caption/image for demonstrating
+    option_fit values.
+
+    Source: https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20Styling.html
+    """
+    # Make the caption
+    if fit is not None:
+        fit_str = "'{}'".format(fit)
+    else:
+        fit_str = str(fit)
+
+    h = HTML(value="" + str(fit_str) + "")
+
+    # Make the green box with the image widget inside it
+    boxb = widgets.Box()
+    boxb.children = [image_widget]
+
+    # Compose into a vertical box
+    vb = widgets.VBox()
+    vb.layout.align_items = "center"
+    vb.children = [h, boxb]
+    return vb
+
+
+boxes = []
+for i in range(NUM_SAMPLES_VIZ):
+    ib = widgets.Image(value=videos[i], width=100, height=100)
+    true_class = info["label"][str(ground_truths[i])]
+    pred_class = info["label"][str(preds[i])]
+    caption = f"T: {true_class} | P: {pred_class}"
+
+    boxes.append(make_box_for_grid(ib, caption))
+
+widgets.GridBox(boxes, layout=widgets.Layout(grid_template_columns="repeat(5, 200px)"))
+
+"""
+## Final Thoughts
+
+With a vanilla implementation we achieve ~75-77% Top-1 accuracy on the test dataset. 
+
+Places to improve:
+
+- Using data augmentation.
+- Using a better regularization scheme for training.
+- Apply different variants of the transformer model.
+
+The hyperparameters used in this tutorial were finalized by running a hyperparameter
+search using [W&B Sweeps](https://docs.wandb.ai/guides/sweeps). You can find out our
+sweeps result [here](https://wandb.ai/minimal-implementations/vivit/sweeps/66fp0lhz) and
+our quick analysis of the results
+[here](https://wandb.ai/minimal-implementations/vivit/reports/Hyperparameter-Tuning-Analysis--VmlldzoxNDEwNzcx).
+[here](https://wandb.ai/minimal-implementations/vivit/reports/Hyperparameter-Tuning-Analysis--VmlldzoxNDEwNzcx).
+
+We are grateful to [Weights and Biases](https://wandb.ai/site) program for helping with
+GPU credits.
+"""
