c7ad8322a0dcea7f82b9c2dd4c9f2dcd20c227d4
François Chollet <francois.chollet@gmail.com>
2022-01-16 13:50:50 -0800

    Copyedits

--- examples/vision/vivit.py
+++ examples/vision/vivit.py
@@ -3,37 +3,38 @@ Title: Video Vision Transformer
 Author: [Aritra Roy Gosthipaty](https://twitter.com/ariG23498), [Ayush Thakur](https://twitter.com/ayushthakur0) (equal contribution)
 Date created: 2022/01/12
 Last modified:  2022/01/12
-Description: A pure transformer based architecture for video classification
+Description: A Transformer-based architecture for video classification.
 """
 """
 ## Introduction
 
-Videos are a sequence of images. Let's assume you have an image
-representation model (CNNs, ViTs, etc.) and a sequence model
-(RNNs, LSTMs, etc.) at hand. We ask you to tweak the models for video
-classification. The immediate thought would be to apply the image
-model to individual frames, then use the sequence model to learn the
-order of the image representation. Applying a classification head on
-the learned sequence representation completes the video classification model.
+Videos are sequences of images. Let's assume you have an image
+representation model (CNN, ViT, etc.) and a sequence model
+(RNN, LSTM, etc.) at hand. We ask you to tweak the model for video
+classification. The simplest approach would be to apply the image
+model to individual frames, use the sequence model to learn
+sequences of image features, then apply a classification head on
+the learned sequence representation.
+The Keras example
 [Video Classification with a CNN-RNN Architecture](https://keras.io/examples/vision/video_classification/)
-explains this approach in detail. Taking a step ahead, you can also
-build a hybrid Transformer-based model for video classification as shown in
+explains this approach in detail. Alernatively, you can also
+build a hybrid Transformer-based model for video classification as shown in the Keras example
 [Video Classification with Transformers](https://keras.io/examples/vision/video_transformers/).
 
 In this example, we minimally implement
 [ViViT: A Video Vision Transformer](https://arxiv.org/abs/2103.15691)
-by Arnab et al. The authors propose a **pure-transformer** based model
+by Arnab et al., a **pure Transformer-based** model
 for video classification. The authors propose a novel embedding scheme
-and many variants of Transformers to model on video clips. We implement
-the embedding scheme and one of the variants of the transformer
-architecture for simplicity.
+and a number of Transformer variants to model video clips. We implement
+the embedding scheme and one of the variants of the Transformer
+architecture, for simplicity.
 
-This example requires TensorFlow 2.6 or higher, and the medmnist
-python package can be installed by running the code cell below.
+This example requires TensorFlow 2.6 or higher, and the `medmnist`
+package, which can be installed by running the code cell below.
 """
 
 """shell
-!pip install -qq medmnist
+pip install -qq medmnist
 """
 
 """
@@ -58,8 +59,8 @@ keras.utils.set_random_seed(SEED)
 """
 ## Hyperparameters
 
-The hyperparameters are chosen specifically based on a hyperparameter
-search. You can find more on this in the Conclusion section.
+The hyperparameters are chosen via hyperparameter
+search. You can learn more about the process in the "conclusion" section.
 """
 
 # DATA
@@ -96,12 +97,10 @@ dataset. The videos are lightweight and easy to train on.
 
 
 def download_and_prepare_dataset(data_info: dict):
-    """
-    Utility function to download the dataset and return train/valid/test
-    videos and labels.
+    """Utility function to download the dataset.
 
     Arguments:
-        data_info (dict): Dataset metadata
+        data_info (dict): Dataset metadata.
     """
     data_path = keras.utils.get_file(origin=data_info["url"], md5_hash=data_info["MD5"])
 
@@ -139,7 +138,7 @@ prepared_dataset = download_and_prepare_dataset(info)
 
 @tf.function
 def preprocess(frames: tf.Tensor, label: tf.Tensor):
-    """Preprocess the frames tensors and parse the labels"""
+    """Preprocess the frames tensors and parse the labels."""
     # Preprocess images
     frames = tf.image.convert_image_dtype(
         frames[
@@ -147,7 +146,6 @@ def preprocess(frames: tf.Tensor, label: tf.Tensor):
         ],  # The new axis is to help for further processing with Conv3D layers
         tf.float32,
     )
-
     # Parse label
     label = tf.cast(label, tf.float32)
     return frames, label
@@ -159,7 +157,7 @@ def prepare_dataloader(
     loader_type: str = "train",
     batch_size: int = BATCH_SIZE,
 ):
-    """Utility function to prepare dataloader"""
+    """Utility function to prepare the dataloader."""
     dataset = tf.data.Dataset.from_tensor_slices((videos, labels))
 
     if loader_type == "train":
@@ -170,7 +168,6 @@ def prepare_dataloader(
         .batch(batch_size)
         .prefetch(tf.data.AUTOTUNE)
     )
-
     return dataloader
 
 
@@ -181,8 +178,8 @@ testloader = prepare_dataloader(test_videos, test_labels, "test")
 """
 ## Tubelet Embedding
 
-In ViTs an image is divided into patches which is then spatially
-flattened and projected as a tokenization scheme. For a video one can
+In ViTs, an image is divided into patches, which are then spatially
+flattened, a process known as tokenization. For a video, one can
 repeat this process for individual frames. **Uniform frame sampling**
 as suggested by the authors is a tokenization scheme in which we
 sample frames from the video clip and perform simple ViT tokenization.
@@ -191,10 +188,11 @@ sample frames from the video clip and perform simple ViT tokenization.
 | :--: |
 | Uniform Frame Sampling [Source](https://arxiv.org/abs/2103.15691) |
 
-**Tubelet Embedding** is different in terms of capturing the temporal
-information. From the video we extract volumes. These volumes contain
+**Tubelet Embedding** is different in terms of capturing temporal
+information from the video.
+First, we extract volumes from the video -- these volumes contain
 patches of the frame and the temporal information as well. The volumes
-are then flattened and projected to build video tokens.
+are then flattened to build video tokens.
 
 | ![tubelet embedding](https://i.imgur.com/9G7QTfV.png) |
 | :--: |
@@ -222,7 +220,7 @@ class TubeletEmbedding(layers.Layer):
 """
 ## Positional Embedding
 
-This layer adds positional information to encoded video tokens.
+This layer adds positional information to the encoded video tokens.
 """
 
 
@@ -251,16 +249,16 @@ class PositionalEncoder(layers.Layer):
 The authors suggest 4 variants of Vision Transformer:
 
 - Spatio-temporal attention
-- Factorised encoder
-- Factorised self-attention
-- Factorised dot-product attention
+- Factorized encoder
+- Factorized self-attention
+- Factorized dot-product attention
 
 In this example, we will implement the **Spatio-temporal attention**
 model for simplicity. The following code snippet is heavily inspired from
 [Image classification with Vision Transformer](https://keras.io/examples/vision/image_classification_with_vision_transformer/).
 One can also refer to the
 [official repository of ViViT](https://github.com/google-research/scenic/tree/main/scenic/projects/vivit)
-to look at all the variants of transformers in jax.
+which contains all the variants, implemented in JAX.
 """
 
 
@@ -274,7 +272,6 @@ def create_vivit_classifier(
     layer_norm_eps=LAYER_NORM_EPS,
     num_classes=NUM_CLASSES,
 ):
-
     # Get the input layer
     inputs = layers.Input(shape=input_shape)
     # Create patches.
@@ -382,9 +379,7 @@ for i, (testsample, label) in enumerate(zip(testsamples, labels)):
 
 
 def make_box_for_grid(image_widget, fit):
-    """
-    Make a VBox to hold caption/image for demonstrating
-    option_fit values.
+    """Make a VBox to hold caption/image for demonstrating option_fit values.
 
     Source: https://ipywidgets.readthedocs.io/en/latest/examples/Widget%20Styling.html
     """
@@ -421,9 +416,9 @@ ipywidgets.widgets.GridBox(
 )
 
 """
-## Final Thoughts
+## Final thoughts
 
-With a vanilla implementation we achieve ~79-80% Top-1 accuracy on the
+With a vanilla implementation, we achieve ~79-80% Top-1 accuracy on the
 test dataset.
 
 The hyperparameters used in this tutorial were finalized by running a
@@ -434,7 +429,7 @@ You can find out our sweeps result
 and our quick analysis of the results
 [here](https://wandb.ai/minimal-implementations/vivit/reports/Hyperparameter-Tuning-Analysis--VmlldzoxNDEwNzcx).
 
-The following are the places of improvement:
+For further improvement, you could look into the following:
 
 - Using data augmentation for videos.
 - Using a better regularization scheme for training.
