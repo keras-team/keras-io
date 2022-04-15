3e497bf009ab4e96989500efa90ba67a61da98f4
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2022-04-15 23:55:59 +0530

    Update Tweet-classification-using-TFDF.py

--- examples/nlp/Tweet-classification-using-TFDF.py
+++ examples/nlp/Tweet-classification-using-TFDF.py
@@ -1,9 +1,31 @@
 """
-Title: FILLME
-Author: FILLME
-Date created: FILLME
-Last modified: FILLME
-Description: FILLME
+Title: Text Classification using TFDF and Pre-trained embeddings
+Author: Gitesh Chawda
+Date created: 15/04/2022
+Last modified: 15/04/2022
+Description: Using Tensorflow Decision Forest for text classification
+"""
+"""
+## Introduction
+
+[TensorFlow Decision Forests](https://www.tensorflow.org/decision_forests) (TF-DF)
+provide powerful models, especially with structured data,
+It is a collection of state-of-the-art algorithms of Decision Forest models that are
+compatible with Keras APIs. The module includes Random Forests, Gradient Boosted Trees,
+and CART, and can be used for regression, classification, and ranking tasks.
+
+Alternatively for getting started
+In this example we will use Gradient Boosted Trees with pre-trained embeddings to
+classify disaster tweets.
+"""
+
+"""
+Install Tensorflow Decision Forest using following command : 
+`!pip3 install -q tensorflow_decision_forests`
+"""
+
+"""
+## Imports
 """
 
 import pandas as pd
@@ -13,67 +35,104 @@ from tensorflow import keras
 import tensorflow_hub as hub
 from tensorflow.keras import layers
 import tensorflow_decision_forests as tfdf
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.metrics import Recall, Precision, Accuracy, AUC
+import matplotlib.pyplot as plt
+
+"""
+## Get the data
+
+Dataset is avaliable at [kaggle](https://www.kaggle.com/c/nlp-getting-started)
+
+dataset description :
+
+1. Files
+
+    a. train.csv - the training set
+
+2. Columns
+
+    a. id - a unique identifier for each tweet
+
+    b. text - the text of the tweet
+
+    c. location - the location the tweet was sent from (may be blank)
+
+    d. keyword - a particular keyword from the tweet (may be blank)
+
+e. target - in train.csv only, this denotes whether a tweet is about a real disaster (1)
+or not (0)
+"""
 
 # Turn .csv files into pandas DataFrame's
 train_df = pd.read_csv(
     "https://raw.githubusercontent.com/IMvision12/Tweets-Classification-NLP/main/train.csv"
-    "https://raw.githubusercontent.com/IMvision12/Tweets-Classification-NLP/main/train.csv"
 )
-test_df = pd.read_csv(
-    "https://raw.githubusercontent.com/IMvision12/Tweets-Classification-NLP/main/test.csv"
-)
-train_df.head()
+print(train_df.head())
 
-train_df.shape
+# Printing shape of the training dataset
+print(train_df.shape)
 
+# Shuffling training data
 train_df_shuffled = train_df.sample(frac=1, random_state=42)
+# Dropping id, keyword and location columns as these columns consists of mostly nan values
+# we will be using only text and target columns
 train_df_shuffled.drop(["id", "keyword", "location"], axis=1, inplace=True)
 train_df_shuffled.reset_index(inplace=True, drop=True)
-train_df_shuffled.head()
+print(train_df_shuffled.head())
 
-train_df_shuffled.info()
+print(train_df_shuffled.info())
 
-train_df_shuffled.target.value_counts()
+# Printing total number of Disaster and non-Disaster tweets
+print(train_df_shuffled.target.value_counts())
 
-# How many samples total?
-print(f"Total training samples: {len(train_df)}")
-print(f"Total test samples: {len(test_df)}")
-print(f"Total samples: {len(train_df) + len(test_df)}")
-
+# Viewing 5 records from training data
 for ind, counter in enumerate(train_df_shuffled.index):
     print(f"Target : {train_df_shuffled['target'][ind]}")
     print(f"Text : {train_df_shuffled['text'][ind]}")
     if counter == 5:
         break
 
-# Use train_test_split to split training data into training and validation sets
-train_sentences, val_sentences, train_labels, val_labels = train_test_split(
-    train_df_shuffled["text"].to_numpy(),
-    train_df_shuffled["target"].to_numpy(),
-    test_size=0.2,  # dedicate 10% of samples to validation set
-    random_state=42,
-)  # random state for reproducibility
+# Splitting dataset into train and test
+test_df = train_df_shuffled.sample(frac=0.2, random_state=42)
+train_df = train_df_shuffled.drop(test_df.index)
 
-print(f"train_sentences shape: {train_sentences.shape}")
-print(f"train_labels shape: {train_labels.shape}")
-print(f"val_sentences shape: {val_sentences.shape}")
-print(f"val_labels shape: {val_labels.shape}")
+print(f"Using {len(train_df)} samples for training and {len(test_df)} for validation")
+
+"""
+## Convert data to tf.Dataset
+"""
 
 
-def create_dataset(sentences, labels, training=False):
-    dataset = tf.data.Dataset.from_tensor_slices((sentences, labels))
+def create_dataset(dataframe, training=False):
+    df = dataframe.copy()
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (df["text"].to_numpy(), df["target"].to_numpy())
+    )
+
     if training:
         dataset = dataset.repeat()
         dataset = dataset.shuffle(1000)
+
     dataset = dataset.batch(32)
     dataset = dataset.prefetch(tf.data.AUTOTUNE)
     return dataset
 
 
-train_ds = create_dataset(train_sentences, train_labels, training=True)
-val_ds = train_ds = create_dataset(val_sentences, val_labels)
+train_ds = create_dataset(train_df, training=True)
+test_ds = create_dataset(test_df)
+
+"""
+## Downloading Pre-trained Embeddings
+
+The Universal Sentence Encoder encodes text into high-dimensional vectors that can be
+used for text classification, semantic similarity, clustering and other natural language
+tasks.It is trained on a variety of data sources and a variety of tasks. The input is
+variable length English text and the output is a 512 dimensional vector.
+
+To understand better about pre-trained embeddings : [Universal Sentence
+Encoder](https://tfhub.dev/google/universal-sentence-encoder/4)
+
+
+"""
 
 sentence_encoder_layer = hub.KerasLayer(
     "https://tfhub.dev/google/universal-sentence-encoder/4",
@@ -83,51 +142,74 @@ sentence_encoder_layer = hub.KerasLayer(
     name="USE",
 )
 
-inputs = layers.Input(shape=(32,), dtype=tf.string)
+"""
+## Build a model
+
+First, we develop a preprocessor model that takes the inputs and passes it through
+sentence encoder layer to generate embedding, which produces a 512 dimensional vector for
+each sentence.
+
+The Preprocessor is then passed to the GradientBoostedTreesModel which will be used for
+predicting disaster tweets.
+
+
+"""
+
+inputs = layers.Input(shape=(), dtype=tf.string)
+# Passing sentences to sentence_encoder_layer
 x = sentence_encoder_layer(inputs)
-preprocessor = tf.keras.Model(inputs=inputs, outputs=x)
+preprocessor = keras.Model(inputs=inputs, outputs=x)
 
 model = tfdf.keras.GradientBoostedTreesModel(
     preprocessing=preprocessor,
-    num_trees=1000,
+    num_trees=30,
     max_vocab_count=1000,
     max_depth=100,
     min_examples=10,
 )
-model.compile(metrics=["accuracy"])
-
-model.fit(x=train_ds)
-
-print("Evaluation :", model.evaluate(val_ds))
-
-
-def calc_metrics(y_true, y_pred):
-    recall, precision, accuracy, auc = Recall(), Precision(), Accuracy(), AUC()
-    recall.update_state(y_true, y_pred), precision.update_state(y_true, y_pred)
-    accuracy.update_state(y_true, y_pred), auc.update_state(y_true, y_pred)
-    results = {
-        "recall": recall.result().numpy(),
-        "precision": precision.result().numpy(),
-        "Accuracy": accuracy.result().numpy(),
-        "AUC": auc.result().numpy(),
-    }
-
-    return results
-
-
-preds = model.predict(val_sentences)
-preds = tf.squeeze(tf.round(preds))
-print(calc_metrics(val_labels, preds))
 
 """
-## Test Set
+## Train the model
 """
 
-test_df.drop(["id", "keyword", "location"], axis=1, inplace=True)
-test_df.reset_index(inplace=True, drop=True)
-test_df.head()
-test_df.head()
+# Compiling model
+model.compile(metrics=["Accuracy", "Recall", "Precision", "AUC"])
+# Training the model
+model.fit(x=train_ds, steps_per_epoch=200)
 
-preds = model.predict(test_df)
-preds = tf.squeeze(tf.round(preds))
-print(preds[:5])
+# Prints training logs
+model.make_inspector().training_logs()
+
+"""
+The `model.summary()` function returns a variety of information about your decision trees
+model, including model type, task, input features, and feature importance.
+
+In our the inputs to the GradientBoostedTreesModel are 512 dimensional vectors so, it
+prints all information of those vectors.
+"""
+
+print(model.summary())
+
+logs = model.make_inspector().training_logs()
+plt.figure(figsize=(12, 4))
+
+plt.subplot(1, 2, 1)
+plt.plot([log.num_trees for log in logs], [log.evaluation.accuracy for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Accuracy")
+
+plt.subplot(1, 2, 2)
+plt.plot([log.num_trees for log in logs], [log.evaluation.loss for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Loss")
+
+plt.show()
+
+"""
+## Evaluating on test data
+"""
+
+results = model.evaluate(test_ds, return_dict=True, verbose=0)
+
+for name, value in results.items():
+    print(f"{name}: {value:.4f}")
