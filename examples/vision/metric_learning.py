3d4c5eef6b5f7eaa1d2b9820418ed8bc122b6403
vrindaprabhu <vrinde91@gmail.com>
2022-06-13 21:26:54 +0530

    Updated the code files with HF links as per example

--- examples/vision/metric_learning.py
+++ examples/vision/metric_learning.py
@@ -311,8 +311,8 @@ disp.plot(include_values=True, cmap="viridis", ax=None, xticks_rotation="vertica
 plt.show()
 
 """
-Example available in HuggingFace.  
-  
+Example available in HuggingFace.
+
 | Trained Model | Demo |
 | :--: | :--: |
 | [![Generic badge](https://img.shields.io/badge/🤗%20Model-Cifar10%20Metric%20Learning-black.svg)](https://huggingface.co/keras-io/cifar10_metric_learning) | [![Generic badge](https://img.shields.io/badge/🤗%20Spaces-Metric%20Learning%20for%20Image%20Similarity%20Search-black.svg)](https://huggingface.co/spaces/keras-io/metric-learning-image-similarity-search) |
