27b3ca15462f760a035f70781598803388deb8c6
Harshvardhan Srivastava <srivastavahv@gmail.com>
2022-08-26 23:19:01 +0530

    added black reformatting

--- examples/nlp/pretraining_BERT.py
+++ examples/nlp/pretraining_BERT.py
@@ -375,7 +375,10 @@ def prepare_train_features(examples):
 
 
 tokenized_dataset = dataset.map(
-    prepare_train_features, batched=True, remove_columns=["text"], num_proc=1,
+    prepare_train_features,
+    batched=True,
+    remove_columns=["text"],
+    num_proc=1,
 )
 
 """
