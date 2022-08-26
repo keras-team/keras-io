9425e7b922193f8e4d14134128a1ff996e4b6585
François Chollet <francois.chollet@gmail.com>
2022-08-26 12:33:56 -0700

    Update img clf example

--- examples/vision/image_classification_from_scratch.py
+++ examples/vision/image_classification_from_scratch.py
@@ -34,11 +34,11 @@ First, let's download the 786M ZIP archive of the raw data:
 """
 
 """shell
-curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip
+curl -O https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_5340.zip
 """
 
 """shell
-unzip -q kagglecatsanddogs_3367a.zip
+unzip -q kagglecatsanddogs_5340.zip
 ls
 """
 
