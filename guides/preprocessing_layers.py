e396fc2e67964b490362883e2ed56d90d65514b4
Guosheng Zhang <zgs0628@gmail.com>
2022-09-07 06:51:56 -0700

    Fix typo (#1053)
    * Fix typo
    
    * Update preprocessing_layers.md
    
    * Update preprocessing_layers.ipynb

--- guides/preprocessing_layers.py
+++ guides/preprocessing_layers.py
@@ -196,7 +196,7 @@ you should put the `TextVectorization` layer in the `tf.data` pipeline to get th
 
 **When running on a TPU, you should always place preprocessing layers in the `tf.data` pipeline**
 (with the exception of `Normalization` and `Rescaling`, which run fine on a TPU and are commonly
-used as the first layer is an image model).
+used as the first layer in an image model).
 """
 
 """
