3d183dc5ac33e41e2af9d906889e5b0e1bb3d8b4
Neel Kovelamudi <nkovela@google.com>
2022-09-26 12:08:40 -0700

    Updated with requested changes

--- examples/keras_recipes/trainer_pattern.py
+++ examples/keras_recipes/trainer_pattern.py
@@ -2,8 +2,15 @@
 Title: Trainer pattern
 Author: [nkovela1](https://nkovela1.github.io/)
 Date created: 2022/09/19
-Last modified: 2022/09/19
-Description: Demonstration of sharing train_step across multiple Keras Models.
+Last modified: 2022/09/23
+Description: Guide on how to share a custom training step across multiple Keras models.
+"""
+"""
+## Introduction
+
+This example shows how to create a custom training step using the Trainer pattern, which can then be shared across multiple Keras models. This pattern overrides the train_step method of the keras.Model class, allowing for the addition of custom loss functions and metrics.
+
+The Trainer pattern can also easily be adapted to more complex models with larger custom training steps, such as this [end-to-end GAN model](https://keras.io/guides/customizing_what_happens_in_fit/#wrapping-up-an-endtoend-gan-example), by putting the custom training step in the Trainer class definition.
 """
 
 """
@@ -11,9 +18,10 @@ Description: Demonstration of sharing train_step across multiple Keras Models.
 """
 
 import tensorflow as tf
+from tensorflow import keras
 
 # Load MNIST dataset and normalize
-mnist = tf.keras.datasets.mnist
+mnist = keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
@@ -23,7 +31,7 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 A custom training step can be created by overriding the train_step method of a Model subclass.
 """ 
-class MyTrainer(tf.keras.Model):
+class MyTrainer(keras.Model):
     def __init__(self, model):
         super().__init__()
         self.model = model
@@ -31,10 +39,27 @@ class MyTrainer(tf.keras.Model):
     def train_step(self, data):
         x, y = data
         with tf.GradientTape() as tape:
-            y_pred = self.model(x, training=True)
-            loss = self.compiled_loss(y, y_pred)
-        self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
-        return self.compute_metrics(x, y, y_pred, None)
+            y_pred = self.model(x, training=True) # Forward pass
+            # Compute loss value
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+        # Update metrics configured in `compile()`
+        self.compiled_metrics.update_state(y, y_pred)
+
+        # Return a dict mapping metric names to current value.
+        return {m.name: m.result() for m in self.metrics}
+    
+    def call(self, x):
+	# Equivalent to `call()` of the wrapped keras.Model
+	x = self.model(x)
+	return x
 
 """
 ## Define multiple models to share the custom training step
@@ -42,22 +67,22 @@ class MyTrainer(tf.keras.Model):
 Let's define two different models that can share our Trainer class and its custom train_step
 """
 
-# Defined using Sequential API
-MyModel = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
+# A model defined using Sequential API
+MyModel = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(256, activation='relu'),
+    keras.layers.Dropout(0.2),
+    keras.layers.Dense(10, activation='softmax')
 ])
 
-# Defined using Functional API
-func_input = tf.keras.Input(shape=(28,28,1))
-x = tf.keras.layers.Flatten(input_shape=(28,28))(func_input)
-x = tf.keras.layers.Dense(512, activation='relu')(x)
-x = tf.keras.layers.Dropout(0.4)(x)
-func_output = tf.keras.layers.Dense(10, activation='softmax')(x)
+# A model defined using Functional API
+func_input = keras.Input(shape=(28,28,1))
+x = keras.layers.Flatten(input_shape=(28,28))(func_input)
+x = keras.layers.Dense(512, activation='relu')(x)
+x = keras.layers.Dropout(0.4)(x)
+func_output = keras.layers.Dense(10, activation='softmax')(x)
 
-YourModel = tf.keras.Model(func_input, func_output)
+YourModel = keras.Model(func_input, func_output)
 
 """
 ## Create Trainer class objects from the models
@@ -68,10 +93,10 @@ trainer_2 = MyTrainer(YourModel)
 """
 ## Compile and fit the models to the MNIST dataset
 """
-trainer_1.compile(tf.keras.optimizers.SGD(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+trainer_1.compile(keras.optimizers.SGD(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 trainer_1.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_test, y_test))
 
-trainer_2.compile(tf.keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+trainer_2.compile(keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 trainer_2.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_test, y_test))
 
 
