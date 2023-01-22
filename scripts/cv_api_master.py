19cccccc448efd90da019503742ebf204abcee1b
Luke Wood <LukeWood@users.noreply.github.com>
2023-01-22 00:08:26 -0500

    Update API symbols + genapi specs (#1206)
    * Begin updating the CV Api
    
    * Begin updating the CV Api
    
    * Begin updating the CV Api
    
    * Syntax fix

--- scripts/cv_api_master.py
+++ scripts/cv_api_master.py
@@ -1,6 +1,6 @@
-PREPROCESSING_MASTER = {
-    "path": "preprocessing/",
-    "title": "Preprocessing layers",
+AUGMENTATION_MASTER = {
+    "path": "augmentation/",
+    "title": "Augmentation layers",
     "toc": True,
     "children": [
         {
@@ -24,19 +24,9 @@ PREPROCESSING_MASTER = {
             "generate": ["keras_cv.layers.CutMix"],
         },
         {
-            "path": "equalization",
-            "title": "Equalization layer",
-            "generate": ["keras_cv.layers.Equalization"],
-        },
-        {
             "path": "fourier_mix",
             "title": "FourierMix layer",
             "generate": ["keras_cv.layers.FourierMix"],
-        },
-        {
-            "path": "grayscale",
-            "title": "Grayscale layer",
-            "generate": ["keras_cv.layers.Grayscale"],
         },
         {
             "path": "grid_mask",
@@ -44,14 +34,14 @@ PREPROCESSING_MASTER = {
             "generate": ["keras_cv.layers.GridMask"],
         },
         {
+            "path": "jittered_resize",
+            "title": "JitteredResize layer",
+            "generate": ["keras_cv.layers.JitteredResize"],
+        },
+        {
             "path": "mix_up",
             "title": "MixUp layer",
             "generate": ["keras_cv.layers.MixUp"],
-        },
-        {
-            "path": "posterization",
-            "title": "Posterization layer",
-            "generate": ["keras_cv.layers.Posterization"],
         },
         {
             "path": "rand_augment",
@@ -106,6 +96,34 @@ PREPROCESSING_MASTER = {
     ],
 }
 
+PREPROCESSING_MASTER = {
+    "path": "preprocessing/",
+    "title": "Preprocessing layers",
+    "toc": True,
+    "children": [
+        {
+            "path": "resizing",
+            "title": "Resizing layer",
+            "generate": ["keras_cv.layers.Resizing"],
+        },
+        {
+            "path": "grayscale",
+            "title": "Grayscale layer",
+            "generate": ["keras_cv.layers.Grayscale"],
+        },
+        {
+            "path": "equalization",
+            "title": "Equalization layer",
+            "generate": ["keras_cv.layers.Equalization"],
+        },
+        {
+            "path": "posterization",
+            "title": "Posterization layer",
+            "generate": ["keras_cv.layers.Posterization"],
+        },
+    ],
+}
+
 BOUNDING_BOX_FORMATS = {
     "path": "formats",
     "title": "Bounding box formats",
@@ -137,6 +155,21 @@ BOUNDING_BOX_UTILS = {
             "title": "Clip bounding boxes to be within the bounds of provided images",
             "generate": ["keras_cv.bounding_box.clip_to_image"],
         },
+        {
+            "path": "to_dense",
+            "title": "Convert a bounding box dictionary to -1 padded Dense tensors",
+            "generate": ["keras_cv.bounding_box.to_dense"],
+        },
+        {
+            "path": "to_ragged",
+            "title": "Convert a bounding box dictionary batched Ragged tensors",
+            "generate": ["keras_cv.bounding_box.to_ragged"],
+        },
+        {
+            "path": "validate_format",
+            "title": "Ensure that your bounding boxes comply with the bounding box spec",
+            "generate": ["keras_cv.bounding_box.validate_format"],
+        },
     ],
 }
 
@@ -158,7 +191,35 @@ REGULARIZATION_MASTER = {
             "generate": [
                 "keras_cv.layers.DropBlock2D",
             ],
-        }
+        },
+        {
+            "path": "drop_path",
+            "title": "DropPath layer",
+            "generate": [
+                "keras_cv.layers.DropPath",
+            ],
+        },
+        {
+            "path": "squeeze_and_excite_2d",
+            "title": "SqueezeAndExcite2D layer",
+            "generate": [
+                "keras_cv.layers.SqueezeAndExcite2D",
+            ],
+        },
+        {
+            "path": "squeeze_and_excite_2d",
+            "title": "SqueezeAndExcite2D layer",
+            "generate": [
+                "keras_cv.layers.SqueezeAndExcite2D",
+            ],
+        },
+        {
+            "path": "stochastic_depth",
+            "title": "StochasticDepth layer",
+            "generate": [
+                "keras_cv.layers.StochasticDepth",
+            ],
+        },
     ],
 }
 
@@ -166,31 +227,32 @@ LAYERS_MASTER = {
     "path": "layers/",
     "title": "Layers",
     "toc": True,
-    "children": [PREPROCESSING_MASTER, REGULARIZATION_MASTER],
+    "children": [AUGMENTATION_MASTER, PREPROCESSING_MASTER, REGULARIZATION_MASTER],
 }
 
-
-METRICS_MASTER = {
-    "path": "metrics/",
-    "title": "Metrics",
-    "toc": True,
-    "children": [
-        {
-            "path": "coco_mean_average_precision",
-            "title": "COCOMeanAveragePrecision metric",
-            "generate": [
-                "keras_cv.metrics.COCOMeanAveragePrecision",
-            ],
-        },
-        {
-            "path": "coco_recall",
-            "title": "COCORecall metric",
-            "generate": [
-                "keras_cv.metrics.COCORecall",
-            ],
-        },
-    ],
-}
+#
+# METRICS_MASTER = {
+#     "path": "metrics/",
+#     "title": "Metrics",
+#     "toc": True,
+#     "children": [
+#         # Temporarily remove COCO metrics
+#         # {
+#         #     "path": "coco_mean_average_precision",
+#         #     "title": "COCOMeanAveragePrecision metric",
+#         #     "generate": [
+#         #         "keras_cv.metrics.COCOMeanAveragePrecision",
+#         #     ],
+#         # },
+#         # {
+#         #     "path": "coco_recall",
+#         #     "title": "COCORecall metric",
+#         #     "generate": [
+#         #         "keras_cv.metrics.COCORecall",
+#         #     ],
+#         # },
+#     ],
+# }
 
 MODELS_MASTER = {
     "path": "models/",
@@ -212,6 +274,24 @@ MODELS_MASTER = {
             ],
         },
         {
+            "path": "faster_rcnn",
+            "title": "The FasterRCNN model",
+            "generate": ["keras_cv.models.FasterRCNN"],
+        },
+        {
+            "path": "efficientnetv2",
+            "title": "EfficientNetV2 models",
+            "generate": [
+                "keras_cv.models.EfficientNetV2B0",
+                "keras_cv.models.EfficientNetV2B1",
+                "keras_cv.models.EfficientNetV2B2",
+                "keras_cv.models.EfficientNetV2B3",
+                "keras_cv.models.EfficientNetV2S",
+                "keras_cv.models.EfficientNetV2M",
+                "keras_cv.models.EfficientNetV2L",
+            ],
+        },
+        {
             "path": "densenet",
             "title": "DenseNet models",
             "generate": [
@@ -227,5 +307,5 @@ CV_API_MASTER = {
     "path": "keras_cv/",
     "title": "KerasCV",
     "toc": True,
-    "children": [LAYERS_MASTER, METRICS_MASTER, MODELS_MASTER, BOUNDING_BOX_MASTER],
+    "children": [LAYERS_MASTER, MODELS_MASTER, BOUNDING_BOX_MASTER],
 }
