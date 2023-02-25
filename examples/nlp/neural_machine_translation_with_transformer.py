Fix padding mask broadcasting for encoder transformer (#1254)
* Fix padding mask broadcasting for encoder transformer

* Fix Translation example

* Fix Translation example

examples/nlp/neural_machine_translation_with_transformer.py
@@ -2,7 +2,7 @@
 Title: English-to-Spanish translation with a sequence-to-sequence Transformer
 Author: [fchollet](https://twitter.com/fchollet)
 Date created: 2021/05/26
-Last modified: 2021/05/26
+Last modified: 2022/02/25
 Description: Implementing a sequence-to-sequene Transformer and training it on a machine translation task.
 Accelerator: GPU
 """
@@ -239,7 +239,7 @@ class TransformerEncoder(layers.Layer):
 
     def call(self, inputs, mask=None):
         if mask is not None:
-            padding_mask = tf.cast(mask[:, tf.newaxis, tf.newaxis, :], dtype="int32")
+            padding_mask = tf.cast(mask[:, tf.newaxis, :], dtype="int32")
         attention_output = self.attention(
             query=inputs, value=inputs, key=inputs, attention_mask=padding_mask
         )