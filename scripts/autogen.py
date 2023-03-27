57368519837c37fe542a8b8fa2758d691a39d486
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-03-27 19:59:12 +0000

    udpate keras-tuner version to 1.3.3 (#1298)

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -40,7 +40,7 @@ EXAMPLES_GH_LOCATION = Path("keras-team") / "keras-io" / "blob" / "master" / "ex
 GUIDES_GH_LOCATION = Path("keras-team") / "keras-io" / "blob" / "master" / "guides"
 PROJECT_URL = {
     "keras": "https://github.com/keras-team/keras/tree/v2.12.0/",
-    "keras_tuner": "https://github.com/keras-team/keras-tuner/tree/v1.3.0/",
+    "keras_tuner": "https://github.com/keras-team/keras-tuner/tree/v1.3.3/",
     "keras_cv": "https://github.com/keras-team/keras-cv/tree/v0.4.2/",
     "keras_nlp": "https://github.com/keras-team/keras-nlp/tree/v0.4.1/",
 }
