fdd99c2cdb96df0845e4c0c0435c402aef72cbdf
Abheesht Sharma <sharmabhee@gmail.com>
2023-05-28 07:35:47 +0530

    Fix title

--- examples/nlp/parameter_efficient_finetuning_of_gpt2_with_lora.py
+++ examples/nlp/parameter_efficient_finetuning_of_gpt2_with_lora.py
@@ -1,5 +1,5 @@
 """
-Title: Parameter-Efficient fine-tuning of GPT-2 with LoRA
+Title: Parameter-efficient fine-tuning of GPT-2 with LoRA
 Author: [Abheesht Sharma](https://github.com/abheesht17/)
 Date created: 2023/05/27
 Last modified: 2023/05/27
