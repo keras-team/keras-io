9c348db716ff965d9ae03d48c7f6a3cc8ff15e5f
Surya <116063290+SuryanarayanaY@users.noreply.github.com>
2023-10-09 11:46:53 +0530

    Correcting formatting errors in new code of mobilevit.py
    Correcting formatting errors in new code of mobilevit.py

--- examples/vision/mobilevit.py
+++ examples/vision/mobilevit.py
@@ -33,8 +33,9 @@ Note: This example should be run with Tensorflow 2.13 and higher.
 import tensorflow as tf
 
 from keras.src.applications import imagenet_utils
-#For versions <TF2.13 change the above import to:
-#from keras.applications import imagenet_utils
+
+# For versions <TF2.13 change the above import to:
+# from keras.applications import imagenet_utils
 from tensorflow.keras import layers
 from tensorflow import keras
 
