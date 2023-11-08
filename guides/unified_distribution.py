427ebaa0ed33e08e792f8aa27be075f0be249d78
Qianli Scott Zhu <scottzhu@google.com>
2023-11-07 16:40:44 -0800

    WIP for guide of distribution API

---
+++ guides/unified_distribution.py
@@ -0,0 +1,22 @@
+"""
+Title: The Distribution API for multi-backend Keras
+Author: [Qianli Zhu](https://github.com/qlzh727)
+Date created: 2023/11/07
+Last modified: 2023/11/07
+Description: Complete guide to the distribution API for multi-backend Keras.
+Accelerator: GPU
+"""
+
+"""
+## Setup
+"""
+
+import keras
+from keras import distribution
+
+"""
+## Introduction
+
+This guide covers 
+
+"""
