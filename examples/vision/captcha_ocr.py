936dd23eb0848836950d7c4878938ea043b12021
Matt Watson <mattdangerw@gmail.com>
2023-11-22 18:58:06 -0800

    Fix example

--- examples/vision/captcha_ocr.py
+++ examples/vision/captcha_ocr.py
@@ -37,9 +37,6 @@ import tensorflow as tf
 import keras
 from keras import layers
 
-# TODO restore traceback filtering.
-keras.config.disable_traceback_filtering()
-
 """
 ## Load the data: [Captcha Images](https://www.kaggle.com/fournierp/captcha-version-2-images)
 Let's download the data.
@@ -357,9 +354,33 @@ and try the demo on [Hugging Face Spaces](https://huggingface.co/spaces/keras-io
 """
 
 
+def ctc_decode(y_pred, input_length, greedy=True, beam_width=100, top_paths=1):
+    input_shape = tf.shape(y_pred)
+    num_samples, num_steps = input_shape[0], input_shape[1]
+    y_pred = tf.math.log(tf.transpose(y_pred, perm=[1, 0, 2]) + keras.backend.epsilon())
+    input_length = tf.cast(input_length, tf.int32)
+
+    if greedy:
+        (decoded, log_prob) = tf.nn.ctc_greedy_decoder(
+            inputs=y_pred, sequence_length=input_length
+        )
+    else:
+        (decoded, log_prob) = tf.compat.v1.nn.ctc_beam_search_decoder(
+            inputs=y_pred,
+            sequence_length=input_length,
+            beam_width=beam_width,
+            top_paths=top_paths,
+        )
+    decoded_dense = []
+    for st in decoded:
+        st = tf.SparseTensor(st.indices, st.values, (num_samples, num_steps))
+        decoded_dense.append(tf.sparse.to_dense(sp_input=st, default_value=-1))
+    return (decoded_dense, log_prob)
+
+
 # Get the prediction model by extracting layers till the output layer
 prediction_model = keras.models.Model(
-    model.get_layer(name="image").input, model.get_layer(name="dense2").output
+    model.input[0], model.get_layer(name="dense2").output
 )
 prediction_model.summary()
 
@@ -368,7 +389,7 @@ prediction_model.summary()
 def decode_batch_predictions(pred):
     input_len = np.ones(pred.shape[0]) * pred.shape[1]
     # Use greedy search. For complex tasks, you can use beam search
-    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+    results = ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
         :, :max_length
     ]
     # Iterate over the results and get back the text
