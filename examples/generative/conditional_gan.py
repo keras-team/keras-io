d011d1bbe8e541383a1316886fb1ee10174ee05e
sineeli <113718461+sineeli@users.noreply.github.com>
2024-01-02 13:59:35 -0800

    Migrate to Keras 3

--- examples/generative/conditional_gan.py
+++ examples/generative/conditional_gan.py
@@ -2,7 +2,7 @@
 Title: Conditional GAN
 Author: [Sayak Paul](https://twitter.com/RisingSayak)
 Date created: 2021/07/13
-Last modified: 2021/07/15
+Last modified: 2024/01/02
 Description: Training a GAN conditioned on class labels to generate handwritten digits.
 Accelerator: GPU
 """
@@ -51,9 +51,10 @@ pip install -q git+https://github.com/tensorflow/docs
 ## Imports
 """
 
-from tensorflow import keras
-from tensorflow.keras import layers
+import keras
 
+from keras import layers
+from keras import ops
 from tensorflow_docs.vis import embed
 import tensorflow as tf
 import numpy as np
@@ -118,9 +119,9 @@ discriminator = keras.Sequential(
     [
         keras.layers.InputLayer((28, 28, discriminator_in_channels)),
         layers.Conv2D(64, (3, 3), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
+        layers.LeakyReLU(negative_slope=0.2),
         layers.Conv2D(128, (3, 3), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
+        layers.LeakyReLU(negative_slope=0.2),
         layers.GlobalMaxPooling2D(),
         layers.Dense(1),
     ],
@@ -134,12 +135,12 @@ generator = keras.Sequential(
         # We want to generate 128 + num_classes coefficients to reshape into a
         # 7x7x(128 + num_classes) map.
         layers.Dense(7 * 7 * generator_in_channels),
-        layers.LeakyReLU(alpha=0.2),
+        layers.LeakyReLU(negative_slope=0.2),
         layers.Reshape((7, 7, generator_in_channels)),
         layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
+        layers.LeakyReLU(negative_slope=0.2),
         layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
-        layers.LeakyReLU(alpha=0.2),
+        layers.LeakyReLU(negative_slope=0.2),
         layers.Conv2D(1, (7, 7), padding="same", activation="sigmoid"),
     ],
     name="generator",
@@ -156,6 +157,7 @@ class ConditionalGAN(keras.Model):
         self.discriminator = discriminator
         self.generator = generator
         self.latent_dim = latent_dim
+        self.seed_generator = keras.random.SeedGenerator(1337)
         self.gen_loss_tracker = keras.metrics.Mean(name="generator_loss")
         self.disc_loss_tracker = keras.metrics.Mean(name="discriminator_loss")
 
@@ -176,18 +178,20 @@ class ConditionalGAN(keras.Model):
         # Add dummy dimensions to the labels so that they can be concatenated with
         # the images. This is for the discriminator.
         image_one_hot_labels = one_hot_labels[:, :, None, None]
-        image_one_hot_labels = tf.repeat(
+        image_one_hot_labels = ops.repeat(
             image_one_hot_labels, repeats=[image_size * image_size]
         )
-        image_one_hot_labels = tf.reshape(
+        image_one_hot_labels = ops.reshape(
             image_one_hot_labels, (-1, image_size, image_size, num_classes)
         )
 
         # Sample random points in the latent space and concatenate the labels.
         # This is for the generator.
-        batch_size = tf.shape(real_images)[0]
-        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
-        random_vector_labels = tf.concat(
+        batch_size = ops.shape(real_images)[0]
+        random_latent_vectors = keras.random.normal(
+            shape=(batch_size, self.latent_dim),
+            seed=self.seed_generator)
+        random_vector_labels = ops.concatenate(
             [random_latent_vectors, one_hot_labels], axis=1
         )
 
@@ -196,15 +200,15 @@ class ConditionalGAN(keras.Model):
 
         # Combine them with real images. Note that we are concatenating the labels
         # with these images here.
-        fake_image_and_labels = tf.concat([generated_images, image_one_hot_labels], -1)
-        real_image_and_labels = tf.concat([real_images, image_one_hot_labels], -1)
-        combined_images = tf.concat(
+        fake_image_and_labels = ops.concatenate([generated_images, image_one_hot_labels], -1)
+        real_image_and_labels = ops.concatenate([real_images, image_one_hot_labels], -1)
+        combined_images = ops.concatenate(
             [fake_image_and_labels, real_image_and_labels], axis=0
         )
 
         # Assemble labels discriminating real from fake images.
-        labels = tf.concat(
-            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        labels = ops.concatenate(
+            [ops.ones((batch_size, 1)), ops.zeros((batch_size, 1))], axis=0
         )
 
         # Train the discriminator.
@@ -217,19 +221,22 @@ class ConditionalGAN(keras.Model):
         )
 
         # Sample random points in the latent space.
-        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
-        random_vector_labels = tf.concat(
+        random_latent_vectors = keras.random.normal(
+            shape=(batch_size, self.latent_dim),
+            seed=self.seed_generator
+        )
+        random_vector_labels = ops.concatenate(
             [random_latent_vectors, one_hot_labels], axis=1
         )
 
         # Assemble labels that say "all real images".
-        misleading_labels = tf.zeros((batch_size, 1))
+        misleading_labels = ops.zeros((batch_size, 1))
 
         # Train the generator (note that we should *not* update the weights
         # of the discriminator)!
         with tf.GradientTape() as tape:
             fake_images = self.generator(random_vector_labels)
-            fake_image_and_labels = tf.concat([fake_images, image_one_hot_labels], -1)
+            fake_image_and_labels = ops.concatenate([fake_images, image_one_hot_labels], -1)
             predictions = self.discriminator(fake_image_and_labels)
             g_loss = self.loss_fn(misleading_labels, predictions)
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
@@ -271,33 +278,33 @@ trained_gen = cond_gan.generator
 num_interpolation = 9  # @param {type:"integer"}
 
 # Sample noise for the interpolation.
-interpolation_noise = tf.random.normal(shape=(1, latent_dim))
-interpolation_noise = tf.repeat(interpolation_noise, repeats=num_interpolation)
-interpolation_noise = tf.reshape(interpolation_noise, (num_interpolation, latent_dim))
+interpolation_noise = keras.random.normal(shape=(1, latent_dim))
+interpolation_noise = ops.repeat(interpolation_noise, repeats=num_interpolation)
+interpolation_noise = ops.reshape(interpolation_noise, (num_interpolation, latent_dim))
 
 
 def interpolate_class(first_number, second_number):
     # Convert the start and end labels to one-hot encoded vectors.
     first_label = keras.utils.to_categorical([first_number], num_classes)
     second_label = keras.utils.to_categorical([second_number], num_classes)
-    first_label = tf.cast(first_label, tf.float32)
-    second_label = tf.cast(second_label, tf.float32)
+    first_label = ops.cast(first_label, "float32")
+    second_label = ops.cast(second_label, "float32")
 
     # Calculate the interpolation vector between the two labels.
-    percent_second_label = tf.linspace(0, 1, num_interpolation)[:, None]
-    percent_second_label = tf.cast(percent_second_label, tf.float32)
+    percent_second_label = ops.linspace(0, 1, num_interpolation)[:, None]
+    percent_second_label = ops.cast(percent_second_label, "float32")
     interpolation_labels = (
         first_label * (1 - percent_second_label) + second_label * percent_second_label
     )
 
     # Combine the noise and the labels and run inference with the generator.
-    noise_and_labels = tf.concat([interpolation_noise, interpolation_labels], 1)
+    noise_and_labels = ops.concatenate([interpolation_noise, interpolation_labels], 1)
     fake = trained_gen.predict(noise_and_labels)
     return fake
 
 
-start_class = 1  # @param {type:"slider", min:0, max:9, step:1}
-end_class = 5  # @param {type:"slider", min:0, max:9, step:1}
+start_class = 2  # @param {type:"slider", min:0, max:9, step:1}
+end_class = 6  # @param {type:"slider", min:0, max:9, step:1}
 
 fake_images = interpolate_class(start_class, end_class)
 
@@ -310,8 +317,8 @@ with the label identities being present in some proportion.
 
 fake_images *= 255.0
 converted_images = fake_images.astype(np.uint8)
-converted_images = tf.image.resize(converted_images, (96, 96)).numpy().astype(np.uint8)
-imageio.mimsave("animation.gif", converted_images, fps=1)
+converted_images = ops.image.resize(converted_images, (96, 96)).numpy().astype(np.uint8)
+imageio.mimsave("animation.gif", converted_images[:, :, :, 0], fps=1)
 embed.embed_file("animation.gif")
 
 """
