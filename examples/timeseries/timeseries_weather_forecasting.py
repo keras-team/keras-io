881df2c0536d812f2c6968286e875266f8d91590
Cédric Uden <cedric@uden.de>
2024-01-18 20:26:50 +0100

    Based on my understanding the default activation function used in the output layer used for the predictions typically has a sigmoid activation function?
    see Fig. 10.16: https://www.deeplearningbook.org/contents/rnn.html (and other literatures)

--- examples/timeseries/timeseries_weather_forecasting.py
+++ examples/timeseries/timeseries_weather_forecasting.py
@@ -271,7 +271,7 @@ print("Target shape:", targets.numpy().shape)
 
 inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
 lstm_out = keras.layers.LSTM(32)(inputs)
-outputs = keras.layers.Dense(1)(lstm_out)
+outputs = keras.layers.Dense(1, activation=keras.activations.sigmoid)(lstm_out)
 
 model = keras.Model(inputs=inputs, outputs=outputs)
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss="mse")
