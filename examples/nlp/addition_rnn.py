79052626aec88d70efde704438bccbedbcc185ac
Francois Chollet <francois.chollet@gmail.com>
2024-02-13 11:07:18 -0800

    Update addition rnn example

--- examples/nlp/addition_rnn.py
+++ examples/nlp/addition_rnn.py
@@ -2,10 +2,11 @@
 Title: Sequence to sequence learning for performing number addition
 Author: [Smerity](https://twitter.com/Smerity) and others
 Date created: 2015/08/17
-Last modified: 2020/04/17
+Last modified: 2024/02/13
 Description: A model that learns to add strings of numbers, e.g. "535+61" -> "596".
 Accelerator: GPU
 """
+
 """
 ## Introduction
 
@@ -208,9 +209,14 @@ model.summary()
 ## Train the model
 """
 
+# Training parameters.
 epochs = 30
 batch_size = 32
 
+# Formatting characters for results display.
+green_color = "\033[92m"
+red_color = "\033[91m"
+end_char = "\033[0m"
 
 # Train the model each generation and show predictions against the validation
 # dataset.
@@ -229,23 +235,17 @@ for epoch in range(1, epochs):
     for i in range(10):
         ind = np.random.randint(0, len(x_val))
         rowx, rowy = x_val[np.array([ind])], y_val[np.array([ind])]
-        preds = np.argmax(model.predict(rowx), axis=-1)
+        preds = np.argmax(model.predict(rowx, verbose=0), axis=-1)
         q = ctable.decode(rowx[0])
         correct = ctable.decode(rowy[0])
         guess = ctable.decode(preds[0], calc_argmax=False)
         print("Q", q[::-1] if REVERSE else q, end=" ")
         print("T", correct, end=" ")
         if correct == guess:
-            print("☑ " + guess)
+            print(f"{green_color}☑ {guess}{end_char}")
         else:
-            print("☒ " + guess)
+            print(f"{red_color}☒ {guess}{end_char}")
 
 """
 You'll get to 99+% validation accuracy after ~30 epochs.
-
-Example available on HuggingFace.
-
-| Trained Model | Demo |
-| :--: | :--: |
-| [![Generic badge](https://img.shields.io/badge/🤗%20Model-Addition%20LSTM-black.svg)](https://huggingface.co/keras-io/addition-lstm) | [![Generic badge](https://img.shields.io/badge/🤗%20Spaces-Addition%20LSTM-black.svg)](https://huggingface.co/spaces/keras-io/addition-lstm) |
 """
