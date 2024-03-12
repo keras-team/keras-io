21fe55538437675a03f2fcc9955c3c5c51918334
Ramesh Sampath <1437573+sampathweb@users.noreply.github.com>
2024-03-12 12:02:41 -0500

    update tf_keras to 2.16 (#1790)

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -48,7 +48,7 @@ PROJECT_URL = {
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.7/",
     "keras_cv": f"{KERAS_TEAM_GH}/keras-cv/tree/v0.8.2/",
     "keras_nlp": f"{KERAS_TEAM_GH}/keras-nlp/tree/v0.8.2/",
-    "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.15.0/",
+    "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.16.0/",
 }
 USE_MULTIPROCESSING = False
 
