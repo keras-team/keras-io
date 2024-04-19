0e376dabaf315fe6b6e2deee1710dbdcd0dce88b
Sitam Meur <103279526+sitamgithub-MSIT@users.noreply.github.com>
2024-04-19 22:08:50 +0000

    seed generator added to rng layer

--- examples/generative/vae.py
+++ examples/generative/vae.py
@@ -29,11 +29,15 @@ from keras import layers
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.seed_generator = keras.random.SeedGenerator(1337)
+
     def call(self, inputs):
         z_mean, z_log_var = inputs
         batch = ops.shape(z_mean)[0]
         dim = ops.shape(z_mean)[1]
-        epsilon = keras.random.normal(shape=(batch, dim))
+        epsilon = keras.random.normal(shape=(batch, dim), seed=self.seed_generator)
         return z_mean + ops.exp(0.5 * z_log_var) * epsilon
 
 
