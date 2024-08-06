3373fff315aa54aa6f8544206fc14827051702e7
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-08-06 14:39:47 +0800

    Fix autogen.py

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -47,7 +47,7 @@ PROJECT_URL = {
     "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.4.1/",
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.7/",
     "keras_cv": f"{KERAS_TEAM_GH}/keras-cv/tree/v0.9.0/",
-    "keras_nlp": f"{KERAS_TEAM_GH}/keras-nlp/tree/v0.14.2/",
+    "keras_nlp": f"{KERAS_TEAM_GH}/keras-nlp/tree/v0.14.3/",
     "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.17.0/",
 }
 USE_MULTIPROCESSING = False
