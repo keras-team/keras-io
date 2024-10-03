964c0f2c8bc00af22a65c9182c5820340ca65fa6
Francois Chollet <francois.chollet@gmail.com>
2024-10-03 12:45:23 -0700

    Update API for Keras 3.6

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -49,7 +49,7 @@ EXAMPLES_GH_LOCATION = Path("keras-team") / "keras-io" / "blob" / "master" / "ex
 GUIDES_GH_LOCATION = Path("keras-team") / "keras-io" / "blob" / "master" / "guides"
 KERAS_TEAM_GH = "https://github.com/keras-team"
 PROJECT_URL = {
-    "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.5.0/",
+    "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.6.0/",
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.7/",
     "keras_cv": f"{KERAS_TEAM_GH}/keras-cv/tree/v0.9.0/",
     "keras_nlp": f"{KERAS_TEAM_GH}/keras-nlp/tree/v0.15.1/",
