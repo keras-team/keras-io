728220038bf47d8d2ba23e60b54b93deaaf482cd
Veeranjaneyulu Ch <74177924+chunduriv@users.noreply.github.com>
2024-12-05 11:50:49 -0800

    Update examples_master.py
    Update index

--- scripts/examples_master.py
+++ scripts/examples_master.py
@@ -867,6 +867,7 @@ EXAMPLES_MASTER = {
                     "path": "molecule_generation",
                     "title": "Drug Molecule Generation with VAE",
                     "subcategory": "Graph generation",
+                    "keras_3": True,
                 },
                 {
                     "path": "wgan-graphs",
