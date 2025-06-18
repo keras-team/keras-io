db38b67c88e2e642c2fda173e88a4ca6a49d7304
Laxma Reddy Patlolla <laxmareddyp@google.com>
2025-06-17 17:54:04 -0700

    Keras hub guide for loading and inference weights hosted on HF

---
+++ guides/keras_hub/hugging_face_keras_integration.py
@@ -0,0 +1,179 @@
+"""
+**Title:** Load HuggingFace Transformers checkpoint into a Multi backend KerasHub model
+**Author:** [Laxma Reddy Patlolla](https://github.com/laxmareddyp), [Divyashree Sreepathihalli](https://github.com/divyashreepathihalli)<br>
+**Date created:** 20245/06/17<br>
+**Last modified:** 2025/06/17<br>
+**Description:** How to load and infer from KerasHub model weights hosted on HuggingFace.
+**Accelerator:** None
+
+## Introduction
+
+KerasHub has built-in converters for HuggingFace's `.safetensors` models.
+Loading model weights from HuggingFace is therefore no more difficult than
+using KerasHub's own presets.
+
+### KerasHub built-in HuggingFace transformers converters
+
+KerasHub simplifies the use of HuggingFace Transformers models through its
+built-in converters. These converters automatically handle the process of translating
+HuggingFace model checkpoints into a format that's compatible with the Keras ecosystem.
+This means you can seamlessly load a wide variety of pretrained models from the HuggingFace
+Hub directly into KerasHub with just a few lines of code.
+
+Key advantages of using KerasHub converters:
+- **Ease of Use**: Load HuggingFace models without manual conversion steps.
+- **Broad Compatibility**: Access a vast range of models available on the HuggingFace Hub.
+- **Seamless Integration**: Work with these models using familiar Keras APIs for training,
+evaluation, and inference.
+
+Fortunately, all of this happens behind the scenes, so you can focus on using
+the models rather than managing the conversion process!
+
+## Setup
+
+Before you begin, make sure you have the necessary libraries installed.
+You'll primarily need `keras` and `keras_hub`.
+
+**Note:** Changing the backend after Keras has been imported might not work as expected.
+Ensure `KERAS_BACKEND` is set at the beginning of your script.
+"""
+
+import os
+
+os.environ["KERAS_BACKEND"] = "tensorflow"  # "tensorflow" or  "torch"
+
+import keras
+import keras_hub
+
+"""KerasHub allows you to easily load models from HuggingFace Transformers.
+Here's an example of how to load a Gemma causal language model.
+In this particular case, you will need to consent to Google's license on
+HuggingFace for being able to download model weights, and provide your
+`HF_TOKEN` as environment variable or as "Colab secret" when working with
+Google Colab.
+
+"""
+
+# not a keras checkpoint, it is a HF transformer checkpoint
+
+gemma_lm = keras_hub.models.GemmaCausalLM.from_preset("hf://google/gemma-2b")
+
+"""Let us try running some inference"""
+
+gemma_lm.generate("I want to say", max_length=30)
+
+"""
+### Fine-tune a Gemma Transformer checkpoint using the Keras model.fit(...) API.
+
+Once you have loaded HuggingFace weights, you can use the instantiated model
+just like any other KerasHub model. For instance, you might fine-tune the model
+on your own data like so:
+"""
+
+features = ["The quick brown fox jumped.", "I forgot my homework."]
+gemma_lm.fit(x=features, batch_size=2)
+
+"""
+### Saving and uploading the new checkpoint
+
+To store and share your fine-tuned model, KerasHub makes it easy to save or
+upload it using standard methods. You can do this through familiar commands
+such as:
+"""
+
+gemma_lm.save_to_preset("./gemma-2b-finetuned")
+keras_hub.upload_preset("hf://username/gemma-2b-finetune", "./gemma-2b-finetune")
+
+"""By uploading your preset, you can then load it from anywhere using:
+`loaded_model = keras_hub.models.GemmaCausalLM.from_preset("hf://YOUR_HF_USERNAME/gemma-2b-finetuned")`
+
+You can now access and use these 400K+ models with KerasHub,
+significantly expanding your model selection and capabilities
+
+By integrating HuggingFace Transformers, KerasHub significantly expands
+your access to pretrained models. The HuggingFace Hub hosts over 400,000 (and growing)
+model checkpoints across various domains like NLP, Computer Vision, Audio, and more.
+
+With KerasHub, you can:
+- **Tap into State-of-the-Art Models**: Easily experiment with the latest
+architectures and pretrained weights from the research community and industry.
+- **Reduce Development Time**: Leverage existing models instead of training from scratch,
+saving significant time and computational resources.
+- **Enhance Model Capabilities**: Find specialized models for a wide array of tasks,
+from text generation and translation to image segmentation and object detection.
+
+This seamless access empowers you to build more powerful and sophisticated AI applications with Keras.
+
+## Use a wider range of frameworks
+
+Keras 3, and by extension KerasHub, is designed for multi-framework compatibility.
+This means you can run your models with different backend frameworks like JAX, TensorFlow, and PyTorch.
+This flexibility allows you to:
+
+- **Choose the Best Backend for Your Needs**: Select a backend based on performance characteristics,
+hardware compatibility (e.g., TPUs with JAX), or existing team expertise.
+- **Interoperability**: More easily integrate KerasHub models into existing
+workflows that might be built on TensorFlow or PyTorch.
+- **Future-Proofing**: Adapt to evolving framework landscapes without
+rewriting your core model logic.
+
+## Run transformer models in JAX backend and on TPUs
+
+To experiment with a model using JAX, you can utilize Keras by setting its backend to JAX.
+By switching Keras’s backend before model construction, and ensuring your environment is connected to a TPU runtime.
+Keras will then automatically leverage JAX’s TPU support,
+allowing your model to train efficiently on TPU hardware without further code changes.
+"""
+
+import os
+
+os.environ["KERAS_BACKEND"] = "jax"
+gemma_lm = keras_hub.models.GemmaCausalLM.from_preset("hf://google/gemma-2b")
+
+"""
+## Common Use Cases
+
+### Generation
+
+A first use case of language models is to generate text.
+Here is an example to load a transformers model and generate new tokens using the `.generate`
+method from KerasHub
+"""
+
+from keras_hub.models import Llama3CausalLM
+
+# Get the model
+causal_lm = Llama3CausalLM.from_preset("hf://NousResearch/Hermes-2-Pro-Llama-3-8B")
+
+prompts = [
+    """<|im_start|>system
+You are a sentient, superintelligent artificial general intelligence, here to teach and assist me.<|im_end|>
+<|im_start|>user
+Write a short story about Goku discovering kirby has teamed up with Majin Buu to destroy the world.<|im_end|>
+<|im_start|>assistant""",
+]
+
+# Generate from the model
+causal_lm.generate(prompts, max_length=30)[0]
+
+"""
+### Changing precision
+
+You can adjust your model’s precision by configuring it through `keras.config` as follows
+
+"""
+
+import keras
+
+keras.config.set_dtype_policy("bfloat16")
+
+from keras_hub.models import Llama3CausalLM
+
+causal_lm = Llama3CausalLM.from_preset("hf://NousResearch/Hermes-2-Pro-Llama-3-8B")
+
+"""
+Go try loading other model weights! You can find more options on HuggingFace
+and use them with `from_preset("hf://<namespace>/<model-name>")`.
+
+Happy experimenting!
+"""
