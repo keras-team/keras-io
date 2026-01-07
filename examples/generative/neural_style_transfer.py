6a3eb6736ed7d46050584c706a9e9474b912e608
Jay Tiwari <jait66995@gmail.com>
2026-01-08 00:49:59 +0530

    Fix: Replace broken Imgur links with stable Google and GitHub URLs

--- examples/generative/neural_style_transfer.py
+++ examples/generative/neural_style_transfer.py
@@ -46,11 +46,16 @@ import numpy as np
 import tensorflow as tf
 from keras.applications import vgg19
 
-base_image_path = keras.utils.get_file("paris.jpg", "https://i.imgur.com/F28w3Ac.jpg")
-style_reference_image_path = keras.utils.get_file(
-    "starry_night.jpg", "https://i.imgur.com/9ooB60I.jpg"
+base_image_path = keras.utils.get_file(
+    "YellowLabradorLooking_new.jpg",
+    "https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg",
 )
-result_prefix = "paris_generated"
+style_reference_image_path = keras.utils.get_file(
+    "starry_night.jpg",
+    "https://raw.githubusercontent.com/jcjohnson/neural-style/master/examples/inputs/starry_night.jpg",
+)
+# ✅ NEW
+result_prefix = "style_transfer_result"
 
 # Weights of the different loss components
 total_variation_weight = 1e-6
