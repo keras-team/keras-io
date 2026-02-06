fa0230e82d8b9fee612640fb857d4022e3f47f48
Sachin Prasad <sachinprasad@google.com>
2026-02-05 23:30:01 -0800

    Update hub_master

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -34,7 +34,7 @@ KERAS_TEAM_GH = "https://github.com/keras-team"
 PROJECT_URL = {
     "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.13.2/",
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.8/",
-    "keras_hub": f"{KERAS_TEAM_GH}/keras-hub/tree/v0.25.1/",
+    "keras_hub": f"{KERAS_TEAM_GH}/keras-hub/tree/v0.26.0/",
     "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.19.0/",
     "keras_rs": f"{KERAS_TEAM_GH}/keras-rs/tree/v0.4.0/",
 }
