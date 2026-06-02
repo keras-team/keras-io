3718054b954208d6e0fe15f7b83316d8c81c46fb
Laxma Reddy Patlolla <laxmareddyp@google.com>
2026-06-01 20:17:28 -0700

    update tf anf tf-keras version

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -35,7 +35,7 @@ PROJECT_URL = {
     "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.14.1/",
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.8/",
     "keras_hub": f"{KERAS_TEAM_GH}/keras-hub/tree/v0.29.1/",
-    "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.21.0/",
+    "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.20.0/",
     "keras_rs": f"{KERAS_TEAM_GH}/keras-rs/tree/v0.4.0/",
 }
 USE_MULTIPROCESSING = False
