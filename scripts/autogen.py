0363e58ff87605a740720c5e27e6bdc1920d12d6
Laxma Reddy Patlolla <laxmareddyp@google.com>
2026-07-18 00:24:27 -0700

    Add Magistral API details

--- scripts/autogen.py
+++ scripts/autogen.py
@@ -34,7 +34,7 @@ KERAS_TEAM_GH = "https://github.com/keras-team"
 PROJECT_URL = {
     "keras": f"{KERAS_TEAM_GH}/keras/tree/v3.15.0/",
     "keras_tuner": f"{KERAS_TEAM_GH}/keras-tuner/tree/v1.4.8/",
-    "keras_hub": f"{KERAS_TEAM_GH}/keras-hub/tree/v0.29.1/",
+    "keras_hub": f"{KERAS_TEAM_GH}/keras-hub/tree/v0.30.0/",
     "tf_keras": f"{KERAS_TEAM_GH}/tf-keras/tree/v2.20.0/",
     "keras_rs": f"{KERAS_TEAM_GH}/keras-rs/tree/v0.4.0/",
 }
